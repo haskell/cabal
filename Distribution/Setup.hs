8ee94e18fab8f6f0a41749fb7083c92ac6d428a6
ijones <ijones@syntaxpolice.org>
2004-04-27 15:51:46 +0000

    used runTestTT to actually execute the Setup tests

--- Distribution/Setup.hs
+++ Distribution/Setup.hs
@@ -41,7 +41,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. -}
 
-module Distribution.Setup (parseArgs, Action(..)) where
+module Distribution.Setup (parseArgs, Action(..), hunitTests) where
 
 -- Local
 import Distribution.Misc(LocalBuildInfo, CompilerFlavor(..),
