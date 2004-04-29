c157cc9a92d7a889a66ed48975724bb1c0e043aa
ijones <ijones@syntaxpolice.org>
2004-04-29 09:04:46 +0000

    basic options parsing * Copied GetOpt and altered it to allow unknown options to flow through   instead of signaling an error. * Implemented parsing of all the flags * No parsing of commands yet * Broke up test suite a bit * Enumerated commands

--- Distribution/Setup.hs
+++ Distribution/Setup.hs
@@ -46,11 +46,13 @@ module Distribution.Setup (parseArgs, Action(..), hunitTests) where
  -- Local
 import Distribution.Misc(LocalBuildInfo, CompilerFlavor(..),
                          Compiler(..), LocalBuildInfo(..))
+import Distribution.GetOpt
 
 -- Misc:
-import HUnit (Test, (~:), (~=?))
+import HUnit (Test(..), (~:), (~=?))
 
--- |Parse the standard command-line arguments
+-- |Parse the standard command-line arguments.  FIX: is there a way to
+-- use getOpts and still get the unknown flags?
 parseArgs :: [String] -> CommandLineOpts
 parseArgs _ = (NoCmd, [])
 
@@ -58,28 +60,49 @@ parseArgs _ = (NoCmd, [])
 -- * command line
 -- ------------------------------------------------------------
 
-
 type CommandLineOpts = (Action,
                         [String]) -- The un-parsed remainder
 
-data Action = ConfigCmd LocalBuildInfo
-            | BuildCmd
-            | InstallCmd
-            | SDistCmd
-            | PackageInfoCmd
-            | RegisterCmd
+data Flag = GhcFlag | NhcFlag | HugsFlag
+          | WithCompiler String | Prefix String | InstPrefix String
+--          | Verbose | Version?
+            deriving (Show, Eq)
+
+options :: [OptDescr Flag]
+options = [Option "g" ["ghc"] (NoArg GhcFlag) "compile with GHC",
+           Option "n" ["nhc"] (NoArg NhcFlag) "compile with NHC",
+           Option "" ["hugs"] (NoArg HugsFlag) "compile with hugs",
+           Option "w" ["with-compiler"] (ReqArg WithCompiler "COMPILER PATH")
+               "give the path to a particular compiler",
+           Option "" ["prefix"] (ReqArg Prefix "DIR")
+               "bake this prefix in preparation of installation",
+           Option "" ["install-prefix"] (ReqArg InstPrefix "DIR")
+               "specify the directory in which to place installed files"
+          ]
+
+-- |command, help string
+commands :: [(String, String)]
+commands = [("configure", "configure this package"),
+            ("build", ""),
+            ("install", ""),
+            ("sdist", ""),
+            ("packageinfo", ""),
+            ("register", ""),
+            ("unregister","")
+           ]
+
+data Action = ConfigCmd LocalBuildInfo -- config
+            | BuildCmd                 -- build
+            | InstallCmd               -- install
+            | SDistCmd                 -- sdist
+            | PackageInfoCmd           -- packageinfo
+            | RegisterCmd              -- register
+            | UnregisterCmd            -- unregister
             | NoCmd -- error case?
 --             | TestCmd 1.0?
 --             | BDist -- 1.0
 --            | CleanCmd                 -- clean
     deriving (Show, Eq)
-
-
--- options :: [ OptDescr (Options -> IO Options) ]
--- options = 
---     [Option "p" ["prefix"]
---        (OptArg
---           (\arg opt -> return opt{command=useinfo...
 
 -- Locate the compiler based on the flavor
 exeLoc :: CompilerFlavor -> IO FilePath
@@ -103,7 +126,18 @@ hunitTests =
                          pkg <- pkgLoc comp
                          return (name, comp, loc, pkg)
                       | (name, comp) <- [("ghc", GHC), ("nhc", NHC), ("hugs", Hugs)]]
-       return $ [-- Config:
+       let (flags, commands, unkFlags, ers)
+               = getOpt Permute options ["configure", "foobar", "--prefix=/foo", "--ghc", "--nhc", "--hugs", "--with-compiler=/comp", "--unknown1", "--unknown2", "--install-prefix=/foo"]
+       return $ [TestLabel "very basic option parsing" $ TestList [
+                 "getOpt flags" ~: "failed" ~:
+                 [Prefix "/foo", GhcFlag, NhcFlag, HugsFlag,
+                  WithCompiler "/comp", InstPrefix "/foo"]
+                 ~=? flags,
+                 "getOpt commands" ~: "failed" ~: ["configure", "foobar"] ~=? commands,
+                 "getOpt unknown opts" ~: "failed" ~:
+                      ["--unknown1", "--unknown2"] ~=? unkFlags,
+                 "getOpt errors" ~: "failed" ~: [] ~=? ers],
+                TestLabel "Config" $ TestList [
                 "config prefix ghc given package tool" ~: "failed" ~:
                         basicGhcConfig ~=? (parseArgs ["--prefix=/lib", "--ghc",
                                                         "--with-compiler=/bin/ghc",
@@ -116,29 +150,29 @@ hunitTests =
                "locate compiler and package tool" ~: "failed" ~: 
                         realGhcConfig ~=? (parseArgs ["configure", "--ghc"]),
                "should we default to the current compiler?" ~: "failed" ~:
-                        realGhcConfig ~=? (parseArgs ["configure"])]
-               -- test location of various compilers
-               ++ ["locate " ++ name ++ " and pkg tool" ~: "failed" ~:
+                        realGhcConfig ~=? (parseArgs ["configure"])],
+               TestLabel "test location of various compilers" $ TestList
+               ["locate " ++ name ++ " and pkg tool" ~: "failed" ~:
                     (ConfigCmd (LocalBuildInfo "/usr/local"
                                 (Compiler comp comploc pkgloc)), [])
                    ~=? (parseArgs ["--prefix=/usr/local", "--"++name, "configure"])
-                   | (name, comp, comploc, pkgloc) <- m]
-               -- find the package tool
-               ++ ["locate pkg tool given " ++ name ~: "failed" ~:
+                   | (name, comp, comploc, pkgloc) <- m],
+               TestLabel "find the package tool" $ TestList
+               ["locate pkg tool given " ++ name ~: "failed" ~:
                     (ConfigCmd (LocalBuildInfo "/usr/local"
                                 (Compiler comp comploc pkgloc)), [])
                    ~=? (parseArgs ["--prefix=/usr/local", "--"++name,
                                    "--with-compiler="++name, "configure"])
-                   | (name, comp, comploc, pkgloc) <- m]
+                   | (name, comp, comploc, pkgloc) <- m],
 
-               -- simpler commands:
-               ++ [flag ~: "failed" ~: (flagCmd, []) ~=? (parseArgs [flag])
+               TestLabel "simpler commands" $ TestList
+               [flag ~: "failed" ~: (flagCmd, []) ~=? (parseArgs [flag])
                    | (flag, flagCmd) <- [("build", BuildCmd),
                                          ("install", InstallCmd),
                                          ("sdist", SDistCmd),
                                          ("packageinfo", PackageInfoCmd),
                                          ("register", RegisterCmd)]
-                  ]
+                  ]]
 
 {- Testing ideas:
    * IO to look for hugs and hugs-pkg (which hugs, etc)
