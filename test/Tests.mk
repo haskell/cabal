d687543e6745e2dd172781a063b3d0876ca99cd5
ijones <ijones@syntaxpolice.org>
2005-01-06 05:02:41 +0000

    cleaned and simplified makefiles to speed up build

---
+++ test/Tests.mk
@@ -0,0 +1,17 @@
+all:
+	ghc -cpp --make -i../.. Setup.lhs -o setup
+clean:
+	rm -f setup a.out
+	rm -rf ,tmp* dist
+	find . -name "*.o" |xargs rm -f
+	find . -name "*.hi" |xargs rm -f
+check: all
+	./setup configure --user --prefix=/tmp/foo
+	./setup build
+	./setup install --install-prefix=/tmp/bar
+	ls /tmp/bar*
+	# install w/ register!
+	./setup install
+#	ls /tmp/foo*
+	./setup sdist
+	ls dist
