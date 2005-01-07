ad1c4ef3e17dc79ad889bd3b8086b3d4e6520470
ijones <ijones@syntaxpolice.org>
2005-01-07 06:05:43 +0000

    simon and ross got rid of dots in objExtension etc

--- Distribution/Compat/FilePath.hs
+++ Distribution/Compat/FilePath.hs
@@ -377,31 +377,31 @@ searchPathSeparator = ':'
 
 -- ToDo: This should be determined via autoconf (AC_EXEEXT)
 -- | Extension for executable files
--- (typically @\"\"@ on Unix and @\".exe\"@ on Windows or OS\/2)
+-- (typically @\"\"@ on Unix and @\"exe\"@ on Windows or OS\/2)
 exeExtension :: String
 #ifdef mingw32_TARGET_OS
-exeExtension = ".exe"
+exeExtension = "exe"
 #else
 exeExtension = ""
 #endif
 
 -- ToDo: This should be determined via autoconf (AC_OBJEXT)
 -- | Extension for object files
--- (typically @\".o\"@ on Unix and @\".obj\"@ on Windows)
+-- (typically @\"o\"@ on Unix and @\"obj\"@ on Windows)
 objExtension :: String
 #ifdef mingw32_TARGET_OS
-objExtension = ".obj"
+objExtension = "obj"
 #else
-objExtension = ".o"
+objExtension = "o"
 #endif
 
 -- | Extension for dynamically linked (or shared) libraries
--- (typically @\".so\"@ on Unix and @\".dll\"@ on Windows)
+-- (typically @\"so\"@ on Unix and @\"dll\"@ on Windows)
 dllExtension :: String
 #ifdef mingw32_TARGET_OS
-dllExtension = ".dll"
+dllExtension = "dll"
 #else
-dllExtension = ".so"
+dllExtension = "so"
 #endif
 
 #endif /* __GLASGOW_HASKELL__ && __GLASGOW_HASKELL__ <= 603 */
