a6ee0db2ff0b3c48dfac2f6bb744a4b8cfebd27b
ijones <ijones@syntaxpolice.org>
2005-03-20 23:06:34 +0000

    wibble last commit, only if genScript true

--- Distribution/Simple/Register.hs
+++ Distribution/Simple/Register.hs
@@ -169,7 +169,7 @@ register pkg_descr lbi (userInst, genScript, verbose)
 	let allFlags = "--auto-ghci-libs":
 		       (register_flags
                         ++ config_flags)
-                       ++ if ghc_63_plus then ["-"] else []
+                       ++ if ghc_63_plus && genScript then ["-"] else []
         let pkgTool = compilerPkgTool (compiler lbi)
 
         if genScript
