991263baf0f59bf1b4769f436b94fc5ebeeeb03f
ijones <ijones@syntaxpolice.org>
2005-04-15 18:09:27 +0000

    added another missing source file

---
+++ tests/HSQL/config.mk
@@ -0,0 +1,28 @@
+WithODBC=NO
+WithPostgreSQL=NO
+WithMySQL=NO
+WithSQLite=NO
+
+SO_EXT = so
+
+WIN32=NO
+
+AR = /usr/bin/ar
+LD = /usr/bin/ld
+INSTALL = /usr/bin/install -c
+
+CPPFLAGS = 
+LDFLAGS =  
+
+HSC2HS = /usr/bin/hsc2hs
+
+HADDOCK = /usr/bin/haddock
+DOC_DIR = /tmp/doc/HSQL
+
+GHC     = /usr/bin/ghc
+GHC_DIR = /tmp/lib/HSQL/GHC
+GHC_PKG = /usr/bin/ghc-pkg
+
+HUGS     = /usr/bin/hugs
+HUGS_DIR = /tmp/lib/HSQL/Hugs
+FFIHUGS  = /usr/bin/ffihugs
