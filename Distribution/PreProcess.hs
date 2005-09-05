40170cd7f01a3fe06c99d767d2eea68edc9e7374
ijones <ijones@syntaxpolice.org>
2005-09-05 18:55:43 +0000

    added -D__GLASGOW_HASKELL__ for hcDefines, probably needed by cpp

--- Distribution/PreProcess.hs
+++ Distribution/PreProcess.hs
@@ -250,6 +250,8 @@ hcDefines Compiler { compilerFlavor=NHC, compilerVersion=version }
   = ["-D__NHC__=" ++ versionInt version]
 hcDefines Compiler { compilerFlavor=Hugs }
   = ["-D__HUGS__"]
+hcDefines Compiler { compilerFlavor=GHC, compilerVersion=version }
+  = ["-D__GLASGOW_HASKELL__=" ++ versionInt version]
 hcDefines _ = []
 
 versionInt :: Version -> String
