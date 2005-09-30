b000d8a491313b8a3d78affcbd1dea7a36258c94
ijones <ijones@syntaxpolice.org>
2005-09-30 12:40:55 +0000

    generalizing haddock based on Program type

--- Distribution/Simple/Configure.hs
+++ Distribution/Simple/Configure.hs
@@ -85,7 +85,8 @@ import Data.Maybe(fromMaybe)
 import System.Directory
 import Distribution.Compat.FilePath (splitFileName, joinFileName,
                                   joinFileExt, exeExtension)
-import Distribution.Program(Program(..), ProgramLocation(..), lookupProgram)
+import Distribution.Program(Program(..), ProgramLocation(..), lookupProgram,
+                            haddockProgram)
 import System.Cmd		( system )
 import System.Exit		( ExitCode(..) )
 import Control.Monad		( when, unless )
@@ -142,7 +143,8 @@ configure pkg_descr cfg
         unless (null exts) $ warn $ -- Just warn, FIXME: Should this be an error?
             show f' ++ " does not support the following extensions:\n " ++
             concat (intersperse ", " (map show exts))
-        haddock   <- lookupProgram "haddock" (configPrograms cfg) (configHaddock cfg)
+        let haddockName = programName haddockProgram
+        haddock   <- lookupProgram haddockName (configPrograms cfg)
         happy     <- findProgram "happy"     (configHappy cfg)
         alex      <- findProgram "alex"      (configAlex cfg)
         hsc2hs    <- findProgram "hsc2hs"    (configHsc2hs cfg)
@@ -155,7 +157,7 @@ configure pkg_descr cfg
         message $ "Compiler flavor: " ++ (show f')
         message $ "Compiler version: " ++ showVersion ver
         message $ "Using package tool: " ++ pkg
-        reportProgram' "haddock"  haddock
+        reportProgram' haddockName haddock
         reportProgram "happy"     happy
         reportProgram "alex"      alex
         reportProgram "hsc2hs"    hsc2hs
