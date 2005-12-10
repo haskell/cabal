6727a3f42cadd94935458012bd06b6901f1519cd
ijones <ijones@syntaxpolice.org>
2005-12-10 23:50:20 +0000

    tweaks to make --with-runghc and --with-runhugs work in cabal-install.

--- cabal-install/CabalInstall.hs
+++ cabal-install/CabalInstall.hs
@@ -6,7 +6,6 @@
 module Main where
 
 import Control.Monad(when)
-import Data.Maybe (fromJust) -- FIX: remove.
 import Data.List(partition)
 import System.Directory(getCurrentDirectory, setCurrentDirectory, getDirectoryContents)
 import System.Cmd(system)
@@ -29,39 +28,46 @@ quitFail e = exitWith e
 doInstall dir conf confArgs = do
   currDir <- getCurrentDirectory
   setCurrentDirectory dir
-  comp <- compilerCommand conf
-  -- FIX: remove fromJust
-  let p = rawSystemProgram 0 (fromJust comp) 
+  compM <- compilerCommand conf
+  let comp = case compM of 
+               Nothing -> noCompErr
+               Just (Program _ _ _ EmptyLocation) -> noCompErr
+               Just c  -> c
+  let p = rawSystemProgram 3 comp
   p ("configure":confArgs) >>= quitFail
   p ["build"]              >>= quitFail
   p ["install"]            >>= quitFail
   setCurrentDirectory currDir
   return ()
+      where noCompErr = error "No compiler found during configure.  Please use --with-runghc= or --with-runhugs=."
 
-installerPrograms = 
-    foldl (\pConf p -> updateProgram (Just p) pConf)
-                        defaultProgramConfiguration
-                        [Program "runghc" "runghc" ["Setup"] EmptyLocation,
-                         Program "runhugs" "runhugs" ["-98", "Setup"] EmptyLocation]
+installerPrograms = defaultProgramConfiguration
 
--- FIX: 
--- * get program config from --with-runghc=, etc.
--- * some day be more flexible here; allow setup script to be built
--- w/ nhc or GHC, for instance.
+-- |Figure out how to use runghc or runhugs.
 compilerCommand :: ConfigFlags -> IO (Maybe Program)
 compilerCommand conf = do
-  let prog = (case configHcFlavor conf of
-               Just GHC  -> "runghc"
-               Just Hugs -> "runhugs"
-               Nothing -> case defaultCompilerFlavor of
-                           Just GHC  -> "runghc"
-                           Just Hugs -> "runhugs"
-                           Nothing   -> error "please specify one of --ghc or --hugs")
-  lookupProgram prog (configPrograms conf)
+  let (progName, args)
+          = case compilerFlav conf of
+              GHC -> ("runghc",   ["Setup"])
+              Hugs -> ("runhugs", ["-98", "Setup"])
+  progM <- lookupProgram progName (configPrograms conf)
+  case progM of
+    Nothing   -> return Nothing
+    Just prog -> return $ Just prog{programArgs=args ++ (programArgs prog)}
+
+compilerFlav :: ConfigFlags -> CompilerFlavor
+compilerFlav conf = 
+    case configHcFlavor conf of
+      Just GHC  -> GHC
+      Just Hugs -> Hugs
+      Nothing -> case defaultCompilerFlavor of
+                   Just GHC  -> GHC
+                   Just Hugs -> Hugs
+                   Nothing   -> error "please specify one of --ghc or --hugs"
 
 unPackOrGo tarOrDir conf confArgs = do
   let (packageIdentStr, ext) = splitFileExt tarOrDir
-  putStrLn $ "package; " ++ (show packageIdentStr)
+  putStrLn $ "package: " ++ (show packageIdentStr)
   case ext of
     "tgz" -> do system $ "tar -zxvf " ++ tarOrDir
                 unPackOrGo packageIdentStr conf confArgs
@@ -83,6 +89,6 @@ main = do
                     (emptyConfigFlags installerPrograms)
                      configArgs
                      []
-  when (toInstall == []) (error "please give a tarball or directory on the command-line.")
+  when (toInstall == []) (error "please give a tarball or directory on the command-line (try --help)")
   mapM (\i -> unPackOrGo i conf configArgs) toInstall
   return ()
