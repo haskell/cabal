6dc5683b56ab6f3dabceb99d27c380978dfb390b
Ian Lynagh <igloo@earth.li>
2007-01-10 17:40:50 +0000

    Pass 0 verbosity on to GHC when building

--- Distribution/Simple/GHC.hs
+++ Distribution/Simple/GHC.hs
@@ -346,7 +346,7 @@ constructGHCCmdLine
         -> [String]
 constructGHCCmdLine lbi bi odir verbose = 
         ["--make"]
-     ++ (if verbose > 4 then ["-v"] else [])
+     ++ (if verbose > 4 then ["-v"] else if verbose > 0 then [] else ["-v0"])
 	    -- Unsupported extensions have already been checked by configure
      ++ (if compilerVersion (compiler lbi) > Version [6,4] []
             then ["-hide-all-packages"]
