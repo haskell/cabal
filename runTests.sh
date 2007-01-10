797db4f865d058b2165de49c6278f242a2f41edf
Ian Lynagh <igloo@earth.li>
2007-01-10 14:27:56 +0000

    Remove some chatter from the test scripts

--- runTests.sh
+++ runTests.sh
@@ -5,7 +5,13 @@ HC=$HCBASE/ghc
 GHCFLAGS='--make -Wall -fno-warn-unused-matches -cpp'
 ISPOSIX=-DHAVE_UNIX_PACKAGE
 
-rm moduleTest
+rm -f moduleTest
 mkdir -p dist/debug
-$HC $GHCFLAGS $ISPOSIX -DDEBUG -odir dist/debug -hidir dist/debug -idist/debug/:.:tests/HUnit-1.0/src tests/ModuleTest.hs -o moduleTest 
+$HC $GHCFLAGS $ISPOSIX -DDEBUG -odir dist/debug -hidir dist/debug -idist/debug/:.:tests/HUnit-1.0/src tests/ModuleTest.hs -o moduleTest 2> stderr
+RES=$?
+if [ $RES != 0 ]
+then
+    cat stderr >&2
+    exit $RES
+fi
 ./moduleTest
