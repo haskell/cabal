e4182354c2b55e8a042c7fc829ee72924dfac675
Ian Lynagh <igloo@earth.li>
2007-01-11 13:26:54 +0000

    Give feedback in runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -7,6 +7,7 @@ ISPOSIX=-DHAVE_UNIX_PACKAGE
 
 rm -f moduleTest
 mkdir -p dist/debug
+echo Building...
 $HC $GHCFLAGS $ISPOSIX -DDEBUG -odir dist/debug -hidir dist/debug -idist/debug/:.:tests/HUnit-1.0/src tests/ModuleTest.hs -o moduleTest 2> stderr
 RES=$?
 if [ $RES != 0 ]
@@ -14,4 +15,7 @@ then
     cat stderr >&2
     exit $RES
 fi
+echo Running...
 ./moduleTest
+echo Done
+
