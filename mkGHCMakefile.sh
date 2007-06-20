6251a0ec7f71464c2a2a950e7a3abe758200c87e
Ross Paterson <ross@soi.city.ac.uk>
2007-06-20 15:21:03 +0000

    break up the long string literal
    This makes the output work with implementations (like Hugs) that impose
    a limit on the size of string literals.

--- mkGHCMakefile.sh
+++ mkGHCMakefile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 file=Distribution/Simple/GHCMakefile.hs
 echo "-- DO NOT EDIT: change GHCMakefile.in, and run ../../mkGHCMakefile.sh" >$file
-echo "module Distribution.Simple.GHCMakefile where{ makefileTemplate=" >>$file
-ghc -e "readFile \"Distribution/Simple/GHCMakefile.in\" >>= print" >>$file
-echo ":: String }" >>$file
+echo "module Distribution.Simple.GHCMakefile where{ makefileTemplate=unlines" >>$file
+ghc -e "readFile \"Distribution/Simple/GHCMakefile.in\" >>= print . lines" >>$file
+echo "}" >>$file
