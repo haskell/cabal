9595961102976c849e13a9b1088357fc8c809bca
Duncan Coutts <duncan@haskell.org>
2007-07-24 17:20:23 +0000

    -Wall police

--- mkGHCMakefile.sh
+++ mkGHCMakefile.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 file=Distribution/Simple/GHCMakefile.hs
 echo "-- DO NOT EDIT: change GHCMakefile.in, and run ../../mkGHCMakefile.sh" >$file
-echo "module Distribution.Simple.GHCMakefile where{ makefileTemplate=unlines" >>$file
+echo "module Distribution.Simple.GHCMakefile where {" >>$file
+echo "makefileTemplate :: String; makefileTemplate=unlines" >>$file
 ghc -e "readFile \"Distribution/Simple/GHCMakefile.in\" >>= print . lines" >>$file
 echo "}" >>$file
