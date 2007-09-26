00ec8873f203abf9f8368653efbcec030fdf5437
Clemens Fruhwirth <clemens@endorphin.org>
2007-09-26 09:00:25 +0000

    Add -package-name to GHC invocation when linking DSOs

--- Distribution/Simple/GHC.hs
+++ Distribution/Simple/GHC.hs
@@ -380,6 +380,7 @@ build pkg_descr lbi verbosity = do
 		  "-dynamic",
 		  "-o", sharedLibName ]
 		++ ghcSharedObjArgs
+		++ ["-package-name", packageId ]
 		++ (concat [ ["-package", showPackageId pkg] | pkg <- packageDeps lbi ])
 
             runLd ldLibName args = do
@@ -593,7 +594,7 @@ makefile pkg_descr lbi flags = do
         ("GHC_CC_OPTS", unwords (ghcCcOptions lbi bi (buildDir lbi))),
         ("GHCI_LIB", mkGHCiLibName builddir (showPackageId (package pkg_descr))),
         ("soext", dllExtension),
-        ("LIB_LD_OPTS", unwords (concat [ ["-package", showPackageId pkg] | pkg <- packageDeps lbi ])),
+        ("LIB_LD_OPTS", unwords ("-package-name" : packageId : concat [ ["-package", showPackageId pkg] | pkg <- packageDeps lbi ])),
         ("AR", programPath arProg),
         ("LD", programPath ldProg)
         ]
