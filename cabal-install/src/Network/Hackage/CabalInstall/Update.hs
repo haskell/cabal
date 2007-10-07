38189542de8b4b09b99a4eb6914d691eb8cc3641
bjorn <bjorn@bringert.net>
2007-10-07 10:02:58 +0000

    Replace zlib use by compression.

--- cabal-install/src/Network/Hackage/CabalInstall/Update.hs
+++ cabal-install/src/Network/Hackage/CabalInstall/Update.hs
@@ -17,13 +17,13 @@ module Network.Hackage.CabalInstall.Update
 import Network.Hackage.CabalInstall.Types (ConfigFlags (..), PkgInfo(..), Repo(..))
 import Network.Hackage.CabalInstall.Utils (isVerbose)
 import Network.Hackage.CabalInstall.Fetch (downloadIndex, packagesDirectory)
+import Network.Hackage.CabalInstall.Tar (gunzip)
 
 import Distribution.Package (PackageIdentifier(..), pkgName, showPackageId)
 import Distribution.PackageDescription (PackageDescription(..), readPackageDescription, GenericPackageDescription(..))
 import Distribution.Verbosity
 import System.FilePath ((</>), joinPath, addExtension, takeExtension, dropExtension)
 
-import Codec.Compression.GZip(decompress)
 import Control.Monad (liftM, when)
 import qualified Data.ByteString.Lazy.Char8 as BS
 import Data.ByteString.Lazy.Char8 (ByteString)
@@ -41,4 +41,4 @@ updateRepo :: ConfigFlags
 updateRepo cfg repo =
     do printf "Downloading package list from server '%s'\n" (repoURL repo)
        indexPath <- downloadIndex cfg repo
-       BS.readFile indexPath >>= BS.writeFile (dropExtension indexPath) . decompress
+       BS.readFile indexPath >>= BS.writeFile (dropExtension indexPath) . gunzip
