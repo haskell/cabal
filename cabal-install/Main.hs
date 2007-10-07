ced0c07182d47b7f37deff71adaea753cc901675
Don Stewart <dons@galois.com>
2007-10-07 15:50:13 +0000

    polish

--- cabal-install/Main.hs
+++ cabal-install/Main.hs
@@ -10,37 +10,45 @@
 --
 -- Entry point to the default cabal-install front-end.
 -----------------------------------------------------------------------------
+
 module Main where
 
-import Data.List (isSuffixOf)
-import System.Environment (getArgs)
-import Hackage.Types (Action (..), Option(..))
-import Hackage.Setup (parseGlobalArgs, parsePackageArgs, configFromOptions)
-import Hackage.Config (defaultConfigFile, loadConfig)
+import Hackage.Types            (Action (..), Option(..))
+import Hackage.Setup            (parseGlobalArgs, parsePackageArgs, configFromOptions)
+import Hackage.Config           (defaultConfigFile, loadConfig)
+import Hackage.List             (list)
+import Hackage.Install          (install)
+import Hackage.Info             (info)
+import Hackage.Update           (update)
+import Hackage.Fetch            (fetch)
+import Hackage.Clean            (clean)
 
-import Hackage.List (list)
-import Hackage.Install (install)
-import Hackage.Info (info)
-import Hackage.Update (update)
-import Hackage.Fetch (fetch)
-import Hackage.Clean (clean)
+import Data.List                (isSuffixOf)
+import System.Environment       (getArgs)
 
-
+-- | Entry point
+--
 main :: IO ()
-main = do args <- getArgs
-          (action, flags, args) <- parseGlobalArgs args
-          configFile <- case [f | OptConfigFile f <- flags] of
-                          [] -> defaultConfigFile
-                          fs -> return $ last fs
-          conf0  <- loadConfig configFile
-          let config = configFromOptions conf0 flags
-              runCmd f = do (globalArgs, pkgs) <- parsePackageArgs action args
-                            f config globalArgs pkgs
-          case action of
-            InstallCmd  -> runCmd install
-            InfoCmd     -> runCmd info
-            ListCmd     -> list config args
-            UpdateCmd   -> update config
-            CleanCmd    -> clean config
-            FetchCmd    -> fetch config args
-            _           -> putStrLn "Unhandled command."
+main = do
+    args       <- getArgs
+    (action, flags, args) <- parseGlobalArgs args
+    configFile <- case [f | OptConfigFile f <- flags] of
+                        [] -> defaultConfigFile
+                        fs -> return (last fs)
+
+    conf0      <- loadConfig configFile
+
+    let config = configFromOptions conf0 flags
+
+        runCmd f = do (globalArgs, pkgs) <- parsePackageArgs action args
+                      f config globalArgs pkgs
+
+    case action of
+        InstallCmd  -> runCmd install
+        InfoCmd     -> runCmd info
+        ListCmd     -> list   config args
+        UpdateCmd   -> update config
+        CleanCmd    -> clean  config
+        FetchCmd    -> fetch  config args
+        _           -> error "Unhandled command."
+
