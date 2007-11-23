dbb334cca9c89831cf71c3672942eefc7c2922bc
Duncan Coutts <duncan@haskell.org>
2007-11-23 21:00:09 +0000

    Workaround import bug in nhc98 Cabal can now build using itself and nhc98

--- Distribution/Simple/SetupWrapper.hs
+++ Distribution/Simple/SetupWrapper.hs
@@ -24,7 +24,7 @@ import Distribution.Simple.Configure
 import Distribution.Simple.Setup	( reqPathArg )
 import Distribution.PackageDescription	 
 				( readPackageDescription,
-                                  packageDescription,
+                                  GenericPackageDescription(packageDescription),
 				  PackageDescription(..),
                                   BuildType(..), cabalVersion )
 import Distribution.Simple.LocalBuildInfo ( distPref )
