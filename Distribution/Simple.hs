918004f629670a73f28d1ba58d906fb23dd9ac75
Duncan Coutts <duncan@haskell.org>
2007-12-17 13:20:48 +0000

    Add --list-options flag, should be useful for shell command line completion It applies at the top level in which case it lists the global flags and the sub-commands. It applies for each sub-command in which case it lists the sub-command's available flags.

--- Distribution/Simple.hs
+++ Distribution/Simple.hs
@@ -152,17 +152,20 @@ defaultMainHelper :: UserHooks -> Args -> IO ()
 defaultMainHelper hooks args =
   case commandsRun globalCommand commands args of
     CommandHelp   help                 -> printHelp help
+    CommandList   opts                 -> printOptionsList opts
     CommandErrors errs                 -> printErrors errs
     CommandReadyToGo (flags, commandParse)  ->
       case commandParse of
         _ | fromFlag (globalVersion flags)        -> printVersion
           | fromFlag (globalNumericVersion flags) -> printNumericVersion
         CommandHelp     help           -> printHelp help
+        CommandList     opts           -> printOptionsList opts
         CommandErrors   errs           -> printErrors errs
         CommandReadyToGo action        -> action
 
   where
     printHelp help = getProgName >>= putStr . help
+    printOptionsList = putStr . unlines
     printErrors errs = do
       putStr (concat (intersperse "\n" errs))
       exitWith (ExitFailure 1)
