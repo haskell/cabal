d2ec987f3d7c40cd8a44e8a37dcd0fafdece34b9
Duncan Coutts <duncan@haskell.org>
2007-12-30 01:25:30 +0000

    Refactor unlit code to improve error and line pragma handling And to make it more like the original code that classified lines separately from checking and transforming them.

--- Distribution/Simple/Hugs.hs
+++ Distribution/Simple/Hugs.hs
@@ -249,7 +249,9 @@ build pkg_descr lbi verbosity = do
 	readHaskellFile :: FilePath -> IO String
 	readHaskellFile file = do
 	    text <- readFile file
-	    return $ if ".lhs" `isSuffixOf` file then unlit file text else text
+	    if ".lhs" `isSuffixOf` file
+              then either return die (unlit file text)
+              else return text
 
 -- ------------------------------------------------------------
 -- * options in source files
@@ -265,11 +267,13 @@ getOptionsFromSource
           )
 getOptionsFromSource file = do
     text <- readFile file
+    text' <- if ".lhs" `isSuffixOf` file
+               then either return die (unlit file text)
+               else return text
     return $ foldr appendOptions ([],[],[]) $ map getOptions $
 	takeWhileJust $ map getPragma $
 	filter textLine $ map (dropWhile isSpace) $ lines $
-	stripComments True $
-	if ".lhs" `isSuffixOf` file then unlit file text else text
+	stripComments True text'
   where textLine [] = False
 	textLine ('#':_) = False
 	textLine _ = True
