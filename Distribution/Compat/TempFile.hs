3ba344ee3f9301f8ccc0fdf53c5ad3db28119a8b
Duncan Coutts <duncan@haskell.org>
2008-01-23 23:58:47 +0000

    Rework withTempFile code and use System.IO.openTempFile where available The System.IO.openTempFile is much better because it opens the temp files securely. Howver it is only available in base-2 and only of GHC. In base-3 it is available for all implementations. So in practice that means it's only for GHC and we have to use our compatability implementation for hugs and nhc98. Not sure of the status for jhc.

--- Distribution/Compat/TempFile.hs
+++ Distribution/Compat/TempFile.hs
@@ -3,62 +3,42 @@
 {-# OPTIONS_NHC98 -cpp #-}
 {-# OPTIONS_JHC -fcpp #-}
 -- #hide
-module Distribution.Compat.TempFile (openTempFile, withTempFile) where
+module Distribution.Compat.TempFile (openTempFile) where
 
-import System.IO (openFile, Handle, IOMode(ReadWriteMode))
-import System.Directory (doesFileExist, removeFile)
-import Control.Exception (finally,try)
-
-import System.FilePath ( (</>), (<.>) )
-
-#if (__GLASGOW_HASKELL__ || __HUGS__)
+#if __NHC__ || __HUGS__
+import System.IO              (openFile, Handle, IOMode(ReadWriteMode))
+import System.Directory       (doesFileExist)
+import System.FilePath        ((</>))
 import System.Posix.Internals (c_getpid)
 #else
-import System.Posix.Types (CPid(..))
+import System.IO (openTempFile)
 #endif
-
 
 -- ------------------------------------------------------------
 -- * temporary files
 -- ------------------------------------------------------------
 
--- TODO: this function *really really really* should be
---       eliminated and replaced with System.IO.openTempFile,
---       except that is currently GHC-only for no valid reason.
+-- This is here for Haskell implementations that do not come with
+-- System.IO.openTempFile. This includes nhc-1.20, hugs-2006.9.
+-- TODO: Not sure about jhc
 
+#if __NHC__ || __HUGS__
 -- use a temporary filename that doesn't already exist.
 -- NB. *not* secure (we don't atomically lock the tmp file we get)
 openTempFile :: FilePath -> String -> IO (FilePath, Handle)
 openTempFile tmp_dir template
   = do x <- getProcessID
        findTempName x
-  where 
-    findTempName x
-      = do let filename = template ++ show x
-	       path = tmp_dir </> filename
-  	   b  <- doesFileExist path
-	   if b then findTempName (x+1)
-		else do hnd <- openFile path ReadWriteMode
-                        return (path, hnd)
-
-#if !(__GLASGOW_HASKELL__ || __HUGS__)
-foreign import ccall unsafe "getpid" c_getpid :: IO CPid
-#endif
-
-getProcessID :: IO Int
-getProcessID = c_getpid >>= return . fromIntegral
-
--- use a temporary filename that doesn't already exist.
--- NB. *not* secure (we don't atomically lock the tmp file we get)
-withTempFile :: FilePath -> String -> (FilePath -> IO a) -> IO a
-withTempFile tmp_dir extn action
-  = do x <- getProcessID
-       findTempName x
   where
+    (templateBase, templateExt) = splitExtension template
+    findTempName :: Int -> IO (FilePath, Handle)
     findTempName x
-      = do let filename = ("tmp" ++ show x) <.> extn
-               path = tmp_dir </> filename
+      = do let path = tmp_dir </> (templateBase ++ show x) <.> templateExt
            b  <- doesFileExist path
            if b then findTempName (x+1)
-                else action path `finally` try (removeFile path)
+                else do hnd <- openFile path ReadWriteMode
+                        return (path, hnd)
 
+    getProcessID :: IO Int
+    getProcessID = fmap fromIntegral c_getpid
+#endif
