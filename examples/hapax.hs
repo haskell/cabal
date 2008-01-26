49784b1c3e755d4072b52b9991867dcc8124404f
Duncan Coutts <duncan@haskell.org>
2008-01-26 23:33:08 +0000

    Remove examples dir which seemed rather pointless

--- examples/hapax.hs
+++
@@ -1,8 +0,0 @@
--- Simple general-purpose Cabal setup script
-
-module Main (main) where
-
-import Distribution.Simple (defaultMainWithHooks, defaultUserHooks)
-
-main :: IO ()
-main = defaultMainWithHooks defaultUserHooks
