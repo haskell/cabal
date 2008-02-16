bff23eed32849f4501d94a791de197bd45c676d8
shelarcy <shelarcy@gmail.com>
2008-02-16 06:24:33 +0000

    Fix: Distribution.Make's configure doesn't work on Windows.

--- Distribution/Make.hs
+++ Distribution/Make.hs
@@ -149,8 +149,9 @@ configureAction :: ConfigFlags -> [String] -> IO ()
 configureAction flags args = do
   no_extra_flags args
   let verbosity = fromFlag (configVerbose flags)
-  rawSystemExit verbosity "./configure"
-    (configureArgs backwardsCompatHack flags)
+  rawSystemExit verbosity "sh" $
+    "configure"
+    : configureArgs backwardsCompatHack flags
   where backwardsCompatHack = True
 
 copyAction :: CopyFlags -> [String] -> IO ()
