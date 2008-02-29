b63a3f7b6516a281467b6e9748376d34df6cb6c4
Duncan Coutts <duncan@haskell.org>
2008-02-29 14:03:00 +0000

    Remove 'info' command, make --dry-run cover that feature In -v mode info produced a detailed tree explaining the dependency resolution. That feature has been moved into 'install --dry-run -v'.

--- cabal-install/Hackage/Info.hs
+++ cabal-install/Hackage/Info.hs
@@ -10,52 +10,19 @@
 --
 -- High level interface to a dry-run package installation.
 -----------------------------------------------------------------------------
-module Hackage.Info where
+module Hackage.Info (
+    flattenResolvedDependencies,
+    infoPkg
+  ) where
 
-import qualified Hackage.IndexUtils as IndexUtils
-import qualified Hackage.DepGraph as DepGraph
-import Hackage.Dependency 
 import Hackage.Fetch
 import Hackage.Types 
 import Hackage.Utils (showDependencies)
 
 import Distribution.Package (showPackageId, Package(..))
 import Distribution.ParseUtils (showDependency)
-import Distribution.Simple.Compiler (Compiler, PackageDB)
-import Distribution.Simple.Configure (getInstalledPackages)
-import Distribution.Simple.Program (ProgramConfiguration)
-import Distribution.Simple.Utils as Utils (notice, info)
-import Distribution.Verbosity (Verbosity)
 
 import Data.List (nubBy)
-import Data.Monoid (Monoid(mconcat))
-
-info :: Verbosity
-     -> PackageDB
-     -> [Repo]
-     -> Compiler
-     -> ProgramConfiguration
-     -> [UnresolvedDependency]
-     -> IO ()
-info verbosity packageDB repos comp conf deps
-    = do Just installed <- getInstalledPackages verbosity comp packageDB conf
-         available <- fmap mconcat (mapM (IndexUtils.readRepoIndex verbosity) repos)
-         deps' <- IndexUtils.disambiguateDependencies available deps
-         let apkgs = resolveDependencies comp installed available deps'
-         details <- mapM infoPkg (flattenResolvedDependencies apkgs)
-         Utils.info verbosity $ unlines (map ("  "++) (concat details))
-         case packagesToInstall apkgs of
-           Left missing -> notice verbosity $
-                "The requested packages cannot be installed, because of missing dependencies:\n"
-             ++ showDependencies missing
-
-           Right pkgs | DepGraph.empty pkgs -> notice verbosity $
-                "All requested packages already installed. Nothing to do."
-
-           Right pkgs -> notice verbosity $
-               "These packages would be installed:\n"
-             ++ unlines [ showPackageId (packageId pkg)
-                        | (DepGraph.ResolvedPackage pkg _ _) <- DepGraph.toList pkgs]
 
 flattenResolvedDependencies :: [ResolvedDependency] -> [ResolvedDependency]
 flattenResolvedDependencies = nubBy fulfillSame. concatMap flatten
