4372f51cd4ad359a72c2b243bc7532dab8c3dc62
Duncan Coutts <duncan@haskell.org>
2008-03-03 01:03:46 +0000

    In 'list', also include packages that are only installed and not available Previously we took the available packages and did an inner join with the installed packages so we missed out packages like base which are installed by not available. Now we get the selected installed and available package sets and do a full outer join using a sort and merge operation. Also use a proper data type to hold the info we print about each package.

--- cabal-install/Hackage/List.hs
+++ cabal-install/Hackage/List.hs
@@ -14,28 +14,29 @@ module Hackage.List (
   list
   ) where
 
-import Data.List (sortBy, groupBy)
-import Data.Maybe (catMaybes)
-import Data.Monoid (Monoid(mconcat,mempty))
-import Data.Map as Map (Map)
-import qualified Data.Map as Map
+import Data.List (sortBy, groupBy, sort, nub)
+import Data.Maybe (catMaybes, listToMaybe, fromJust)
+import Data.Monoid (Monoid(mconcat))
+import Control.Monad (MonadPlus(mplus))
+import Control.Exception (assert)
 
 import Distribution.Package (PackageIdentifier(..), Package(..))
-import Distribution.PackageDescription as Cabal
+import Distribution.License (License)
+import qualified Distribution.PackageDescription as Available
+import Distribution.InstalledPackageInfo (InstalledPackageInfo)
+import qualified Distribution.InstalledPackageInfo as Installed
 import qualified Distribution.Simple.PackageIndex as PackageIndex
-import Distribution.Version (Version,showVersion)
+import Distribution.Version (Version, showVersion)
 import Distribution.Verbosity (Verbosity)
 
-import qualified Hackage.IndexUtils as IndexUtils
+import qualified Hackage.IndexUtils as IndexUtils (readRepoIndex)
 import Hackage.Setup (ListFlags(..))
 import Hackage.Types (PkgInfo(..), Repo)
-import Distribution.Simple.Configure as Cabal (getInstalledPackages)
-import Distribution.Simple.Compiler as Cabal (Compiler,PackageDB)
-import Distribution.Simple.PackageIndex as Installed
-import Distribution.Simple.Program as Cabal (ProgramConfiguration)
-import Distribution.Simple.Utils (equating, comparing, lowercase, notice)
+import Distribution.Simple.Configure (getInstalledPackages)
+import Distribution.Simple.Compiler (Compiler,PackageDB)
+import Distribution.Simple.Program (ProgramConfiguration)
+import Distribution.Simple.Utils (equating, comparing, notice, intercalate)
 import Distribution.Simple.Setup (fromFlag)
-import Distribution.InstalledPackageInfo as Installed
 
 -- |Show information about packages
 list :: Verbosity
@@ -47,90 +48,139 @@ list :: Verbosity
      -> [String]
      -> IO ()
 list verbosity packageDB repos comp conf listFlags pats = do
-    indexes <- mapM (IndexUtils.readRepoIndex verbosity) repos
-    let index = mconcat indexes
-        pkgs | null pats = PackageIndex.allPackages index
+    Just installed <- getInstalledPackages verbosity comp packageDB conf
+    available <- fmap mconcat (mapM (IndexUtils.readRepoIndex verbosity) repos)
+    let pkgs | null pats = (PackageIndex.allPackages installed
+                           ,PackageIndex.allPackages available)
              | otherwise =
-                 concatMap (PackageIndex.searchByNameSubstring index) pats
+                 (concatMap (PackageIndex.searchByNameSubstring installed) pats
+                 ,concatMap (PackageIndex.searchByNameSubstring available) pats)
+        matches = installedFilter
+                . map (uncurry mergePackageInfo)
+                $ uncurry mergePackages pkgs
 
-    instPkgs <- do
-        pkgsM <- Cabal.getInstalledPackages verbosity comp packageDB conf
-        case pkgsM of
-            Nothing -> do
-                notice verbosity "Unable to read list of installed packages."
-                notice verbosity $ "Your compiler (" ++ show comp ++ ") is not supported."
-                notice verbosity "Pretending no packages are installed."
-                return mempty
-            Just ps ->
-                return
-                    . Map.fromList $
-                    [ (name, versions)
-                    | installed <- allPackagesByName ps
-                    , let name = pkgName . packageId . Installed.package . head $ installed
-                    , let versions =
-                            [ pkgVersion . packageId . Installed.package $ i
-                            | i <- installed
-                            ]
-                    ]
-    let matches =
-          installedFilter instPkgs
-          . groupBy (equating (pkgName . packageId))
-          . sortBy (comparing nameAndVersion)
-          $ pkgs
-
-    if fromFlag (listSimpleOutput listFlags)
-      then putStr . unlines . map (simpleShowPackageId . packageId) . concat $ matches
+    if simpleOutput
+      then putStr $ unlines
+             [ name pkg ++ " " ++ showVersion version
+             | pkg <- matches
+             , version <- if onlyInstalled
+                            then              installedVersions pkg
+                            else nub . sort $ installedVersions pkg
+                                           ++ availableVersions pkg ]
       else
         if null matches
             then notice verbosity "No mathes found."
-            else putStr . unlines . map (showPkgVersions instPkgs) $ matches
-
-   where
-    nameAndVersion p = (lowercase name, name, version)
-        where name = pkgName (packageId p)
-              version = pkgVersion (packageId p)
-    simpleShowPackageId :: PackageIdentifier -> String
-    simpleShowPackageId pkg = pkgName pkg ++ " " ++ showVersion (pkgVersion pkg)
-    installedFilter :: Map String [Version] -> [[PkgInfo]] -> [[PkgInfo]]
-    installedFilter installed pkgs
-        | fromFlag (listInstalled listFlags) =
-            [ matches
-            | pkg <- pkgs
-            , let installedVersions =
-                    maybe [] id
-                        (Map.lookup
-                            (pkgName . packageId . head $ pkg)
-                            installed)
-            , let matches =
-                    [ cab
-                    | cab <- pkg
-                    , any (\i -> (pkgVersion . packageId) cab == i)
-                          installedVersions
-                    ]
-            , not (null matches)
-            ]
-        | otherwise = pkgs
-
-showPkgVersions :: Map String [Version] -> [PkgInfo] -> String
-showPkgVersions installedPkgs pkgs = unlines $
-    [ " * " ++ name ] ++ map (indent 6) (catMaybes [
-      -- does it matter which repo the package came from?
-      -- compare to gentoo overlays
-      p $ "Latest version available: " ++ showVersion (pkgVersion (packageId pkg))
-    , installedVersion >>= return . ("Latest version installed: " ++) . showVersion
-    -- TODO: add size of required downloads? need hackage support for this
-    , s (Cabal.homepage pd) ("Homepage: " ++)
-    , s (Cabal.category pd) ("Category: " ++)
-    , p $ "Synopsis: " ++ synopsis (packageDescription (pkgDesc pkg))
-    , p $ "License:  " ++ show (Cabal.license pd)
-    ])
+            else notice verbosity $
+                   intercalate "\n" (map showPackageInfo matches)
   where
-    p = Just
-    s str f
-        | not (null str) = Just (f str)
-        | otherwise = Nothing
+    installedFilter
+      | onlyInstalled = filter (not . null . installedVersions)
+      | otherwise     = id
+    onlyInstalled = fromFlag (listInstalled listFlags)
+    simpleOutput  = fromFlag (listSimpleOutput listFlags)
+
+-- | The info that we can display for each package. It is information per
+-- package name and covers all installed and avilable versions.
+--
+data PackageDisplayInfo = PackageDisplayInfo {
+    name              :: String,
+    installedVersions :: [Version],
+    availableVersions :: [Version],
+    homepage          :: String,
+    category          :: String,
+    synopsis          :: String,
+    license           :: License
+  }
+
+showPackageInfo :: PackageDisplayInfo -> String
+showPackageInfo pkg =
+  unlines $
+     [" * " ++ name pkg]
+  ++ (map (indent 6) . catMaybes) [
+       maybeNull (availableVersions pkg) $
+       "Latest version available: "
+    ++ showVersion (maximum (availableVersions pkg))
+     , maybeNull (installedVersions pkg) $
+       "Latest version installed: "
+    ++ showVersion (maximum (installedVersions pkg))
+     , maybeNull (homepage pkg) $
+       "Homepage: " ++ homepage pkg
+     , maybeNull (category pkg) $
+       "Category: " ++ category pkg
+     , maybeNull (synopsis pkg) $
+       "Synopsis: " ++ synopsis pkg
+     , Just $
+       "License:  " ++ show (license pkg)
+     ]
+  where
     indent n str = replicate n ' ' ++ str
-    pd = packageDescription (pkgDesc pkg)
-    name = pkgName (packageId pkg)
-    pkg = last pkgs
-    installedVersion = fmap maximum $ Map.lookup name installedPkgs
+    maybeNull [] _ = Nothing
+    maybeNull _  s = Just s
+
+-- | We get the 'PackageDisplayInfo' by combining the info for the installed
+-- and available versions of a package.
+--
+-- * We're building info about a various versions of a single named package so
+-- the input package info records are all supposed to refer to the same
+-- package name.
+--
+mergePackageInfo :: [InstalledPackageInfo]
+                 -> [PkgInfo]
+                 -> PackageDisplayInfo
+mergePackageInfo installed available =
+  assert (length installed + length available > 0) $
+  PackageDisplayInfo {
+    name              = combine (pkgName . packageId) latestAvailable
+                                (pkgName . packageId) latestInstalled,
+    installedVersions = map (pkgVersion . packageId) installed,
+    availableVersions = map (pkgVersion . packageId) available,
+    homepage          = combine Available.homepage latestAvailableDesc
+                                Installed.homepage latestInstalled,
+    category          = combine Available.category latestAvailableDesc
+                                Installed.category latestInstalled,
+    synopsis          = combine Available.synopsis latestAvailableDesc
+                                Installed.description latestInstalled,
+    license           = combine Available.license  latestAvailableDesc
+                                Installed.license latestInstalled
+  }
+  where
+    combine f x g y = fromJust (fmap f x `mplus` fmap g y)
+    latestInstalled = latestOf installed
+    latestAvailable = latestOf available
+    latestAvailableDesc = fmap (Available.packageDescription . pkgDesc)
+                          latestAvailable
+    latestOf :: Package pkg => [pkg] -> Maybe pkg
+    latestOf = listToMaybe . sortBy (comparing (pkgVersion . packageId))
+
+-- | Rearrange installed and available packages into groups referring to the
+-- same package by name. In the result pairs, the lists are guaranteed to not
+-- both be empty.
+--
+mergePackages ::   [InstalledPackageInfo] -> [PkgInfo]
+              -> [([InstalledPackageInfo],   [PkgInfo])]
+mergePackages installed available =
+    map (\(is, as) -> (maybe [] snd is
+                    ,maybe [] snd as))
+  $ mergeBy (\i a -> fst i `compare` fst a)
+            (groupOn (pkgName . packageId) installed)
+            (groupOn (pkgName . packageId) available)
+
+groupOn :: Ord key => (a -> key) -> [a] -> [(key,[a])]
+groupOn key = map (\xs -> (key (head xs), xs))
+            . groupBy (equating key)
+            . sortBy (comparing key)
+
+-- | Generic merging utility. For sorted input lists this is a full outer join.
+--
+-- * The result list never contains @(Nothing, Nothing)@.
+--
+mergeBy :: (a -> b -> Ordering) -> [a] -> [b] -> [(Maybe a, Maybe b)]
+mergeBy cmp = merge
+  where
+    merge []     ys     = [ (Nothing, Just y) | y <- ys]
+    merge xs     []     = [ (Just x, Nothing) | x <- xs]
+    merge (x:xs) (y:ys) =
+      case x `cmp` y of
+        GT -> (Nothing, Just y) : merge (x:xs) ys
+        EQ -> (Just x,  Just y) : merge xs     ys
+        LT -> (Just x, Nothing) : merge xs  (y:ys)
