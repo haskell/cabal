7bbbe5961e73c3f3608f2fdda83c6803f5c111ab
Pepe Iborra <mnislaih@gmail.com>
2008-03-17 20:30:15 +0000

    #223 part 1: Extend Distribution.Command.Simple.Option      so that it really represents an option and not just a flag.      It's been renamed to OptionField as it models a field in a flags-like data structure.
            data OptionField a = OptionField {
              optionName        :: Name,
              optionDescr       :: [OptDescr a] }
          
            data OptDescr a  = ReqArg Description OptFlags ArgDescr (ReadE (a->a))         (a -> [String])
                             | OptArg Description OptFlags ArgDescr (ReadE (a->a)) (a->a)  (a -> [Maybe String])
                             | ChoiceOpt [(Description, OptFlags, a->a, a -> Bool)]
    			 | BoolOpt Description OptFlags{-True-} OptFlags{-False-} (Bool -> a -> a) (a -> Bool)
          
          An option field can expand to several command line options, which are all defined together.
          For example, the compiler flag is defined as follows.
          
                option [] ["compiler"] "compiler"
                   configHcFlavor (\v flags -> flags { configHcFlavor = v })
                   (choiceOpt [ (Flag GHC, ("g", ["ghc"]), "compile with GHC")
                              , (Flag NHC, ([] , ["nhc98"]), "compile with NHC")
                              , (Flag JHC, ([] , ["jhc"]), "compile with JHC")
                              , (Flag Hugs,([] , ["hugs"]), "compile with Hugs")])
          
          We can need to use several kinds of OptDescr for the same option, as in the 
          optimization Option (really a extreme case):
          
                ,multiOption "optimization"
                   configOptimization (\v flags -> flags { configOptimization = v })
                   [optArg' "n" (Flag . flagToOptimisationLevel)
                    ....
                    ....
                            "Build with optimization (n is 0--2, default is 1)",
                    noArg (Flag NoOptimisation) []

---
+++ Distribution/ReadE.hs
@@ -0,0 +1,96 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Distribution.ReadE
+-- Copyright   :  Jose Iborra 2008
+--
+-- Maintainer  :  Isaac Jones <ijones@syntaxpolice.org>
+-- Stability   :  alpha
+-- Portability :  portable
+--
+-- Simple parsing with failure
+
+{- Copyright (c) 2007, Jose Iborra
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of Isaac Jones nor the names of other
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. -}
+
+module Distribution.ReadE (
+   -- * ReadE
+   ReadE(..), succeedReadE, failReadE,
+   -- * Projections
+   readS_to_E, readS_to_E', readP_to_E, readP_to_E',
+   parseReadE, readEOrFail,
+  ) where
+
+import Data.Either (either)
+import Distribution.Compat.ReadP
+
+-- | Parser with simple error reporting
+newtype ReadE a = ReadE {runReadE :: String -> Either ErrorMsg a}
+type ErrorMsg   = String
+
+instance Functor ReadE where
+  fmap f (ReadE p) = ReadE $ \txt -> case p txt of
+                                       Right a  -> Right (f a)
+                                       Left err -> Left err
+
+succeedReadE :: (String -> a) -> ReadE a
+succeedReadE f = ReadE (Right . f)
+
+failReadE :: ErrorMsg -> ReadE a
+failReadE = ReadE . const Left
+
+readS_to_E :: (String -> ErrorMsg) -> ReadS a -> ReadE a
+readS_to_E err r = ReadE$ \txt -> case r txt of
+                           [(a,[])] -> Right a
+                           _        -> Left (err txt)
+
+readS_to_E' :: String -> String -> ReadS a -> ReadE a
+readS_to_E' name arg_desc r =
+    ReadE $ \txt -> case r txt of
+              [(a,[])] -> Right a
+              _ -> error (concat ["Failed to parse ", name,
+                                  ". Expected " ++ arg_desc,
+                                  ", found " ++ show txt])
+
+readP_to_E :: (String -> ErrorMsg) -> ReadP a a -> ReadE a
+readP_to_E err = readS_to_E err . readP_to_S
+
+readP_to_E' :: String -> String -> ReadP a a -> ReadE a
+readP_to_E' name arg_descr = readS_to_E' name arg_descr . readP_to_S
+
+
+parseReadE :: ReadE a -> ReadP r a
+parseReadE (ReadE p) = do
+  txt <- look
+  either fail return (p txt)
+
+readEOrFail :: ReadE a -> (String -> a)
+readEOrFail r = either error id . runReadE r
+
