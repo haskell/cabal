d13264ff03bc79b8102d43801bc48ba6811b418b
Duncan Coutts <duncan@haskell.org>
2008-03-27 00:53:21 +0000

    Separate the OS/Arch classifiation used for different purposes We have to classify System.Info.{os,arch} strings into the OS and Arch enums. For that purpose we have to be quite permissive since there are lots of ailases that the various Haskell implementations use. However for parsing os and arch names in .cabal files we'd prefer to use canonical names, though we do have to allow the couple aliases already in common use.

--- Distribution/System.hs
+++ Distribution/System.hs
@@ -15,6 +15,25 @@ import Distribution.Text (Text(..), display)
 import qualified Distribution.Compat.ReadP as Parse
 import qualified Text.PrettyPrint as Disp
 
+-- | How strict to be when classifying strings into the 'OS' and 'Arch' enums.
+--
+-- The reason we have multiple ways to do the classification is because there
+-- are two situations where we need to do it.
+--
+-- For parsing os and arch names in .cabal files we really want everyone to be
+-- referring to the same or or arch by the same name. Variety is not a virtue
+-- in this case. We don't mind about case though.
+--
+-- For the System.Info.os\/arch different Haskell implementations use different
+-- names for the same or\/arch. Also they tend to distinguish versions of an
+-- os\/arch which we just don't care about.
+--
+-- The 'Compat' classification allows us to recognise aliases that are already
+-- in common use but it allows us to distinguish them from the canonical name
+-- which enables us to warn about such deprecated aliases.
+--
+data ClassificationStrictness = Permissive | Compat | Strict
+
 -- ------------------------------------------------------------
 -- * Operating System
 -- ------------------------------------------------------------
@@ -30,31 +49,32 @@ knownOSs = [Linux, Windows, OSX
            ,FreeBSD, OpenBSD, NetBSD
            ,Solaris, AIX, HPUX, IRIX]
 
-osAliases :: OS -> [String]
-osAliases Windows     = ["mingw32", "cygwin32"]
-osAliases OSX         = ["darwin"]
-osAliases FreeBSD     = ["kfreebsdgnu"]
-osAliases Solaris     = ["solaris2"]
-osAliases _           = []
+osAliases :: ClassificationStrictness -> OS -> [String]
+osAliases Permissive Windows = ["mingw32", "cygwin32"]
+osAliases Compat     Windows = ["mingw32", "win32"]
+osAliases _          OSX     = ["darwin"]
+osAliases Permissive FreeBSD = ["kfreebsdgnu"]
+osAliases Permissive Solaris = ["solaris2"]
+osAliases _          _       = []
 
 instance Text OS where
   disp (OtherOS name) = Disp.text name
   disp other          = Disp.text (lowercase (show other))
 
-  parse = fmap classifyOS ident
+  parse = fmap (classifyOS Compat) ident
 
-classifyOS :: String -> OS
-classifyOS s =
+classifyOS :: ClassificationStrictness -> String -> OS
+classifyOS strictness s =
   case lookup (lowercase s) osMap of
     Just os -> os
     Nothing -> OtherOS s
   where
     osMap = [ (name, os)
             | os <- knownOSs
-            , name <- display os : osAliases os ]
+            , name <- display os : osAliases strictness os ]
 
 buildOS :: OS
-buildOS = classifyOS System.Info.os
+buildOS = classifyOS Permissive System.Info.os
 
 -- ------------------------------------------------------------
 -- * Machine Architecture
@@ -75,32 +95,34 @@ knownArches = [I386, X86_64, PPC, PPC64, Sparc
               ,Alpha, Hppa, Rs6000
               ,M68k, Vax]
 
-archAliases :: Arch -> [String]
-archAliases PPC   = ["powerpc"]
-archAliases PPC64 = ["powerpc64"]
-archAliases Sparc = ["sparc64"]
-archAliases Mips  = ["mipsel", "mipseb"]
-archAliases Arm   = ["armeb", "armel"]
-archAliases _     = []
+archAliases :: ClassificationStrictness -> Arch -> [String]
+archAliases Strict _     = []
+archAliases Compat _     = []
+archAliases _      PPC   = ["powerpc"]
+archAliases _      PPC64 = ["powerpc64"]
+archAliases _      Sparc = ["sparc64", "sun4"]
+archAliases _      Mips  = ["mipsel", "mipseb"]
+archAliases _      Arm   = ["armeb", "armel"]
+archAliases _      _     = []
 
 instance Text Arch where
   disp (OtherArch name) = Disp.text name
   disp other            = Disp.text (lowercase (show other))
 
-  parse = fmap classifyArch ident
+  parse = fmap (classifyArch Strict) ident
 
-classifyArch :: String -> Arch
-classifyArch s =
+classifyArch :: ClassificationStrictness -> String -> Arch
+classifyArch strictness s =
   case lookup (lowercase s) archMap of
     Just arch -> arch
     Nothing   -> OtherArch s
   where
     archMap = [ (name, arch)
               | arch <- knownArches
-              , name <- display arch : archAliases arch ]
+              , name <- display arch : archAliases strictness arch ]
 
 buildArch :: Arch
-buildArch = classifyArch System.Info.arch
+buildArch = classifyArch Permissive System.Info.arch
 
 ident :: Parse.ReadP r String
 ident = Parse.munch1 (\c -> Char.isAlphaNum c || c == '_' || c == '-')
