c36a533498276c405625bfa540bdc70f9ac0646d
Duncan Coutts <duncan@haskell.org>
2008-05-04 19:45:30 +0000

    Add new utils, duplicates, duplicatesBy and mergeBy

--- cabal-install/Hackage/Utils.hs
+++ cabal-install/Hackage/Utils.hs
@@ -6,6 +6,8 @@ import Distribution.Text
          ( display )
 import Distribution.Simple.Utils (intercalate)
 
+import Data.List
+         ( sortBy, groupBy )
 import Control.Monad (guard)
 import Control.Exception (Exception, catchJust, ioErrors)
 import System.IO.Error (isDoesNotExistError)
@@ -22,3 +24,32 @@ fileNotFoundExceptions e =
 
 showDependencies :: [Dependency] -> String
 showDependencies = intercalate ", " . map display
+
+-- | Generic merging utility. For sorted input lists this is a full outer join.
+--
+-- * The result list never contains @(Nothing, Nothing)@.
+--
+mergeBy :: (a -> b -> Ordering) -> [a] -> [b] -> [MergeResult a b]
+mergeBy cmp = merge
+  where
+    merge []     ys     = [ OnlyInRight y | y <- ys]
+    merge xs     []     = [ OnlyInLeft  x | x <- xs]
+    merge (x:xs) (y:ys) =
+      case x `cmp` y of
+        GT -> OnlyInRight   y : merge (x:xs) ys
+        EQ -> InBoth      x y : merge xs     ys
+        LT -> OnlyInLeft  x   : merge xs  (y:ys)
+
+data MergeResult a b = OnlyInLeft a | InBoth a b | OnlyInRight b
+
+duplicates :: Ord a => [a] -> [[a]]
+duplicates = duplicatesBy compare
+
+duplicatesBy :: (a -> a -> Ordering) -> [a] -> [[a]]
+duplicatesBy cmp = filter moreThanOne . groupBy eq . sortBy cmp
+  where
+    eq a b = case cmp a b of
+               EQ -> True
+               _  -> False
+    moreThanOne (_:_:_) = True
+    moreThanOne _       = False
