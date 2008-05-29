726dcfdf9ad31eeeb37c4ceb470ddbbe291c8731
Duncan Coutts <duncan@haskell.org>
2008-05-29 09:22:44 +0000

    Add notThisVersion :: Version -> VersionRange Opposite of ThisVersion, it means /= x.y but is actually implemented as > x.y || < x.y as we do not have not or not equal as primitives.

--- Distribution/Version.hs
+++ Distribution/Version.hs
@@ -45,7 +45,7 @@ module Distribution.Version (
   Version(..),
 
   -- * Version ranges
-  VersionRange(..), 
+  VersionRange(..), notThisVersion,
   orLaterVersion, orEarlierVersion,
   betweenVersionsInclusive,
   withinRange,
@@ -83,6 +83,9 @@ data VersionRange
 isAnyVersion :: VersionRange -> Bool
 isAnyVersion AnyVersion = True
 isAnyVersion _ = False
+
+notThisVersion :: Version -> VersionRange
+notThisVersion v = UnionVersionRanges (EarlierVersion v) (LaterVersion v)
 
 orLaterVersion :: Version -> VersionRange
 orLaterVersion   v = UnionVersionRanges (ThisVersion v) (LaterVersion v)
