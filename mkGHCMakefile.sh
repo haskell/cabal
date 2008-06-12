59f2b77eb2a73988a4aa5e2e76c30c133a30a3da
Duncan Coutts <duncan@haskell.org>
2008-06-12 19:03:17 +0000

    Move the mkGHCMakefile.sh out of the root dir Having it there confuses people. They think they have to run it as part of the install process.

--- mkGHCMakefile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-file=Distribution/Simple/GHC/Makefile.hs
-echo "-- DO NOT EDIT: change Makefile.in, and run ../../../mkGHCMakefile.sh" >$file
-echo "module Distribution.Simple.GHC.Makefile where {" >>$file
-echo "makefileTemplate :: String; makefileTemplate=unlines" >>$file
-ghc -e "readFile \"Distribution/Simple/GHC/Makefile.in\" >>= print . lines" >>$file
-echo "}" >>$file
