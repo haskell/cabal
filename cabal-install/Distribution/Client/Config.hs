2cfad55c3529c88f46c8fdc37029d28d40022e2f
Duncan Coutts <duncan@haskell.org>
2008-08-15 14:27:35 +0000

    allow tilde in hackage server URIs report and fix from Nicolas Frisby <nicolas.frisby@gmail.com> (code had changed slightly so the original patch didn't apply cleanly)

--- cabal-install/Distribution/Client/Config.hs
+++ cabal-install/Distribution/Client/Config.hs
@@ -296,7 +296,7 @@ parseRepo :: Parse.ReadP r RemoteRepo
 parseRepo = do name <- Parse.munch1 (\c -> isAlphaNum c || c `elem` "_-.")
                Parse.char ':'
                uriStr <- Parse.munch1 (\c -> isAlphaNum c
-                                          || c `elem` "+-=._/*()@'$:;&!?")
+                                          || c `elem` "+-=._/*()@'$:;&!?~")
                uri <- maybe Parse.pfail return (parseAbsoluteURI uriStr)
                return $ RemoteRepo {
                  remoteRepoName = name,
