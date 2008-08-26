bb96e428ae5c5b9c9dc0581b9b9f18f455ca89af
Duncan Coutts <duncan@haskell.org>
2008-08-26 21:09:23 +0000

    Fix up the bootstrap.sh and include it in the tarball

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@
 # HTTP packages. It then installs cabal-install itself.
 # It expects to be run inside the cabal-install directory.
 
-CABAL_VER="1.4.0.1"
+CABAL_VER="1.4.0.2"
 HTTP_VER="3001.0.4"
 ZLIB_VER="0.4.0.4"
 
@@ -17,19 +17,19 @@ ZLIB_URL=${HACKAGE_URL}/zlib/${ZLIB_VER}/zlib-${ZLIB_VER}.tar.gz
 
 wget ${CABAL_URL} ${HTTP_URL} ${ZLIB_URL}
 
-tar -zxf Cabal-1.4.0.1.tar.gz
-pushd Cabal-1.4.0.1
+tar -zxf Cabal-${CABAL_VER}.tar.gz
+pushd Cabal-${CABAL_VER}
 ghc --make Setup
 ./Setup configure --user && ./Setup build && ./Setup install
 popd
 
-tar -zxf HTTP-3001.0.4.tar.gz
-pushd HTTP-3001.0.4
+tar -zxf HTTP-${HTTP_VER}.tar.gz
+pushd HTTP-${HTTP_VER}
 runghc Setup configure --user && runghc Setup build && runghc Setup install
 popd
 
-tar -zxf zlib-0.4.0.4.tar.gz
-pushd zlib-0.4.0.4
+tar -zxf zlib-${ZLIB_VER}.tar.gz
+pushd zlib-${ZLIB_VER}
 runghc Setup configure --user && runghc Setup build && runghc Setup install
 popd
 
