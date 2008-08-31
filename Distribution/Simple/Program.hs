f738058de54076b853c086258b01d357917a993c
Duncan Coutts <duncan@haskell.org>
2008-08-31 21:55:51 +0000

    Simplify the handling of --with-prog= in build/haddock commands. We allow extra rgs and the location of programs to be given to the build and haddock commands, not just at configure time. The code to do this is now simpler and more general. This should not be the default use mode however since it involves configuring the programs each time where as doing it at configure time allows it to be done once and saved. Further, specifying a different version of the program at build time than at configure time is likely to fail, especially for the compiler programs. Changing the compiler really requires reconfiguring.

--- Distribution/Simple/Program.hs
+++ Distribution/Simple/Program.hs
@@ -48,7 +48,6 @@ module Distribution.Simple.Program (
     , rawSystemProgramStdout
 
     -- * The collection of unconfigured and configured progams
-    , findProgram
     , builtinPrograms
 
     -- * The collection of configured programs we can run
@@ -528,13 +527,6 @@ rawSystemProgramStdoutConf verbosity prog programConf extraArgs =
 -- ------------------------------------------------------------
 -- * Known programs
 -- ------------------------------------------------------------
-
-findProgram :: String -> Maybe Program
-findProgram progname
-    = case filter ((progname ==) . programName) builtinPrograms of
-      [] -> Nothing
-      [p] -> Just p
-      _ -> error "findProgram: Found more than one program"
 
 -- | The default list of programs.
 -- These programs are typically used internally to Cabal.
