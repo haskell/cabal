c728889a1ad4860513523fbeb916a4152232d0d5
Clemens Fruhwirth <clemens@endorphin.org>
2008-10-08 15:44:10 +0000

    Also respect the max. command line size in Makefile driven builds

--- Distribution/Simple/GHC.hs
+++ Distribution/Simple/GHC.hs
@@ -584,12 +584,7 @@ build pkg_descr lbi verbosity = do
 
             runAr = rawSystemProgramConf verbosity arProgram (withPrograms lbi)
 
-             --TODO: discover this at configure time or runtime on unix
-             -- The value is 32k on Windows and posix specifies a minimum of 4k
-             -- but all sensible unixes use more than 4k.
-             -- we could use getSysVar ArgumentLimit but that's in the unix lib
-            maxCommandLineSize = 30 * 1024
-
+        maxCommandLineSize <- discoverMaxCommandLineSize
         ifVanillaLib False $ xargs maxCommandLineSize
           runAr arArgs arObjArgs
 
@@ -756,6 +751,14 @@ ghcCcOptions lbi bi odir
 mkGHCiLibName :: PackageIdentifier -> String
 mkGHCiLibName lib = "HS" ++ display lib <.> "o"
 
+
+--TODO: discover this at configure time or runtime on unix
+-- The value is 32k on Windows and posix specifies a minimum of 4k
+-- but all sensible unixes use more than 4k.
+-- we could use getSysVar ArgumentLimit but that's in the unix lib
+discoverMaxCommandLineSize :: IO Int
+discoverMaxCommandLineSize = return (30 * 1024)
+
 -- -----------------------------------------------------------------------------
 -- Building a Makefile
 
@@ -779,6 +782,7 @@ makefile pkg_descr lbi flags = do
   let builddir = buildDir lbi
       Just ghcProg = lookupProgram ghcProgram (withPrograms lbi)
       Just ghcVersion = programVersion ghcProg
+  maxCommandLineSize <- discoverMaxCommandLineSize
   let decls = [
         ("modules", unwords (map display (PD.exposedModules lib ++ otherModules bi))),
         ("GHC", programPath ghcProg),
@@ -805,7 +809,9 @@ makefile pkg_descr lbi flags = do
         ("LD", programPath ldProg ++ concat [" " ++ arg | arg <- programArgs ldProg ]),
         ("GENERATE_DOT_DEPEND", if ghcVersion >= Version [6,9] []
                                 then "-dep-makefile $(odir)/.depend"
-                                else "-optdep-f -optdep$(odir)/.depend")
+                                else "-optdep-f -optdep$(odir)/.depend"),
+        ("MAX_COMMAND_LINE_SIZE", show maxCommandLineSize)
+
         ]
       mkRules srcdir = [
         "$(odir_)%.$(osuf) : " ++ srcdir ++ "/%.hs",
