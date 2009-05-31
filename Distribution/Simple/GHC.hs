816805f2f066ff5a24ef162957c5feb9bf9250d6
Duncan Coutts <duncan@haskell.org>
2009-05-31 19:25:45 +0000

    Make rendering the ghc package db stack more lenient Allow the user package db to appear after a specific one. No reason not to and makes some things in cabal-install more convenient.

--- Distribution/Simple/GHC.hs
+++ Distribution/Simple/GHC.hs
@@ -769,14 +769,15 @@ ghcOptions lbi bi clbi odir
 
 ghcPackageDbOptions :: PackageDBStack -> [String]
 ghcPackageDbOptions dbstack = case dbstack of
-  (GlobalPackageDB:UserPackageDB:dbs) -> concatMap specific dbs
-  (GlobalPackageDB:dbs)               -> "-no-user-package-conf"
-                                       : concatMap specific dbs
-  _                                   -> ierror
+  (GlobalPackageDB:dbs)
+    | UserPackageDB `elem` dbs -> concatMap specific dbs
+    | otherwise                -> "-no-user-package-conf"
+                                : concatMap specific dbs
+  _                            -> ierror
   where
     specific (SpecificPackageDB db) = [ "-package-conf", db ]
-    specific _ = ierror
-    ierror     = error "internal error: unexpected package db stack"
+    specific _                      = []
+    ierror = error "internal error: unexpected package db stack"
 
 constructCcCmdLine :: LocalBuildInfo -> BuildInfo -> ComponentLocalBuildInfo
                    -> FilePath -> FilePath -> Verbosity -> (FilePath,[String])
