f4496ccab8d0442124e5a9a6eb9c3343697dd7ab
Stephen Blackheath <grossly.sensitive.stephen@blacksapphire.com>
2009-10-05 16:32:02 +0000

    Fix test case InternalLibrary4 on account of a change in Cabal's behaviour.

--- tests/PackageTests/BuildDeps/InternalLibrary4/Check.hs
+++ tests/PackageTests/BuildDeps/InternalLibrary4/Check.hs
@@ -3,7 +3,7 @@ module PackageTests.BuildDeps.InternalLibrary4.Check where
 import Test.HUnit
 import PackageTests.PackageTester
 import System.FilePath
-import qualified Data.ByteString.Char8 as C
+import Data.List
 
 
 suite :: Test
@@ -15,10 +15,9 @@ suite = TestCase $ do
     iResult <- cabal_install specTI                     
     assertEqual "cabal install should succeed - see to-install/test-log.txt" True (successful iResult)
     bResult <- cabal_build spec
-    assertEqual "cabal build should succeed - see test-log.txt" True (successful bResult)
+    assertEqual "cabal build should fail - see test-log.txt" False (successful bResult)
     unregister "InternalLibrary4"
 
-    (_, _, output) <- run (Just $ directory spec) "dist/build/lemon/lemon" []
-    C.appendFile (directory spec </> "test-log.txt") (C.pack $ "\ndist/build/lemon/lemon\n"++output)
-    assertEqual "executable should have linked with the installed library" "myLibFunc installed" (concat $ lines output)
+    assertEqual "error should say 'cannot satisfy dependency InternalLibrary4 >=0.2'" True $
+        "cannot satisfy dependency InternalLibrary4 >=0.2" `isInfixOf` (intercalate " " $ lines $ outputText bResult)
 
