c9ed01b099c2f215a98c7ce1bb3536c25b3bcbae
Sergei Trofimovich <slyfox@community.haskell.org>
2009-12-20 21:44:48 +0000

    Distribution/Client/Setup.hs: suppress warning (unused variable)

--- cabal-install/Distribution/Client/Setup.hs
+++ cabal-install/Distribution/Client/Setup.hs
@@ -867,7 +867,7 @@ readRepo = readPToMaybe parseRepo
 parseRepo :: Parse.ReadP r RemoteRepo
 parseRepo = do
   name <- Parse.munch1 (\c -> isAlphaNum c || c `elem` "_-.")
-  Parse.char ':'
+  _ <- Parse.char ':'
   uriStr <- Parse.munch1 (\c -> isAlphaNum c || c `elem` "+-=._/*()@'$:;&!?~")
   uri <- maybe Parse.pfail return (parseAbsoluteURI uriStr)
   return $ RemoteRepo {
