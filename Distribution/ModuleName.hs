2b71426cd2077bbd3c07cd1555d740d11432ecd7
Ian Lynagh <igloo@earth.li>
2010-03-26 13:17:52 +0000

    Remove an out-of-date comment

--- Distribution/ModuleName.hs
+++ Distribution/ModuleName.hs
@@ -86,8 +86,6 @@ validModuleComponent []     = False
 validModuleComponent (c:cs) = Char.isUpper c
                            && all validModuleChar cs
 
--- FIXME This is used in Distribution/Simple/PreProcess.hs, so we can't
--- deprecate it yet without getting warnings, and thus build failures
 {-# DEPRECATED simple "use ModuleName.fromString instead" #-}
 simple :: String -> ModuleName
 simple str = ModuleName [str]
