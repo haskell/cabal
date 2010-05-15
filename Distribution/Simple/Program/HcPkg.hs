1b90401d3b4d53b5df641cc474e1dcb06ff3a153
Ian Lynagh <igloo@earth.li>
2010-05-15 19:38:10 +0000

    Add some diagnostic info to some internal errors When giving an "internal error: unexpected package db stack" error, say what the stack is.

--- Distribution/Simple/Program/HcPkg.hs
+++ Distribution/Simple/Program/HcPkg.hs
@@ -239,7 +239,7 @@ packageDbStackOpts dbstack = case dbstack of
   where
     specific (SpecificPackageDB db) = "--package-conf=" ++ db
     specific _ = ierror
-    ierror     = error "internal error: unexpected package db stack"
+    ierror     = error ("internal error: unexpected package db stack: " ++ show dbstack)
 
 packageDbOpts :: PackageDB -> String
 packageDbOpts GlobalPackageDB        = "--global"
