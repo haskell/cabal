54257d5a2f1b52339348f5140c62f748b6cd663a
Duncan Coutts <duncan@haskell.org>
2010-10-27 13:43:19 +0000

    Fix a documentation reference to ghc 6.14

--- Language/Haskell/Extension.hs
+++ Language/Haskell/Extension.hs
@@ -356,7 +356,7 @@ data KnownExtension =
   | PackageImports
 
   -- | [GHC &#xa7; 7.8.6] Deprecated in GHC 6.12 and will be removed in
-  -- GHC 6.14.  Allow a type variable to be instantiated at a
+  -- GHC 7.  Allow a type variable to be instantiated at a
   -- polymorphic type.
   | ImpredicativeTypes
 
