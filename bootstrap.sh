a79250d28aac159945e3c6fc6da5dc0d024c0795
Duncan Coutts <duncan@haskell.org>
2010-11-23 17:25:16 +0000

    Update dependencies in the boostrap.sh script

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,12 +45,13 @@ done
 
 # Versions of the packages to install.
 # The version regex says what existing installed versions are ok.
-PARSEC_VER="2.1.0.1";  PARSEC_VER_REGEXP="2\."     # == 2.*
-NETWORK_VER="2.2.1.5"; NETWORK_VER_REGEXP="2\."    # == 2.*
-CABAL_VER="1.8.0.2";   CABAL_VER_REGEXP="1\.8\."   # == 1.8.*
-MTL_VER="1.1.0.2";     MTL_VER_REGEXP="1\.1\."     # == 1.1.*
-HTTP_VER="4000.0.8";   HTTP_VER_REGEXP="4000\.0"   # == 4000.0.*
-ZLIB_VER="0.5.2.0";    ZLIB_VER_REGEXP="0\.[45]\." # == 0.5.*
+PARSEC_VER="2.1.0.1";   PARSEC_VER_REGEXP="2\."     # == 2.*
+NETWORK_VER="2.2.1.10"; NETWORK_VER_REGEXP="2\."    # == 2.*
+CABAL_VER="1.10.0.0";   CABAL_VER_REGEXP="1\.10\."  # == 1.10.*
+MTL_VER="1.1.1.0";      MTL_VER_REGEXP="1\.1\."     # == 1.1.*
+HTTP_VER="4000.0.8";    HTTP_VER_REGEXP="4000\.0"   # == 4000.0.*
+ZLIB_VER="0.5.2.0";     ZLIB_VER_REGEXP="0\.[45]\." # == 0.4.* || ==0.5.*
+TIME_VER="1.2.0.3"      TIME_VER_REGEXP="0\.[12]\." # == 0.1.* || ==0.2.*
 
 HACKAGE_URL="http://hackage.haskell.org/packages/archive"
 
@@ -198,6 +199,7 @@ info_pkg "parsec"  ${PARSEC_VER}  ${PARSEC_VER_REGEXP}
 info_pkg "network" ${NETWORK_VER} ${NETWORK_VER_REGEXP}
 info_pkg "Cabal"   ${CABAL_VER}   ${CABAL_VER_REGEXP}
 info_pkg "mtl"     ${MTL_VER}     ${MTL_VER_REGEXP}
+info_pkg "time"    ${TIME_VER}   ${TIME_VER_REGEXP}
 info_pkg "HTTP"    ${HTTP_VER}    ${HTTP_VER_REGEXP}
 info_pkg "zlib"    ${ZLIB_VER}    ${ZLIB_VER_REGEXP}
 
@@ -205,6 +207,7 @@ do_pkg "parsec"  ${PARSEC_VER}  ${PARSEC_VER_REGEXP}
 do_pkg "network" ${NETWORK_VER} ${NETWORK_VER_REGEXP}
 do_pkg "Cabal"   ${CABAL_VER}   ${CABAL_VER_REGEXP}
 do_pkg "mtl"     ${MTL_VER}     ${MTL_VER_REGEXP}
+do_pkg "time"    ${TIME_VER}    ${TIME_VER_REGEXP}
 do_pkg "HTTP"    ${HTTP_VER}    ${HTTP_VER_REGEXP}
 do_pkg "zlib"    ${ZLIB_VER}    ${ZLIB_VER_REGEXP}
 
