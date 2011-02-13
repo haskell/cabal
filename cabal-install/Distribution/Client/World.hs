9f0659927c8c11ad66cae6cc4cc7246b1e479c34
Duncan Coutts <duncan@community.haskell.org>
2011-02-13 18:50:06 +0000

    Separate WorldPkgInfo type from UnresolvedDependency type Currently just a renamed copy of UnresolvedDependency but called WorldPkgInfo and defined in the World module. This is in preparation to remove all other uses of the UnresolvedDependency type.

--- cabal-install/Distribution/Client/World.hs
+++ cabal-install/Distribution/Client/World.hs
@@ -23,6 +23,7 @@
 --
 -----------------------------------------------------------------------------
 module Distribution.Client.World (
+    WorldPkgInfo(..),
     insert,
     delete,
     getContents,
@@ -32,50 +33,68 @@ module Distribution.Client.World (
     isGoodWorldTarget,
   ) where
 
-import Distribution.Simple.Utils( writeFileAtomic )
 import Distribution.Client.Types
     ( UnresolvedDependency(..) )
 import Distribution.Package
-    ( PackageName(..), Dependency( Dependency ) )
+         ( PackageName(..), Dependency(..) )
+import Distribution.PackageDescription
+         ( FlagAssignment, FlagName(FlagName) )
 import Distribution.Version( anyVersion )
-import Distribution.Text( display, simpleParse )
-import Distribution.Verbosity ( Verbosity )
-import Distribution.Simple.Utils ( die, info, chattyTry )
-import Data.List( unionBy, deleteFirstsBy, nubBy )
-import Data.Maybe( isJust, fromJust )
-import System.IO.Error( isDoesNotExistError, )
+import Distribution.Verbosity
+         ( Verbosity )
+import Distribution.Simple.Utils
+         ( die, info, chattyTry, writeFileAtomic )
+import Distribution.Text
+         ( Text(..), display, simpleParse )
+import qualified Distribution.Compat.ReadP as Parse
+import qualified Text.PrettyPrint as Disp
+import Text.PrettyPrint ( (<>), (<+>) )
+
+
+import Data.Char as Char
+
+import Data.List
+         ( unionBy, deleteFirstsBy, nubBy )
+import Data.Maybe
+         ( isJust, fromJust )
+import System.IO.Error
+         ( isDoesNotExistError )
 import qualified Data.ByteString.Lazy.Char8 as B
-import Prelude hiding ( getContents )
+import Prelude hiding (getContents)
+
+
+data WorldPkgInfo = WorldPkgInfo Dependency FlagAssignment
+  deriving (Show,Eq)
 
 -- | Adds packages to the world file; creates the file if it doesn't
 -- exist yet. Version constraints and flag assignments for a package are
 -- updated if already present. IO errors are non-fatal.
-insert :: Verbosity -> FilePath -> [UnresolvedDependency] -> IO ()
+insert :: Verbosity -> FilePath -> [WorldPkgInfo] -> IO ()
 insert = modifyWorld $ unionBy equalUDep
 
 -- | Removes packages from the world file.
 -- Note: Currently unused as there is no mechanism in Cabal (yet) to
 -- handle uninstalls. IO errors are non-fatal.
-delete :: Verbosity -> FilePath -> [UnresolvedDependency] -> IO ()
+delete :: Verbosity -> FilePath -> [WorldPkgInfo] -> IO ()
 delete = modifyWorld $ flip (deleteFirstsBy equalUDep)
 
--- | UnresolvedDependency values are considered equal if they refer to
+-- | WorldPkgInfo values are considered equal if they refer to
 -- the same package, i.e., we don't care about differing versions or flags.
-equalUDep :: UnresolvedDependency -> UnresolvedDependency -> Bool
-equalUDep (UnresolvedDependency (Dependency pkg1 _) _)
-          (UnresolvedDependency (Dependency pkg2 _) _) = pkg1 == pkg2
+equalUDep :: WorldPkgInfo -> WorldPkgInfo -> Bool
+equalUDep (WorldPkgInfo (Dependency pkg1 _) _)
+          (WorldPkgInfo (Dependency pkg2 _) _) = pkg1 == pkg2
 
 -- | Modifies the world file by applying an update-function ('unionBy'
 -- for 'insert', 'deleteFirstsBy' for 'delete') to the given list of
 -- packages. IO errors are considered non-fatal.
-modifyWorld :: ([UnresolvedDependency] -> [UnresolvedDependency]
-                -> [UnresolvedDependency])
+modifyWorld :: ([WorldPkgInfo] -> [WorldPkgInfo]
+                -> [WorldPkgInfo])
                         -- ^ Function that defines how
                         -- the list of user packages are merged with
                         -- existing world packages.
             -> Verbosity
             -> FilePath               -- ^ Location of the world file
-            -> [UnresolvedDependency] -- ^ list of user supplied packages
+            -> [WorldPkgInfo] -- ^ list of user supplied packages
             -> IO ()
 modifyWorld _ _         _     []   = return ()
 modifyWorld f verbosity world pkgs =
@@ -96,7 +115,7 @@ modifyWorld f verbosity world pkgs =
 
 
 -- | Returns the content of the world file as a list
-getContents :: FilePath -> IO [UnresolvedDependency]
+getContents :: FilePath -> IO [WorldPkgInfo]
 getContents world = do
   content <- safelyReadFile world
   let result = map simpleParse (lines $ B.unpack content)
@@ -130,3 +149,52 @@ isGoodWorldTarget (UnresolvedDependency (Dependency pkg ver) flags) =
      pkg == worldPkg
   && ver == anyVersion
   && null flags
+
+instance Text WorldPkgInfo where
+  disp (WorldPkgInfo dep flags) = disp dep <+> dispFlags flags
+    where
+      dispFlags [] = Disp.empty
+      dispFlags fs = Disp.text "--flags="
+                  <> Disp.doubleQuotes (flagAssToDoc fs)
+      flagAssToDoc = foldr (\(FlagName fname,val) flagAssDoc ->
+                             (if not val then Disp.char '-'
+                                         else Disp.empty)
+                             Disp.<> Disp.text fname
+                             Disp.<+> flagAssDoc)
+                           Disp.empty
+  parse = do
+      dep <- parse
+      Parse.skipSpaces
+      flagAss <- Parse.option [] parseFlagAssignment
+      return $ WorldPkgInfo dep flagAss
+    where
+      parseFlagAssignment :: Parse.ReadP r FlagAssignment
+      parseFlagAssignment = do
+          _ <- Parse.string "--flags"
+          Parse.skipSpaces
+          _ <- Parse.char '='
+          Parse.skipSpaces
+          inDoubleQuotes $ Parse.many1 flag
+        where
+          inDoubleQuotes :: Parse.ReadP r a -> Parse.ReadP r a
+          inDoubleQuotes = Parse.between (Parse.char '"') (Parse.char '"')
+
+          flag = do
+            Parse.skipSpaces
+            val <- negative Parse.+++ positive
+            name <- ident
+            Parse.skipSpaces
+            return (FlagName name,val)
+          negative = do
+            _ <- Parse.char '-'
+            return False
+          positive = return True
+
+          ident :: Parse.ReadP r String
+          ident = do
+            -- First character must be a letter/digit to avoid flags
+            -- like "+-debug":
+            c  <- Parse.satisfy Char.isAlphaNum
+            cs <- Parse.munch (\ch -> Char.isAlphaNum ch || ch == '_'
+                                                         || ch == '-')
+            return (c:cs)
