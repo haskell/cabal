0d6cc11957cff1e2e94a1457ae95c725d95eef23
Duncan Coutts <duncan@community.haskell.org>
2011-07-18 23:57:28 +0000

    Fix cabal haddock for packages with internal dependencies

--- cabal/Distribution/Simple/LocalBuildInfo.hs
+++ cabal/Distribution/Simple/LocalBuildInfo.hs
@@ -140,13 +140,17 @@ data LocalBuildInfo = LocalBuildInfo {
         progSuffix    :: PathTemplate -- ^Suffix to be appended to installed executables
   } deriving (Read, Show)
 
--- | External package dependencies for the package as a whole, the union of the
--- individual 'targetPackageDeps'.
+-- | External package dependencies for the package as a whole. This is the
+-- union of the individual 'componentPackageDeps', less any internal deps.
 externalPackageDeps :: LocalBuildInfo -> [(InstalledPackageId, PackageId)]
-externalPackageDeps lbi = nub $
+externalPackageDeps lbi = filter (not . internal . snd) $ nub $
   -- TODO:  what about non-buildable components?
-     maybe [] componentPackageDeps (libraryConfig lbi)
-  ++ concatMap (componentPackageDeps . snd) (executableConfigs lbi)
+       maybe [] componentPackageDeps (libraryConfig lbi)
+    ++ concatMap (componentPackageDeps . snd) (executableConfigs lbi)
+  where
+    -- True if this dependency is an internal one (depends on the library
+    -- defined in the same package).
+    internal pkgid = pkgid == packageId (localPkgDescr lbi)
 
 -- | The installed package Id we use for local packages registered in the local
 -- package db. This is what is used for intra-package deps between components.
