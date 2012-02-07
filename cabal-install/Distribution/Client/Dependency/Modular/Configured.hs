2c6036643564753114e1e0919a3004140fddc04c
Thomas Tuegel <ttuegel@gmail.com>
2012-02-07 19:45:43 +0000

    Update types in modular dependency solver to compile with new test/benchmark dependency constraints.

--- cabal-install/Distribution/Client/Dependency/Modular/Configured.hs
+++ cabal-install/Distribution/Client/Dependency/Modular/Configured.hs
@@ -1,9 +1,10 @@
 module Distribution.Client.Dependency.Modular.Configured where
 
 import Distribution.PackageDescription (FlagAssignment) -- from Cabal
+import Distribution.Client.Types (OptionalStanza)
 
 import Distribution.Client.Dependency.Modular.Package
 
 -- | A configured package is a package instance together with
 -- a flag assignment and complete dependencies.
-data CP qpn = CP (PI qpn) FlagAssignment [PI qpn]
+data CP qpn = CP (PI qpn) FlagAssignment [OptionalStanza] [PI qpn]
