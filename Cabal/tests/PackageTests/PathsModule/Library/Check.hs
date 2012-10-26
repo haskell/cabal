bac660f5e681d0c0bcefc5754a598cc85cd360d1
Johan Tibell <johan.tibell@gmail.com>
2012-10-25 21:52:11 -0700

    Add tests for PathsModule

---
+++ Cabal/tests/PackageTests/PathsModule/Library/Check.hs
@@ -0,0 +1,13 @@
+module PackageTests.PathsModule.Library.Check (suite) where
+
+import Distribution.Version
+import PackageTests.PackageTester (PackageSpec(..), assertBuildSucceeded,
+                                   cabal_build)
+import System.FilePath
+import Test.HUnit
+
+suite :: Test
+suite = TestCase $ do
+    let spec = PackageSpec ("PackageTests" </> "PathsModule" </> "Library") []
+    result <- cabal_build spec
+    assertBuildSucceeded result
