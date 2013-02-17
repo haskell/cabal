e4ca22f370a50b937747ed31e0cb5db900c7720a
Sönke Hahn <soenkehahn@gmail.com>
2013-02-17 12:52:51 +0100

    don't assume ghc in warning message about destructive reinstalls

--- cabal-install/Distribution/Client/Install.hs
+++ cabal-install/Distribution/Client/Install.hs
@@ -432,7 +432,7 @@ checkPrintPlan verbosity installed installPlan sourcePkgDb
                  ["Continuing even though the plan contains dangerous reinstalls."]
                else
                  ["Try --avoid-reinstalls to avoid selecting destructive reinstalls or " ++
-                  "manually unregister conflicting packages (using 'ghc-pkg unregister').",
+                  "manually uninstall conflicting packages.",
                   "Use --force-reinstalls if you want to install anyway (including destructive reinstalls)."]
       else unless dryRun $ warn verbosity
              "Note that reinstalls are always dangerous. Continuing anyway..."
