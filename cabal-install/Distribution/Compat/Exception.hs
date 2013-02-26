cbe63e86a543a9e2f513d62cedbf00d905d41908
Ben Millwood <thebenmachine+git@gmail.com>
2013-02-26 21:23:52 +0000

    Add mask, mask_ to Distribution.Compat.Exception
    This fixes the build on GHC 6.12.3 and also makes -j work there.

--- cabal-install/Distribution/Compat/Exception.hs
+++ cabal-install/Distribution/Compat/Exception.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_HADDOCK hide #-}
 module Distribution.Compat.Exception (
   SomeException,
+  mask,
+  mask_,
   onException,
   catchIO,
   handleIO,
@@ -11,6 +14,24 @@ module Distribution.Compat.Exception (
 import System.Exit
 import qualified Control.Exception as Exception
 import Control.Exception (SomeException)
+
+#if MIN_VERSION_base(4,3,0)
+-- it's much less of a headache if we re-export the "real" mask and mask_
+-- so there's never more than one definition to conflict
+import Control.Exception (mask, mask_)
+#else
+import Control.Exception (block, unblock)
+#endif
+
+#if !MIN_VERSION_base(4,3,0)
+-- note: less polymorphic than 'real' mask, to avoid RankNTypes
+-- we don't need the full generality where we use it
+mask :: ((IO a -> IO a) -> IO b) -> IO b
+mask handler = block (handler unblock)
+
+mask_ :: IO a -> IO a
+mask_ = block
+#endif
 
 onException :: IO a -> IO b -> IO a
 onException = Exception.onException
