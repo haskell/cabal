457f08f151109c521f0440e836a431121913c78f
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2013-05-02 20:43:34 +0200

    Reowrk the logic a bit.

--- cabal-install/Distribution/Client/SrcDist.hs
+++ cabal-install/Distribution/Client/SrcDist.hs
@@ -44,40 +44,48 @@ sdist flags exflags = do
   pkg <- return . flattenPackageDescription
          =<< readPackageDescription verbosity
          =<< defaultPackageDesc verbosity
-  let withDir = if isOutDirectory then (\f -> f tmpTargetDir)
+  let withDir = if not needMakeArchive then (\f -> f tmpTargetDir)
                 else withTempDirectory verbosity False tmpTargetDir "sdist."
-  -- Otherwise 'withTempDir' fails...
-  createDirectoryIfMissingVerbose verbosity True tmpTargetDir
+  -- 'withTempDir' fails if we don't create 'tmpTargetDir'...
+  when needMakeArchive $
+    createDirectoryIfMissingVerbose verbosity True tmpTargetDir
   withDir $ \tmpDir -> do
     let outDir = if isOutDirectory then tmpDir else tmpDir </> tarBallName pkg
-        flags' = (if isOutDirectory then flags
+        flags' = (if not needMakeArchive then flags
                   else flags { sDistDirectory = Flag outDir })
                  { sDistVerbosity = Flag $ if   verbosity == normal
                                            then lessVerbose verbosity
                                            else verbosity }
+    unless isListSources $
+      createDirectoryIfMissingVerbose verbosity True outDir
 
-    createDirectoryIfMissingVerbose verbosity True outDir
-
-    -- Run 'setup sdist --output-directory=tmpDir'
+    -- Run 'setup sdist --output-directory=tmpDir' (or
+    -- '--list-source'/'--output-directory=someOtherDir') in case we were passed
+    -- those options.
     setupWrapper verbosity setupOpts (Just pkg) sdistCommand (const flags') []
 
-    -- And unless we were given --list-sources or --output-directory ourselves,
+    -- Unless we were given --list-sources or --output-directory ourselves,
     -- create an archive.
-    unless (isListSources || isOutDirectory) $
+    when needMakeArchive $
       createArchive verbosity pkg tmpDir distPref
 
     when isOutDirectory $
       notice verbosity $ "Source directory created: " ++ tmpTargetDir
 
+    when isListSources $
+      notice verbosity $ "List of package sources written to file '"
+                         ++ (fromFlag . sDistListSources $ flags) ++ "'"
+
   where
     flagEnabled f  = not . null . flagToList . f $ flags
 
-    isListSources  = flagEnabled sDistListSources
-    isOutDirectory = flagEnabled sDistDirectory
-    verbosity      = fromFlag (sDistVerbosity flags)
-    distPref       = fromFlag (sDistDistPref flags)
-    tmpTargetDir   = fromFlagOrDefault (srcPref distPref) (sDistDirectory flags)
-    setupOpts      = defaultSetupScriptOptions {
+    isListSources   = flagEnabled sDistListSources
+    isOutDirectory  = flagEnabled sDistDirectory
+    needMakeArchive = not (isListSources || isOutDirectory)
+    verbosity       = fromFlag (sDistVerbosity flags)
+    distPref        = fromFlag (sDistDistPref flags)
+    tmpTargetDir    = fromFlagOrDefault (srcPref distPref) (sDistDirectory flags)
+    setupOpts       = defaultSetupScriptOptions {
       -- The '--output-directory' sdist flag was introduced in Cabal 1.12, and
       -- '--list-sources' in 1.17.
       useCabalVersion = if isListSources
