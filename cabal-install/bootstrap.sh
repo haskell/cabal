5469c16016c5beedb412341e85c5678b0d9a2299
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2013-09-01 01:25:31 +0200

    Update bootstrap.sh.

--- cabal-install/bootstrap.sh
+++ cabal-install/bootstrap.sh
@@ -51,14 +51,14 @@ PREFIX=${PREFIX:-${DEFAULT_PREFIX}}
 # The version regex says what existing installed versions are ok.
 PARSEC_VER="3.1.3";    PARSEC_VER_REGEXP="[23]\."              # == 2.* || == 3.*
 DEEPSEQ_VER="1.3.0.1"; DEEPSEQ_VER_REGEXP="1\.[1-9]\."         # >= 1.1 && < 2
-TEXT_VER="0.11.2.3";   TEXT_VER_REGEXP="0\.([2-9]|(1[0-1]))\." # >= 0.2 && < 0.12
-NETWORK_VER="2.4.1.0"; NETWORK_VER_REGEXP="2\."                # == 2.*
+TEXT_VER="0.11.3.1";   TEXT_VER_REGEXP="0\.([2-9]|(1[0-1]))\." # >= 0.2 && < 0.12
+NETWORK_VER="2.4.1.2"; NETWORK_VER_REGEXP="2\."                # == 2.*
 CABAL_VER="1.18.0.0";  CABAL_VER_REGEXP="1\.1[89]\."           # >= 1.18 && < 1.20
 TRANS_VER="0.3.0.0";   TRANS_VER_REGEXP="0\.[23]\."            # >= 0.2.* && < 0.4.*
 MTL_VER="2.1.2";       MTL_VER_REGEXP="[2]\."                  #  == 2.*
-HTTP_VER="4000.2.5";   HTTP_VER_REGEXP="4000\.[012]\."         # == 4000.0.* || 4000.1.* || 4000.2.*
-ZLIB_VER="0.5.4.0";    ZLIB_VER_REGEXP="0\.[45]\."             # == 0.4.* || == 0.5.*
-TIME_VER="1.4.0.2"     TIME_VER_REGEXP="1\.[1234]\.?"          # >= 1.1 && < 1.5
+HTTP_VER="4000.2.8";   HTTP_VER_REGEXP="4000\.[012]\."         # == 4000.0.* || 4000.1.* || 4000.2.*
+ZLIB_VER="0.5.4.1";    ZLIB_VER_REGEXP="0\.[45]\."             # == 0.4.* || == 0.5.*
+TIME_VER="1.4.1"       TIME_VER_REGEXP="1\.[1234]\.?"          # >= 1.1 && < 1.5
 RANDOM_VER="1.0.1.1"   RANDOM_VER_REGEXP="1\.0\."              # >= 1 && < 1.1
 STM_VER="2.4.2";       STM_VER_REGEXP="2\."                    # == 2.*
 
