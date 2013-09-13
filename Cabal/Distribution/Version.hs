75f55963881eb70de9ebf5a2c4e1901d5e6c18f0
Herbert Valerio Riedel <hvr@gnu.org>
2013-09-13 11:22:58 +0200

    Support non-orphaned `instance Data Version` in GHC≥7.7
    Signed-off-by: Herbert Valerio Riedel <hvr@gnu.org>

--- Cabal/Distribution/Version.hs
+++ Cabal/Distribution/Version.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveDataTypeable, StandaloneDeriving #-}
+{-# LANGUAGE CPP, DeriveDataTypeable, StandaloneDeriving #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Distribution.Version
@@ -121,8 +121,10 @@ data VersionRange
   | VersionRangeParens     VersionRange -- just '(exp)' parentheses syntax
   deriving (Show,Read,Eq,Typeable,Data)
 
--- TODO orphan
+#if __GLASGOW_HASKELL__ < 707
+-- starting with ghc-7.7/base-4.7 this instance is provided in "Data.Data"
 deriving instance Data Version
+#endif
 
 {-# DEPRECATED AnyVersion "Use 'anyVersion', 'foldVersionRange' or 'asVersionIntervals'" #-}
 {-# DEPRECATED ThisVersion "use 'thisVersion', 'foldVersionRange' or 'asVersionIntervals'" #-}
