1e808b1ef91b4bca7eee5caac0bc1fa5d97cc08c
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2013-12-31 23:48:46 +0100

    Update 'bootstrap.sh'.

--- cabal-install/bootstrap.sh
+++ cabal-install/bootstrap.sh
@@ -51,7 +51,7 @@ PREFIX=${PREFIX:-${DEFAULT_PREFIX}}
 # The version regex says what existing installed versions are ok.
 PARSEC_VER="3.1.4";    PARSEC_VER_REGEXP="[23]\."              # == 2.* || == 3.*
 DEEPSEQ_VER="1.3.0.2"; DEEPSEQ_VER_REGEXP="1\.[1-9]\."         # >= 1.1 && < 2
-TEXT_VER="1.0.0.0";    TEXT_VER_REGEXP="((1\.0\.)|(0\.([2-9]|(1[0-1]))\.))" # >= 0.2 && < 1.1
+TEXT_VER="1.0.0.1";    TEXT_VER_REGEXP="((1\.0\.)|(0\.([2-9]|(1[0-1]))\.))" # >= 0.2 && < 1.1
 NETWORK_VER="2.4.2.1"; NETWORK_VER_REGEXP="2\."                # == 2.*
 CABAL_VER="1.19.2";    CABAL_VER_REGEXP="1\.1[9]\."            # >= 1.19 && < 1.20
 TRANS_VER="0.3.0.0";   TRANS_VER_REGEXP="0\.[23]\."            # >= 0.2.* && < 0.4.*
