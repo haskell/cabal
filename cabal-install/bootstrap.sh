aef2fdbe52994a4262eab2bf66cdf15de8b376ff
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2014-04-16 01:16:49 +0200

    Update bootstrap.sh.
    (cherry picked from commit a63871b7ed005fae265a9e8517e51c346e8c6b5a)
    
    Conflicts:
    	cabal-install/bootstrap.sh

--- cabal-install/bootstrap.sh
+++ cabal-install/bootstrap.sh
@@ -122,25 +122,25 @@ PARSEC_VER="3.1.5";    PARSEC_VER_REGEXP="[23]\."
                        # == 2.* || == 3.*
 DEEPSEQ_VER="1.3.0.2"; DEEPSEQ_VER_REGEXP="1\.[1-9]\."
                        # >= 1.1 && < 2
-TEXT_VER="1.1.0.0";    TEXT_VER_REGEXP="((1\.[01]\.)|(0\.([2-9]|(1[0-1]))\.))"
+TEXT_VER="1.1.0.1";    TEXT_VER_REGEXP="((1\.[01]\.)|(0\.([2-9]|(1[0-1]))\.))"
                        # >= 0.2 && < 1.2
-NETWORK_VER="2.4.2.2"; NETWORK_VER_REGEXP="2\."
+NETWORK_VER="2.5.0.0"; NETWORK_VER_REGEXP="2\."
                        # == 2.*
 CABAL_VER="1.20.0.0";  CABAL_VER_REGEXP="1\.2[01]\."
                        # >= 1.20 && < 1.21
 TRANS_VER="0.3.0.0";   TRANS_VER_REGEXP="0\.[23]\."
                        # >= 0.2.* && < 0.4.*
-MTL_VER="2.1.2";       MTL_VER_REGEXP="[2]\."
+MTL_VER="2.1.3.1";     MTL_VER_REGEXP="[2]\."
                        #  == 2.*
-HTTP_VER="4000.2.10";  HTTP_VER_REGEXP="4000\.2\.[5-9]"
+HTTP_VER="4000.2.12";  HTTP_VER_REGEXP="4000\.2\.[5-9]"
                        # >= 4000.2.5 < 4000.3
 ZLIB_VER="0.5.4.1";    ZLIB_VER_REGEXP="0\.[45]\."
                        # == 0.4.* || == 0.5.*
-TIME_VER="1.4.1"       TIME_VER_REGEXP="1\.[1234]\.?"
+TIME_VER="1.4.2"       TIME_VER_REGEXP="1\.[1234]\.?"
                        # >= 1.1 && < 1.5
 RANDOM_VER="1.0.1.1"   RANDOM_VER_REGEXP="1\.0\."
                        # >= 1 && < 1.1
-STM_VER="2.4.2";       STM_VER_REGEXP="2\."
+STM_VER="2.4.3";       STM_VER_REGEXP="2\."
                        # == 2.*
 
 HACKAGE_URL="https://hackage.haskell.org/package"
