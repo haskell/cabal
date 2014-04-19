40356c6331bb51549cc9299d0ca2d81f5525c444
Thomas Tuegel <ttuegel@gmail.com>
2014-04-19 09:56:27 -0500

    cabal test: only build tests and dependencies

--- cabal-install/Main.hs
+++ cabal-install/Main.hs
@@ -105,7 +105,7 @@ import Distribution.Client.Utils              (determineNumJobs
                                               ,existsAndIsMoreRecentThan)
 
 import Distribution.PackageDescription
-         ( Executable(..) )
+         ( Executable(..), testName )
 import Distribution.PackageDescription.Parse
          ( readPackageDescription )
 import Distribution.PackageDescription.PrettyPrint
@@ -145,6 +145,7 @@ import System.IO                (BufferMode(LineBuffering),
                                  hSetBuffering, stdout)
 import System.Directory         (doesFileExist, getCurrentDirectory)
 import Data.List                (intercalate)
+import Data.Maybe               (mapMaybe)
 import Data.Monoid              (Monoid(..))
 import Control.Monad            (when, unless)
 
@@ -711,12 +712,24 @@ testAction (testFlags, buildFlags, buildExFlags) extraArgs globalFlags = do
                           globalFlags noAddSource
                           (buildNumJobs buildFlags') checkFlags
 
+  -- the package was just configured, so the LBI must be available
+  lbi <- getPersistBuildConfig distPref
+  let pkgDescr = LBI.localPkgDescr lbi
+      nameTestsOnly = LBI.foldComponent (const Nothing)
+                                        (const Nothing)
+                                        (Just . testName)
+                                        (const Nothing)
+      tests = mapMaybe nameTestsOnly $ LBI.pkgComponents pkgDescr
+      extraArgs'
+        | null extraArgs = tests
+        | otherwise = extraArgs
+
   maybeWithSandboxDirOnSearchPath useSandbox $
-    build verbosity config distPref buildFlags' extraArgs
+    build verbosity config distPref buildFlags' extraArgs'
 
   maybeWithSandboxDirOnSearchPath useSandbox $
     setupWrapper verbosity setupOptions Nothing
-      Cabal.testCommand (const testFlags) extraArgs
+      Cabal.testCommand (const testFlags) extraArgs'
 
 benchmarkAction :: (BenchmarkFlags, BuildFlags, BuildExFlags)
                    -> [String] -> GlobalFlags
