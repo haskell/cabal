170c344b3121e62f6871ec53a7b7e7f218b105ee
Iain Nicol <iain@iainnicol.com>
2014-05-10 13:44:19 +0100

    Bring a Haddock version check in line with message

--- Cabal/Distribution/Simple/Haddock.hs
+++ Cabal/Distribution/Simple/Haddock.hs
@@ -175,7 +175,7 @@ haddock pkg_descr lbi suffixes flags = do
     let isVersion2   = version >= Version [2,0] []
 
     when ( flag haddockHoogle
-           && version > Version [2] []
+           && version >= Version [2] []
            && version < Version [2,2] []) $
          die "haddock 2.0 and 2.1 do not support the --hoogle flag."
 
