49cd635c3d6baf1b926948b4ff71d73935b201fb
Erik de Castro Lopo <erikd@mega-nerd.com>
2014-10-19 21:52:05 +1100

    Cabal: Add a NubList type with tests.
    A NubList is a list where all elements are unique, but are maintained in
    their original order. The Monoid NubList's mappend implement is just:
    
        NubList xs `mappend` NubList ys = NubList . nub $ xs ++ ys
    
    which preserves NubList's required properties (ordering and unique-ness
    of elements) and meets the requirements of the Monoid laws (identity,
    associativity and closure).

---
+++ Cabal/tests/UnitTests/Distribution/Utils/NubList.hs
@@ -0,0 +1,44 @@
+module UnitTests.Distribution.Utils.NubList
+    ( tests
+    ) where
+
+import Data.Monoid
+import Distribution.Utils.NubList
+import Test.Framework
+import Test.Framework.Providers.HUnit (testCase)
+import Test.Framework.Providers.QuickCheck2
+import Test.HUnit (Assertion, assertBool)
+
+tests :: [Test]
+tests =
+    [ testCase "Numlist retains ordering" testOrdering
+    , testCase "Numlist removes duplicates" testDeDupe
+    , testProperty "Monoid Numlist Identity" prop_Identity
+    , testProperty "Monoid Numlist Associativity" prop_Associativity
+    ]
+
+someIntList :: [Int]
+-- This list must not have duplicate entries.
+someIntList = [ 1, 3, 4, 2, 0, 7, 6, 5, 9, -1 ]
+
+testOrdering :: Assertion
+testOrdering =
+    assertBool "Maintains element ordering:" $
+        fromNubList (toNubList someIntList) == someIntList
+
+testDeDupe :: Assertion
+testDeDupe =
+    assertBool "De-duplicates a list:" $
+        fromNubList (toNubList (someIntList ++ someIntList)) == someIntList
+
+-- ---------------------------------------------------------------------------
+-- QuickCheck properties for NubList
+
+prop_Identity :: [Int] -> Bool
+prop_Identity xs =
+    mempty `mappend` toNubList xs == toNubList xs `mappend` mempty
+
+prop_Associativity :: [Int] -> [Int] -> [Int] -> Bool
+prop_Associativity xs ys zs =
+    (toNubList xs `mappend` toNubList ys) `mappend` toNubList zs
+            == toNubList xs `mappend` (toNubList ys `mappend` toNubList zs)
