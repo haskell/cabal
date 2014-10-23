a63b6063100a71dd1490a786b11b288e0a06ed96
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2014-10-23 22:34:52 +0200

    Check that sandbox exists before running 'sandbox delete'.

--- cabal-install/tests/PackageTests/MultipleSource/Check.hs
+++ cabal-install/tests/PackageTests/MultipleSource/Check.hs
@@ -8,7 +8,9 @@ import PackageTests.PackageTester
 import Test.Framework                 as TF (Test)
 import Test.Framework.Providers.HUnit (testCase)
 
-import System.FilePath ((</>))
+import Control.Monad    (void, when)
+import System.Directory (doesDirectoryExist)
+import System.FilePath  ((</>))
 
 dir :: FilePath
 dir = "PackageTests" </> "MultipleSource"
@@ -16,8 +18,9 @@ dir = "PackageTests" </> "MultipleSource"
 tests :: FilePath -> [TF.Test]
 tests cabalPath =
     [ testCase "finds second source of multiple source" $ do
-          -- can fail if there is no pre-existing sandbox
-          _ <- cabal_sandbox dir ["delete"] cabalPath
+          sandboxExists <- doesDirectoryExist $ dir </> ".cabal-sandbox"
+          when sandboxExists $
+            void $ cabal_sandbox dir ["delete"] cabalPath
           assertSandboxSucceeded =<< cabal_sandbox dir ["init"] cabalPath
           assertSandboxSucceeded =<< cabal_sandbox dir ["add-source", "p"] cabalPath
           assertSandboxSucceeded =<< cabal_sandbox dir ["add-source", "q"] cabalPath
