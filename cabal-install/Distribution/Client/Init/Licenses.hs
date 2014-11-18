ceca5e26f70e8922c902a79b616e044ebaf40b5d
Vincent Hanquez <vincent@snarc.org>
2014-11-18 07:02:12 +0000

    add ISC license to cabal init

--- cabal-install/Distribution/Client/Init/Licenses.hs
+++ cabal-install/Distribution/Client/Init/Licenses.hs
@@ -10,7 +10,7 @@ module Distribution.Client.Init.Licenses
   , apache20
   , mit
   , mpl20
-
+  , isc
   ) where
 
 type License = String
@@ -3045,4 +3045,21 @@ mpl20 = unlines
     , ""
     , "  This Source Code Form is \"Incompatible With Secondary Licenses\", as"
     , "  defined by the Mozilla Public License, v. 2.0."
+    ]
+
+isc :: String -> String -> License
+isc authors year = unlines
+    [ "Copyright (c) " ++ year ++ " " ++ authors
+    , ""
+    , "Permission to use, copy, modify, and/or distribute this software for any purpose"
+    , "with or without fee is hereby granted, provided that the above copyright notice"
+    , "and this permission notice appear in all copies."
+    , ""
+    , "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH"
+    , "REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND"
+    , "FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,"
+    , "INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS"
+    , "OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER"
+    , "TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF"
+    , "THIS SOFTWARE."
     ]
