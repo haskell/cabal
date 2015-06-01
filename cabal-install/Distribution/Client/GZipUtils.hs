05b4a8d3f71597cb4d99930b2a1c344993280c67
Matthias Fischmann <mf@zerobuzz.net>
2015-06-01 18:28:31 +0200

    Revert noise and relax zlib min version back to 0.5.3 (part 1/2).

--- cabal-install/Distribution/Client/GZipUtils.hs
+++ cabal-install/Distribution/Client/GZipUtils.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE CPP #-}
 
 -----------------------------------------------------------------------------
 -- |
@@ -19,34 +20,30 @@ module Distribution.Client.GZipUtils (
 
 import Data.Monoid ((<>))
 import Control.Monad.ST.Lazy (ST)
-import Data.ByteString.Lazy as BS (ByteString, take, unpack, fromChunks)
+#if MIN_VERSION_zlib(0,6,0)
+import Data.ByteString.Lazy as BS (ByteString, fromChunks)
+#else
+import Data.ByteString.Lazy.Internal as BS (ByteString(Empty, Chunk))
+#endif
 import qualified Data.ByteString as SBS (ByteString)
-import qualified Codec.Compression.GZip as GZip
 import Codec.Compression.Zlib.Internal
 import Data.Maybe (fromMaybe)
 
--- | Does the argument start with @0x1f8b@?  See
--- <http://tools.ietf.org/html/rfc1952#page-5>.
-verifyGzipMagicHeader :: ByteString -> Bool
-verifyGzipMagicHeader = (== [31, 139]) . BS.unpack . BS.take 2
-
--- | If first two bytes of input contain magic gzip marker, decompress as gzip.
--- If they don't, attempt to decompress as zlib.  If that fails, return input
--- bytestring.  It is the callers responsibility to verify that it is not, in
--- fact, garbage.
+-- | Attempts to decompress the `bytes' under the assumption that
+-- "data format" error at the very beginning of the stream means
+-- that it is already decompressed. Caller should make sanity checks
+-- to verify that it is not, in fact, garbage.
 --
 -- This is to deal with http proxies that lie to us and transparently
 -- decompress without removing the content-encoding header. See:
--- <https://github.com/haskell/cabal/issues/678>.  (Zlib does not provide a
--- magic marker that we can check for, so the only safe way to determine
--- whether the string is zlib-compressed is to try decompress it.)
+-- <https://github.com/haskell/cabal/issues/678>
 --
 maybeDecompress :: ByteString -> ByteString
-maybeDecompress bytes | verifyGzipMagicHeader bytes = GZip.decompress bytes
+#if MIN_VERSION_zlib(0,6,0)
 maybeDecompress bytes = fromMaybe bytes $ foldDecompressStreamWithInput outputAv inputReq err s bytes
   where
     outputAv :: SBS.ByteString -> Maybe ByteString -> Maybe ByteString
-    outputAv outchunk = fmap (<> fromChunks [outchunk])
+    outputAv outchunk = fmap (fromChunks [outchunk] <>)
       -- (7.4.2 and down do not support 'fromStrict'.)
 
     inputReq :: ByteString -> Maybe ByteString
@@ -57,3 +54,17 @@ maybeDecompress bytes = fromMaybe bytes $ foldDecompressStreamWithInput outputAv
 
     s :: forall s . DecompressStream (ST s)
     s = decompressST gzipOrZlibFormat defaultDecompressParams
+#else
+maybeDecompress bytes = foldStream $ decompressWithErrors gzipOrZlibFormat defaultDecompressParams bytes
+  where
+    -- DataError at the beginning of the stream probably means that stream is not compressed.
+    -- Returning it as-is.
+    -- TODO: alternatively, we might consider looking for the two magic bytes
+    -- at the beginning of the gzip header.
+    foldStream (StreamError DataError _) = bytes
+    foldStream somethingElse = doFold somethingElse
+
+    doFold StreamEnd               = BS.Empty
+    doFold (StreamChunk bs stream) = BS.Chunk bs (doFold stream)
+    doFold (StreamError _ msg)  = error $ "Codec.Compression.Zlib: " ++ msg
+#endif
