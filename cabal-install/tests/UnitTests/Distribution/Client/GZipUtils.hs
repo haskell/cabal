c09779dcdd9381d75037e3016e647a80aca41561
Matthias Fischmann <mf@zerobuzz.net>
2015-06-06 09:19:27 +0200

    Use edsko's implementation of maybeDecompress; add test case.

--- cabal-install/tests/UnitTests/Distribution/Client/GZipUtils.hs
+++ cabal-install/tests/UnitTests/Distribution/Client/GZipUtils.hs
@@ -2,10 +2,15 @@ module UnitTests.Distribution.Client.GZipUtils (
   tests
   ) where
 
-import Distribution.Client.GZipUtils         (maybeDecompress)
-import Data.ByteString.Lazy.Char8    as BS   (pack)
-import Codec.Compression.Zlib        as Zlib
-import Codec.Compression.GZip        as GZip
+import Codec.Compression.GZip          as GZip
+import Codec.Compression.Zlib          as Zlib
+import Control.Exception.Base                  (evaluate)
+import Control.Exception                       (try, SomeException)
+import Control.Monad                           (void)
+import Data.ByteString.Lazy.Char8     as BS    (pack, init, length)
+import Data.Either                             (isLeft)
+import Data.Monoid                             ((<>))
+import Distribution.Client.GZipUtils           (maybeDecompress)
 
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -19,7 +24,11 @@ maybeDecompressUnitTest =
         assertBool "decompress plain" (maybeDecompress original       == original)
      >> assertBool "decompress zlib"  (maybeDecompress compressedZlib == original)
      >> assertBool "decompress gzip"  (maybeDecompress compressedGZip == original)
+     >> (runBrokenStream >>= assertBool "decompress broken" . isLeft)
   where
     original = BS.pack "original uncompressed input"
     compressedZlib = Zlib.compress original
     compressedGZip = GZip.compress original
+
+    runBrokenStream :: IO (Either SomeException ())
+    runBrokenStream = try . void . evaluate . BS.length $ maybeDecompress (BS.init compressedZlib <> BS.pack "*")
