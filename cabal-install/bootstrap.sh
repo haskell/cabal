4957a48aebdfbe2772aa051d3829ffcc404fe525
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2015-12-06 19:59:20 +0100

    Update bootstrap.sh

--- cabal-install/bootstrap.sh
+++ cabal-install/bootstrap.sh
@@ -179,25 +179,25 @@ PREFIX=${PREFIX:-${DEFAULT_PREFIX}}
 # The version regex says what existing installed versions are ok.
 PARSEC_VER="3.1.9";    PARSEC_VER_REGEXP="[3]\.[01]\."
                        # >= 3.0 && < 3.2
-DEEPSEQ_VER="1.4.1.1"; DEEPSEQ_VER_REGEXP="1\.[1-9]\."
+DEEPSEQ_VER="1.4.1.2"; DEEPSEQ_VER_REGEXP="1\.[1-9]\."
                        # >= 1.1 && < 2
 BINARY_VER="0.7.2.3";  BINARY_VER_REGEXP="[0]\.[7]\."
                        # == 0.7.*
-TEXT_VER="1.2.1.1";    TEXT_VER_REGEXP="((1\.[012]\.)|(0\.([2-9]|(1[0-1]))\.))"
+TEXT_VER="1.2.1.3";    TEXT_VER_REGEXP="((1\.[012]\.)|(0\.([2-9]|(1[0-1]))\.))"
                        # >= 0.2 && < 1.3
-NETWORK_VER="2.6.2.0"; NETWORK_VER_REGEXP="2\.[0-6]\."
+NETWORK_VER="2.6.2.1"; NETWORK_VER_REGEXP="2\.[0-6]\."
                        # >= 2.0 && < 2.7
 NETWORK_URI_VER="2.6.0.3"; NETWORK_URI_VER_REGEXP="2\.6\."
                        # >= 2.6 && < 2.7
 CABAL_VER="1.23.0.0";  CABAL_VER_REGEXP="1\.23\."
                        # >= 1.23 && < 1.24
-TRANS_VER="0.4.2.0";   TRANS_VER_REGEXP="0\.[4]\."
+TRANS_VER="0.4.3.0";   TRANS_VER_REGEXP="0\.[4]\."
                        # >= 0.2.* && < 0.5
 MTL_VER="2.2.1";       MTL_VER_REGEXP="[2]\."
                        #  >= 2.0 && < 3
-HTTP_VER="4000.2.19";  HTTP_VER_REGEXP="4000\.2\.([5-9]|1[0-9]|2[0-9])"
+HTTP_VER="4000.2.22";  HTTP_VER_REGEXP="4000\.2\.([5-9]|1[0-9]|2[0-9])"
                        # >= 4000.2.5 < 4000.3
-ZLIB_VER="0.5.4.2";    ZLIB_VER_REGEXP="0\.[45]\."
+ZLIB_VER="0.6.1.1";    ZLIB_VER_REGEXP="0\.[4-6]\."
                        # == 0.4.* || == 0.5.*
 TIME_VER="1.5.0.1"     TIME_VER_REGEXP="1\.[12345]\.?"
                        # >= 1.1 && < 1.6
