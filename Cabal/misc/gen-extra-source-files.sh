515872c862dc34742dd5401fe0e4a7d53ef1a87d
Edward Z. Yang <ezyang@cs.stanford.edu>
2016-01-01 19:16:26 -0800

    Remove unnecessary collate
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- Cabal/misc/gen-extra-source-files.sh
+++ Cabal/misc/gen-extra-source-files.sh
@@ -4,7 +4,7 @@ set -ex
 find tests -type f \( -name '*.hs' -or -name '*.lhs' -or -name '*.c' -or -name '*.sh' \
     -or -name '*.cabal' -or -name '*.hsc' \) -and -not -regex ".*/dist/.*" \
     | awk '/Check.hs$|UnitTests|PackageTester|autogen|PackageTests.hs|CreatePipe/ { next } { print }' \
-    | LC_ALL=C LC_COLLATE=C sort \
+    | LC_ALL=C sort \
     | sed -e 's/^/  /' \
     > misc/source-file-list
 
