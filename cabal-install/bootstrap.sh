842b0372145164cb83c1f55de05f1a1d7a4df0b7
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2016-01-15 20:57:32 +0100

    Comment.
    [ci skip]

--- cabal-install/bootstrap.sh
+++ cabal-install/bootstrap.sh
@@ -190,7 +190,7 @@ NETWORK_VER="2.6.2.1"; NETWORK_VER_REGEXP="2\.[0-6]\."
 NETWORK_URI_VER="2.6.0.3"; NETWORK_URI_VER_REGEXP="2\.6\."
                        # >= 2.6 && < 2.7
 CABAL_VER="1.23.1.0";  CABAL_VER_REGEXP="1\.23\.[1-9]"
-                       # >= 1.23 && < 1.24
+                       # >= 1.23.1 && < 1.24
 TRANS_VER="0.5.0.0";   TRANS_VER_REGEXP="0\.[45]\."
                        # >= 0.2.* && < 0.6
 MTL_VER="2.2.1";       MTL_VER_REGEXP="[2]\."
