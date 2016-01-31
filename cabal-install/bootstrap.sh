10a3fc5225096c111fed2b2b6830cae36a4cc0c9
Ryan Thomas <ryan@ryant.org>
2016-02-01 09:00:57 +1100

    Bump the cabal-install version to 1.18.2.0

--- cabal-install/bootstrap.sh
+++ cabal-install/bootstrap.sh
@@ -53,7 +53,7 @@ PARSEC_VER="3.1.5";    PARSEC_VER_REGEXP="[23]\."              # == 2.* || == 3.
 DEEPSEQ_VER="1.3.0.2"; DEEPSEQ_VER_REGEXP="1\.[1-9]\."         # >= 1.1 && < 2
 TEXT_VER="1.1.0.0";    TEXT_VER_REGEXP="((1\.[01]\.)|(0\.([2-9]|(1[0-1]))\.))" # >= 0.2 && < 1.2
 NETWORK_VER="2.6.0.2"; NETWORK_VER_REGEXP="2\."                # == 2.*
-CABAL_VER="1.18.1.6";  CABAL_VER_REGEXP="1\.1[89]\."           # >= 1.18 && < 1.20
+CABAL_VER="1.18.1.7";  CABAL_VER_REGEXP="1\.1[89]\."           # >= 1.18 && < 1.20
 TRANS_VER="0.3.0.0";   TRANS_VER_REGEXP="0\.[23]\."            # >= 0.2.* && < 0.4.*
 MTL_VER="2.1.2";       MTL_VER_REGEXP="[2]\."                  #  == 2.*
 HTTP_VER="4000.2.16.1";  HTTP_VER_REGEXP="4000\.[012]\."         # == 4000.0.* || 4000.1.* || 4000.2.*
