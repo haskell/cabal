c029aef8fb3ca96481b0e9455cbf390ea22a3601
Herbert Valerio Riedel <hvr@gnu.org>
2016-02-14 13:49:00 +0100

    Enhance information exposed in plan.json
     - add top-level `cabal-version` and `cabal-lib-version` fields
       (provides quickndirty schema format-version info)
     - `compentId` (sic) field renamed to `id`
     - Add `flags` field with a mapping flags to booleans
     - Move `lib` to `components.lib`
     - Add support for showing dependencies for non-lib components
       (e.g. `components.exe:cabal`)

--- cabal-install/Distribution/Client/MultiPkg.hs
+++ cabal-install/Distribution/Client/MultiPkg.hs
@@ -34,6 +34,7 @@ import qualified Distribution.Simple.BuildTarget as Cabal
 import           Distribution.Simple.Utils hiding (matchFileGlob)
 import           Distribution.Verbosity
 import           Distribution.Text
+import qualified Paths_cabal_install (version)
 
 import qualified Data.Set as Set
 import qualified Data.Map as Map
@@ -503,32 +504,54 @@ encodePlanToJson _sharedPackageConfig elaboratedInstallPlan =
     --TODO: [nice to have] include all of the sharedPackageConfig and all of
     --      the parts of the elaboratedInstallPlan
     J.encodeToBuilder $
-      J.object [ "install-plan" J..= jsonIPlan ]
+      J.object [ "cabal-version"     J..= jdisplay Paths_cabal_install.version
+               , "cabal-lib-version" J..= jdisplay cabalVersion
+               , "install-plan"      J..= jsonIPlan
+               ]
   where
     jsonIPlan = map toJ (InstallPlan.toList elaboratedInstallPlan)
+
+    -- ipi :: InstalledPackageInfo
     toJ (InstallPlan.PreExisting ipi) =
+      -- installed packages currently lack configuration information
+      -- such as their flag settings or non-lib components.
+      --
+      -- TODO: how to find out whether package is "local"?
       J.object
-        [ "type"      J..= J.String "pre-existing"
-        , "compentId" J..= unCId (installedUnitId ipi)
-        , "lib"       J..= J.object
-                             [ "depends" J..= map unCId (installedDepends ipi)
-                             ]
+        [ "type"       J..= J.String "pre-existing"
+        , "id"         J..= jdisplay (installedUnitId ipi)
+        , "components" J..= J.object
+          [ "lib" J..= J.object [ "depends" J..= map jdisplay (installedDepends ipi) ] ]
         ]
 
+    -- ecp :: ElaboratedConfiguredPackage
     toJ (InstallPlan.Configured ecp) =
       J.object
-        [ "type"      J..= J.String "configured"
-        , "compentId" J..= (unCId . installedUnitId) ecp
-        , "lib"       J..= J.object
-                             [ "depends" J..= map unCId
-                                                (ComponentDeps.libraryDeps
-                                                  (depends ecp))
-                             ]
+        [ "type"       J..= J.String "configured"
+        , "id"         J..= (jdisplay . installedUnitId) ecp
+        , "components" J..= components
+        , "flags"      J..= J.object [ fn J..= v
+                                     | (PD.FlagName fn,v) <- pkgFlagAssignment ecp ]
         ]
+      where
+        components = J.object
+          [ comp2str c J..= J.object
+            [ "depends" J..= map (jdisplay . installedUnitId) v ]
+          | (c,v) <- ComponentDeps.toList (pkgDependencies ecp) ]
 
     toJ _ = error "encodePlanToJson: only expecting PreExisting and Configured"
 
-    unCId uid = J.String (display uid)
+    -- TODO: maybe move this helper to "ComponentDeps" module?
+    --       Or maybe define a 'Text' instance?
+    comp2str c = case c of
+        ComponentDeps.ComponentLib     -> "lib"
+        ComponentDeps.ComponentExe s   -> "exe:"   <> s
+        ComponentDeps.ComponentTest s  -> "test:"  <> s
+        ComponentDeps.ComponentBench s -> "bench:" <> s
+        ComponentDeps.ComponentSetup   -> "setup"
+
+    jdisplay :: Text a => a -> J.Value
+    jdisplay = J.String . display
 
 -- | Write a 'BB.Builder' to a file
 writeFileBB :: FilePath -> BB.Builder -> IO ()
