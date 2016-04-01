d2dd0fd3f5e3b38993959a4e88589ddbc3df500b
Kristen Kozak <grayjay@wordroute.com>
2016-04-01 11:10:42 -0700

    Avoid linking setup dependencies of linked packages
    When the solver links a package, it also merges the 'LinkGroup's of the
    package's direct dependencies.  This commit causes the solver to skip setup
    dependencies, which should remain independent of the package's other
    dependencies.

--- cabal-install/Distribution/Client/Dependency/Modular/Linking.hs
+++ cabal-install/Distribution/Client/Dependency/Modular/Linking.hs
@@ -32,7 +32,7 @@ import qualified Distribution.Client.Dependency.Modular.PSQ as P
 import qualified Distribution.Client.Dependency.Modular.ConflictSet as CS
 
 import Distribution.Client.Types (OptionalStanza(..))
-import Distribution.Client.ComponentDeps (Component)
+import Distribution.Client.ComponentDeps (Component(ComponentSetup))
 
 {-------------------------------------------------------------------------------
   Add linking
@@ -206,7 +206,7 @@ conflict = lift' . Left
 execUpdateState :: UpdateState () -> ValidateState -> Either Conflict ValidateState
 execUpdateState = execStateT . unUpdateState
 
-pickPOption :: QPN -> POption -> FlaggedDeps comp QPN -> UpdateState ()
+pickPOption :: QPN -> POption -> FlaggedDeps Component QPN -> UpdateState ()
 pickPOption qpn (POption i Nothing)    _deps = pickConcrete qpn i
 pickPOption qpn (POption i (Just pp'))  deps = pickLink     qpn i pp' deps
 
@@ -224,7 +224,7 @@ pickConcrete qpn@(Q pp _) i = do
       Just lg ->
         makeCanonical lg qpn i
 
-pickLink :: QPN -> I -> PP -> FlaggedDeps comp QPN -> UpdateState ()
+pickLink :: QPN -> I -> PP -> FlaggedDeps Component QPN -> UpdateState ()
 pickLink qpn@(Q _pp pn) i pp' deps = do
     vs <- get
 
@@ -279,10 +279,12 @@ makeCanonical lg qpn@(Q pp _) i =
 -- because having the direct dependencies in a link group means that we must
 -- have already made or will make sooner or later a link choice for one of these
 -- as well, and cover their dependencies at that point.
-linkDeps :: [Var QPN] -> PP -> FlaggedDeps comp QPN -> UpdateState ()
+linkDeps :: [Var QPN] -> PP -> FlaggedDeps Component QPN -> UpdateState ()
 linkDeps parents pp' = mapM_ go
   where
-    go :: FlaggedDep comp QPN -> UpdateState ()
+    go :: FlaggedDep Component QPN -> UpdateState ()
+    -- Skip setup dependencies.
+    go (Simple _ ComponentSetup) = return ()
     go (Simple (Dep qpn@(Q _ pn) _) _) = do
       vs <- get
       let qpn' = Q pp' pn
