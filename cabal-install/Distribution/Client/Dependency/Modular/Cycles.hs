503593c3c5a222ad6c2e168bb7443fd1d0309882
Edsko de Vries <edsko@well-typed.com>
2016-04-17 23:17:06 +0800

    Fix construction of conflict sets in cycle check
    The cycle check was not constructing the correct conflict sets; when we found a
    cycle, we constructed a conflict set consisting of all choices that we made so
    far, as long as those choices directly involved a package in the cycle. But
    sometimes the dependencies are more subtle than that. Now instead we construct
    conflict sets from all packages _and their goal reason chains_ that were
    involved in the cycle and then union all those.

--- cabal-install/Distribution/Client/Dependency/Modular/Cycles.hs
+++ cabal-install/Distribution/Client/Dependency/Modular/Cycles.hs
@@ -7,10 +7,9 @@ import Prelude hiding (cycle)
 import Control.Monad
 import Control.Monad.Reader
 import Data.Graph (SCC)
-import Data.Set   (Set)
+import Data.Map   (Map)
 import qualified Data.Graph       as Gr
 import qualified Data.Map         as Map
-import qualified Data.Set         as Set
 import qualified Data.Traversable as T
 
 #if !MIN_VERSION_base(4,8,0)
@@ -18,23 +17,22 @@ import Control.Applicative ((<$>))
 #endif
 
 import Distribution.Client.Dependency.Modular.Dependency
-import Distribution.Client.Dependency.Modular.Flag
 import Distribution.Client.Dependency.Modular.Package
 import Distribution.Client.Dependency.Modular.Tree
 import qualified Distribution.Client.Dependency.Modular.ConflictSet as CS
 
-type DetectCycles = Reader (ConflictSet QPN)
+type DetectCycles = Reader (Map QPN QGoalReasonChain)
 
 -- | Find and reject any solutions that are cyclic
 detectCyclesPhase :: Tree QGoalReasonChain -> Tree QGoalReasonChain
-detectCyclesPhase = (`runReader` CS.empty) .  cata go
+detectCyclesPhase = (`runReader` Map.empty) .  cata go
   where
     -- Most cases are simple; we just need to remember which choices we made
     go :: TreeF QGoalReasonChain (DetectCycles (Tree QGoalReasonChain)) -> DetectCycles (Tree QGoalReasonChain)
-    go (PChoiceF qpn gr     cs) = PChoice qpn gr     <$> local (CS.insert $ P qpn) (T.sequence cs)
-    go (FChoiceF qfn gr w m cs) = FChoice qfn gr w m <$> local (CS.insert $ F qfn) (T.sequence cs)
-    go (SChoiceF qsn gr w   cs) = SChoice qsn gr w   <$> local (CS.insert $ S qsn) (T.sequence cs)
-    go (GoalChoiceF         cs) = GoalChoice         <$>                           (T.sequence cs)
+    go (PChoiceF qpn gr     cs) = PChoice qpn gr     <$> local (Map.insert qpn gr) (T.sequence cs)
+    go (FChoiceF qfn gr w m cs) = FChoice qfn gr w m <$> T.sequence cs
+    go (SChoiceF qsn gr w   cs) = SChoice qsn gr w   <$> T.sequence cs
+    go (GoalChoiceF         cs) = GoalChoice         <$> T.sequence cs
     go (FailF cs reason)        = return $ Fail cs reason
 
     -- We check for cycles only if we have actually found a solution
@@ -49,26 +47,16 @@ detectCyclesPhase = (`runReader` CS.empty) .  cata go
 -- as the full conflict set containing all decisions that led to that 'Done'
 -- node, check if the solution is cyclic. If it is, return the conflict set
 -- containing all decisions that could potentially break the cycle.
-findCycles :: ConflictSet QPN -> RevDepMap -> Maybe (ConflictSet QPN)
-findCycles fullSet revDeps = do
+findCycles :: Map QPN QGoalReasonChain -> RevDepMap -> Maybe (ConflictSet QPN)
+findCycles grs revDeps = do
     guard $ not (null cycles)
-    return $ relevantConflictSet (Set.fromList (concat cycles)) fullSet
+    return $ CS.unions $ map (\(qpn, gr) -> toConflictSet $ Goal (P qpn) gr) $ head cycles
   where
-    cycles :: [[QPN]]
+    cycles :: [[(QPN, QGoalReasonChain)]]
     cycles = [vs | Gr.CyclicSCC vs <- scc]
 
-    scc :: [SCC QPN]
+    scc :: [SCC (QPN, QGoalReasonChain)]
     scc = Gr.stronglyConnComp . map aux . Map.toList $ revDeps
 
-    aux :: (QPN, [(comp, QPN)]) -> (QPN, QPN, [QPN])
-    aux (fr, to) = (fr, fr, map snd to)
-
--- | Construct the relevant conflict set given the full conflict set that
--- lead to this decision and the set of packages involved in the cycle
-relevantConflictSet :: Set QPN -> ConflictSet QPN -> ConflictSet QPN
-relevantConflictSet cycle = CS.filter isRelevant
-  where
-    isRelevant :: Var QPN -> Bool
-    isRelevant (P qpn)                  = qpn `Set.member` cycle
-    isRelevant (F (FN (PI qpn _i) _fn)) = qpn `Set.member` cycle
-    isRelevant (S (SN (PI qpn _i) _sn)) = qpn `Set.member` cycle
+    aux :: (QPN, [(comp, QPN)]) -> ((QPN, QGoalReasonChain), QPN, [QPN])
+    aux (fr, to) = ((fr, grs Map.! fr), fr, map snd to)
