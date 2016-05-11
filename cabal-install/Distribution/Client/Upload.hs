f5d9221f050eb654aa94dc7d36afe3476f2125fa
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2016-05-11 10:33:06 +0200

    upload: more detailed message on successful upload
    The message now includes a link to the package page and says
    whether the package was uploaded as a candidate or not.

--- cabal-install/Distribution/Client/Upload.hs
+++ cabal-install/Distribution/Client/Upload.hs
@@ -21,10 +21,10 @@ import Network.HTTP (Header(..), HeaderName(..))
 import System.IO        (hFlush, stdin, stdout, hGetEcho, hSetEcho)
 import System.Exit      (exitFailure)
 import Control.Exception (bracket)
-import System.FilePath  ((</>), takeExtension, takeFileName)
+import System.FilePath  ((</>), takeExtension, takeFileName, dropExtension)
 import qualified System.FilePath.Posix as FilePath.Posix ((</>))
 import System.Directory
-import Control.Monad (forM_, when)
+import Control.Monad (forM_, when, foldM)
 import Data.Maybe (catMaybes)
 import Data.Char (isSpace)
 
@@ -33,6 +33,13 @@ type Auth = Maybe (String, String)
 checkURI :: URI
 Just checkURI = parseURI $ "http://hackage.haskell.org/cgi-bin/"
                            ++ "hackage-scripts/check-pkg"
+
+stripExtensions :: [String] -> FilePath -> Maybe String
+stripExtensions exts path = foldM f path (reverse exts)
+ where
+  f p e
+    | takeExtension p == '.':e = Just (dropExtension p)
+    | otherwise = Nothing
 
 upload :: Verbosity -> RepoContext
        -> Maybe Username -> Maybe Password -> Bool -> [FilePath]
@@ -52,12 +59,22 @@ upload verbosity repoCtxt mUsername mPassword candidate paths = do
                 then "packages/candidates"
                 else "upload"
         }
+        packageURI pkgid = targetRepoURI {
+            uriPath = rootIfEmpty (uriPath targetRepoURI) FilePath.Posix.</> mconcat
+              [ "package/", pkgid
+              , if candidate then "/candidate" else ""
+              ]
+        }
     Username username <- maybe promptUsername return mUsername
     Password password <- maybe promptPassword return mPassword
     let auth = Just (username,password)
     forM_ paths $ \path -> do
       notice verbosity $ "Uploading " ++ path ++ "... "
-      handlePackage transport verbosity uploadURI auth path
+      case fmap takeFileName (stripExtensions ["tar", "gz"] path) of
+        Just pkgid -> handlePackage transport verbosity uploadURI (packageURI pkgid) auth candidate path
+        -- This case shouldn't really happen, since we check in Main that we only pass tar.gz files
+        -- to upload.
+        Nothing -> die $ "Not a tar.gz file: " ++ path
 
 uploadDoc :: Verbosity -> RepoContext
           -> Maybe Username -> Maybe Password -> Bool -> FilePath
@@ -153,23 +170,29 @@ check verbosity repoCtxt paths = do
     transport <- repoContextGetTransport repoCtxt
     forM_ paths $ \path -> do
       notice verbosity $ "Checking " ++ path ++ "... "
-      handlePackage transport verbosity checkURI Nothing path
+      handlePackage transport verbosity checkURI checkURI Nothing False path
 
-handlePackage :: HttpTransport -> Verbosity -> URI -> Auth
-              -> FilePath -> IO ()
-handlePackage transport verbosity uri auth path =
+handlePackage :: HttpTransport -> Verbosity -> URI -> URI -> Auth
+              -> Bool -> FilePath -> IO ()
+handlePackage transport verbosity uri packageUri auth candidate path =
   do resp <- postHttpFile transport verbosity uri path auth
      case resp of
-       (code,warnings) | code `elem` [200, 204] -> do
-          notice verbosity $ "Ok. " ++ formatWarnings (trim warnings)
+       (code,warnings) | code `elem` [200, 204] ->
+          notice verbosity $ okMessage ++
+            if null warnings then "" else "\n" ++ formatWarnings (trim warnings)
        (code,err)  -> do
           notice verbosity $ "Error uploading " ++ path ++ ": "
                           ++ "http code " ++ show code ++ "\n"
                           ++ err
           exitFailure
+ where
+  okMessage
+    | candidate =
+        "Package successfully uploaded as candidate. You can now preview the \
+        \result at " ++ show packageUri ++ ". To publish the candidate, use cabal upload --publish."
+    | otherwise = "Package successfully published. You can now view it at " ++ show packageUri ++ "."
 
 formatWarnings :: String -> String
-formatWarnings "" = ""
 formatWarnings x = "Warnings:\n" ++ (unlines . map ("- " ++) . lines) x
 
 -- Trim
