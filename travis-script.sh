e848a1ccca80af0d9573b0f58dd3410e108fce3c
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2016-05-28 23:22:47 +0200

    travis-script.sh: respect -j in install_from_tarball()
    I didn't modify this when I added -j support, but @23Skidoo agreed it
    should be changed, so we do it now.

--- travis-script.sh
+++ travis-script.sh
@@ -144,7 +144,7 @@ install_from_tarball() {
    SRC_TGZ=$(cabal info . | awk '{print $2 ".tar.gz";exit}') ;
    export SRC_TGZ
    if [ -f "dist/$SRC_TGZ" ]; then
-      cabal install -j1 "dist/$SRC_TGZ" -v2;
+      cabal install $jobs "dist/$SRC_TGZ" -v2;
    else
       echo "expected 'dist/$SRC_TGZ' not found";
       exit 1;
