182bbabea1f40dc6a215856c59553eb3c22ad5f4
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2016-07-17 10:34:31 +0200

    Unbreak CI for PRs.

---
+++ travis-deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+setup() {
+    git clone --depth=50 --branch=gh-pages \
+        https://github.com/haskell/cabal-website.git ../cabal-website
+    cd ../cabal-website
+    openssl aes-256-cbc -K $encrypted_edaf6551664d_key \
+            -iv $encrypted_edaf6551664d_iv \
+            -in id_ed25519_cabal_website.aes256.enc -out id_ed25519 -d
+    mv id_ed25519 ~/.ssh/id_ed25519
+    chmod 400 ~/.ssh/id_ed25519
+    cd -
+}
+
+deploy() {
+    git config --global user.email "builds@travis-ci.org"
+    git config --global user.name "Travis CI User"
+    mkdir -p ../cabal-website/doc/html
+    mv Cabal/dist/doc/html/Cabal ../cabal-website/doc/html/Cabal
+    cd ../cabal-website
+    git add .
+    git commit --amend --reset-author -m "Deploy to GitHub ($(date))."
+    git push --force --quiet \
+               git@github.com:haskell/haskell-website.git gh-pages
+}
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]
+then
+    case "${1}" in
+        "setup")
+            setup
+            ;;
+        "deploy")
+            deploy
+            ;;
+        *)
+            echo Unknown command!
+            ;;
+    esac
+fi
