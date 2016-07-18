99913a91c66cadf5a24bbbd475baf0fe3a8c0f57
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2016-07-18 23:01:08 +0200

    travis-deploy.sh: Copy-pasto.

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -5,9 +5,9 @@ deploy() {
     git config --global user.email "builds@travis-ci.org"
     git config --global user.name "Travis CI User"
     git clone https://github.com/haskell/cabal-website.git ../cabal-website
+    cd ../cabal-website
     git checkout --track -b gh-pages origin/gh-pages
     cd -
-    cd ../cabal-website
     mkdir -p ../cabal-website/doc/html
     mv Cabal/dist/doc/html/Cabal ../cabal-website/doc/html/Cabal
     cd ../cabal-website
