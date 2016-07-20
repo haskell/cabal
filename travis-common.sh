851600e601e43e76c77cb06c7b7328bbac3e8d48
Edward Z. Yang <ezyang@cs.stanford.edu>
2016-07-19 17:59:38 -0700

    Rewrite Travis CI script.
    Lots of changes:
    
        - When possible, we use the container infrastructure (sudo: false)
          rather than Google Compute Engine infrastructure (sudo: required).
          Unfortunately, we can't use GCE for the Linux builds, where
          reduced RAM available hoses are GHC build.
    
        - Switched from using ./Setup and old-style cabal to new-build.
          There are numerous great benefits but the best is that
          .cabal/store can be cached on Travis, leading to huge speedups
          on the build.  Downside is we need to string-and-ceiling-wax
          support for test/haddock/etc.
    
        - I stopped bootstrapping on every build we do; instead there
          is a separate bootstrap build we do to make sure that that
          is working.  This also speeds up the basic builds since
          we are not building Cabal/cabal-install multiple times.
    
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

---
+++ travis-common.sh
@@ -0,0 +1,17 @@
+set -e
+
+CABAL_VERSION="1.25.0.0"
+
+# ---------------------------------------------------------------------
+# Timing / diagnostic output
+# ---------------------------------------------------------------------
+
+timed() {
+    echo "\$ $*"
+    start_time=$(date +%s)
+    $*
+    end_time=$(date +%s)
+    duration=$((end_time - start_time))
+    echo "$* took $duration seconds."
+    echo "----"
+}
---
+++ travis-meta.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+. ./travis-common.sh
+
+# ---------------------------------------------------------------------
+# Check that auto-generated files/fields are up to date.
+# ---------------------------------------------------------------------
+
+# Regenerate the CONTRIBUTORS file.
+# Currently doesn't work because Travis uses --depth=50 when cloning.
+#./Cabal/misc/gen-authors.sh > AUTHORS
+
+# Regenerate the 'extra-source-files' field in Cabal.cabal.
+(cd Cabal && timed ./misc/gen-extra-source-files.sh Cabal.cabal)
+
+# Regenerate the 'extra-source-files' field in cabal-install.cabal.
+(cd cabal-install && ../Cabal/misc/gen-extra-source-files.sh cabal-install.cabal)
+
+# Fail if the diff is not empty.
+timed ./Cabal/misc/travis-diff-files.sh
