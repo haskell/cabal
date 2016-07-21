019ef6f6e92bede745e20d96274d5aa7f717f87b
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2016-07-21 08:48:59 +0200

    Attempt to unbreak doc uploading.

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -4,16 +4,13 @@ set -ex
 deploy() {
     git config --global user.email "builds@travis-ci.org"
     git config --global user.name "Travis CI User"
-    git clone https://github.com/haskell/cabal-website.git ../cabal-website
-    cd ../cabal-website
-    git checkout --track -b gh-pages origin/gh-pages
-    cd -
-    mkdir -p ../cabal-website/doc/html
-    mv Cabal/dist/doc/html/Cabal ../cabal-website/doc/html/Cabal
-    cd ../cabal-website
-    git add --all .
-    git commit --amend --reset-author -m "Deploy to GitHub ($(date))."
-    git push --force git@github.com:haskell/cabal-website.git gh-pages:gh-pages
+    git clone https://github.com/haskell/cabal-website.git cabal-website
+    (cd cabal-website && git checkout --track -b gh-pages origin/gh-pages)
+    mkdir -p cabal-website/doc/html
+    mv Cabal/dist/doc/html/Cabal cabal-website/doc/html/Cabal
+    (cd cabal-website && git add --all .)
+    (cd cabal-website && git commit --amend --reset-author -m "Deploy to GitHub ($(date)).")
+    (cd cabal-website && git push --force git@github.com:haskell/cabal-website.git gh-pages:gh-pages)
 }
 
 if [ "x$TRAVIS_PULL_REQUEST" = "xfalse" -a "x$TRAVIS_BRANCH" = "xmaster" \
