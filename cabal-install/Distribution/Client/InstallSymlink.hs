f1f287bbf341b797da7d1f7f51e77a4c677d422b
Duncan Coutts <duncan@community.haskell.org>
2016-07-24 12:16:40 +0100

    Convert the old install code to use InstallPlan.execute
    Redefine the local executeInstallPlan util in terms of
    InstallPlan.execute. Of course now executeInstallPlan returns
    BuildResults instead of an upated InstallPlan. This requires changes in
    all the code that looks at the result of the build execution, including
    console error reporting, build reports, symlinks etc.

--- cabal-install/Distribution/Client/InstallSymlink.hs
+++ cabal-install/Distribution/Client/InstallSymlink.hs
@@ -38,8 +38,7 @@ symlinkBinary _ _ _ _ = fail "Symlinking feature not available on Windows"
 #else
 
 import Distribution.Client.Types
-         ( GenericReadyPackage(..), ReadyPackage, enableStanzas
-         , ConfiguredPackage(..))
+         ( ConfiguredPackage(..), BuildResults, enableStanzas )
 import Distribution.Client.Setup
          ( InstallFlags(installSymlinkBinDir) )
 import qualified Distribution.Client.InstallPlan as InstallPlan
@@ -105,8 +104,9 @@ symlinkBinaries :: Platform -> Compiler
                 -> ConfigFlags
                 -> InstallFlags
                 -> InstallPlan
+                -> BuildResults
                 -> IO [(PackageIdentifier, String, FilePath)]
-symlinkBinaries platform comp configFlags installFlags plan =
+symlinkBinaries platform comp configFlags installFlags plan buildResults =
   case flagToMaybe (installSymlinkBinDir installFlags) of
     Nothing            -> return []
     Just symlinkBinDir
@@ -135,15 +135,17 @@ symlinkBinaries platform comp configFlags installFlags plan =
   where
     exes =
       [ (cpkg, pkg, exe)
-      | InstallPlan.Installed cpkg _ _ <- InstallPlan.toList plan
-      , let pkg   = pkgDescription cpkg
+      | InstallPlan.Configured cpkg <- InstallPlan.toList plan
+      , case InstallPlan.lookupBuildResult cpkg buildResults of
+          Just (Right _success) -> True
+          _                     -> False
+      , let pkg :: PackageDescription
+            pkg = pkgDescription cpkg
       , exe <- PackageDescription.executables pkg
       , PackageDescription.buildable (PackageDescription.buildInfo exe) ]
 
-    pkgDescription :: ReadyPackage -> PackageDescription
-    pkgDescription (ReadyPackage (ConfiguredPackage
-                                    _ (SourcePackage _ pkg _ _)
-                                    flags stanzas _)) =
+    pkgDescription (ConfiguredPackage _ (SourcePackage _ pkg _ _)
+                                      flags stanzas _) =
       case finalizePackageDescription flags
              (const True)
              platform cinfo [] (enableStanzas stanzas pkg) of
