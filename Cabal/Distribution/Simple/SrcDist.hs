2e543d37733a0438466e6b0804fc777032ada844
fmaste <fmaste@users.noreply.github.com>
2016-08-11 19:13:22 +0000

    Not used

--- Cabal/Distribution/Simple/SrcDist.hs
+++ Cabal/Distribution/Simple/SrcDist.hs
@@ -77,7 +77,7 @@ sdist pkg mb_lbi flags mkTmpDir pps =
 
   -- When given --list-sources, just output the list of sources to a file.
   case (sDistListSources flags) of
-    Flag path -> sdistListSources path pkg mb_lbi flags mkTmpDir pps
+    Flag path -> sdistListSources path pkg flags mkTmpDir pps
     NoFlag    -> do
       -- do some QA
       printPackageProblems verbosity pkg
@@ -120,12 +120,11 @@ sdist pkg mb_lbi flags mkTmpDir pps =
 -- |Create a file with the path of each source line by line.
 sdistListSources :: FilePath    -- ^ output file
       -> PackageDescription     -- ^information from the tarball
-      -> Maybe LocalBuildInfo   -- ^Information from configure
       -> SDistFlags             -- ^verbosity & snapshot
       -> (FilePath -> FilePath) -- ^build prefix (temp dir)
       -> [PPSuffixHandler]      -- ^ extra preprocessors (includes suffixes)
       -> IO ()
-sdistListSources path pkg mb_lbi flags mkTmpDir pps =
+sdistListSources path pkg flags mkTmpDir pps =
   withFile path WriteMode $ \outHandle -> do
     (ordinary, maybeExecutable) <- listPackageSources verbosity pkg pps
     traverse_ (hPutStrLn outHandle) ordinary
