c7f8ca63820dfd77188fb756de001a107186afbb
Herbert Valerio Riedel <hvr@gnu.org>
2016-08-22 22:30:43 +0200

    Make ComponentDeps.zip compatible with containers<0.5

--- cabal-install/Distribution/Solver/Types/ComponentDeps.hs
+++ cabal-install/Distribution/Solver/Types/ComponentDeps.hs
@@ -124,6 +124,8 @@ insert comp a = ComponentDeps . Map.alter aux comp . unComponentDeps
 -- | Zip two 'ComponentDeps' together by 'Component', using 'mempty'
 -- as the neutral element when a 'Component' is present only in one.
 zip :: (Monoid a, Monoid b) => ComponentDeps a -> ComponentDeps b -> ComponentDeps (a, b)
+{- TODO/FIXME: Once we can expect containers>=0.5, switch to the more efficient version below:
+
 zip (ComponentDeps d1) (ComponentDeps d2) =
     ComponentDeps $
       Map.mergeWithKey
@@ -131,6 +133,15 @@ zip (ComponentDeps d1) (ComponentDeps d2) =
         (fmap (\a -> (a, mempty)))
         (fmap (\b -> (mempty, b)))
         d1 d2
+
+-}
+zip (ComponentDeps d1) (ComponentDeps d2) =
+    ComponentDeps $
+      Map.unionWith
+        mappend
+        (Map.map (\a -> (a, mempty)) d1)
+        (Map.map (\b -> (mempty, b)) d2)
+
 
 -- | Keep only selected components (and their associated deps info).
 filterDeps :: (Component -> a -> Bool) -> ComponentDeps a -> ComponentDeps a
