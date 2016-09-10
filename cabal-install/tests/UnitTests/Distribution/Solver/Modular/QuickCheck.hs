553959f808eaef84602de773cf92d718906fdde0
Kristen Kozak <grayjay@wordroute.com>
2016-09-10 13:30:45 -0700

    Evaluate solver result in quickcheck tests to force install plan validation.
    Previously, the tests only evaluated enough of the result to determine whether
    the solver found an install plan, but that install plan could be invalid.

--- cabal-install/tests/UnitTests/Distribution/Solver/Modular/QuickCheck.hs
+++ cabal-install/tests/UnitTests/Distribution/Solver/Modular/QuickCheck.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module UnitTests.Distribution.Solver.Modular.QuickCheck (tests) where
 
+import Control.DeepSeq (NFData, force)
 import Control.Monad (foldM)
 import Data.Either (lefts)
 import Data.Function (on)
 import Data.List (groupBy, isInfixOf, nub, nubBy, sort)
 import Data.Maybe (isJust)
+import GHC.Generics (Generic)
 
 #if !MIN_VERSION_base(4,8,0)
 import Control.Applicative ((<$>), (<*>))
@@ -111,7 +114,10 @@ solve enableBj reorder indep solver targets (TestDb db) =
         | otherwise                                = OtherFailure
   in Result {
        resultLog = lg
-     , resultPlan = either (Left . failure) (Right . extractInstallPlan) result
+     , resultPlan =
+         -- Force the result so that we check for internal errors when we check
+         -- for success or failure. See D.C.Dependency.validateSolverResult.
+         force $ either (Left . failure) (Right . extractInstallPlan) result
      }
 
 -- | How to modify the order of the input targets.
@@ -130,7 +136,9 @@ data Result = Result {
   }
 
 data Failure = BackjumpLimitReached | OtherFailure
-  deriving (Eq, Show)
+  deriving (Eq, Generic, Show)
+
+instance NFData Failure
 
 -- | Package name.
 newtype PN = PN { unPN :: String }
