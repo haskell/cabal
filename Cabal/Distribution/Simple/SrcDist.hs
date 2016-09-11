850a06a63ed6e57707919a06595cb390406bbb64
Lennart Kolmodin <kolmodin@gmail.com>
2016-09-11 19:09:53 +0200

    Use isRelative for sdist, not isRelativeOnAnyPlatform.

--- Cabal/Distribution/Simple/SrcDist.hs
+++ Cabal/Distribution/Simple/SrcDist.hs
@@ -63,7 +63,7 @@ import qualified Data.Map as Map
 import Data.Time (UTCTime, getCurrentTime, toGregorian, utctDay)
 import System.Directory ( doesFileExist )
 import System.IO (IOMode(WriteMode), hPutStrLn, withFile)
-import System.FilePath ((</>), (<.>), dropExtension)
+import System.FilePath ((</>), (<.>), dropExtension, isRelative)
 
 -- |Create a source distribution.
 sdist :: PackageDescription     -- ^information from the tarball
@@ -216,7 +216,7 @@ listPackageSourcesOrdinary verbosity pkg_descr pps =
   , fmap concat
     . withAllLib $ \ l -> do
        let lbi = libBuildInfo l
-           relincdirs = "." : filter isRelativeOnAnyPlatform (includeDirs lbi)
+           relincdirs = "." : filter isRelative (includeDirs lbi)
        traverse (fmap snd . findIncludeFile relincdirs) (installIncludes lbi)
 
     -- Setup script, if it exists.
