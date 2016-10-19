af2c328529cd632430729b6bcbf9a1db402ef364
Edward Z. Yang <ezyang@cs.stanford.edu>
2016-10-18 17:06:45 -0700

    Put PackageTests.Tests in the correct spot, and fix up update-cabal-files.sh.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- update-cabal-files.sh
+++ update-cabal-files.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-(cd Cabal; misc/gen-extra-source-files.sh Cabal.cabal)
-(cd cabal-install; ../Cabal/misc/gen-extra-source-files.sh cabal-install.cabal)
+(cd Cabal; misc/gen-extra-source-files.hs Cabal.cabal)
+(cd cabal-install; ../Cabal/misc/gen-extra-source-files.hs cabal-install.cabal)
