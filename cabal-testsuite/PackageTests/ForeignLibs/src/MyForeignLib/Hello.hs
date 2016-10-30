172d283dff9396cf712cd03061e970a2b35df7cc
Auke Booij <auke@tulcod.com>
2016-10-30 19:42:05 +0000

    Add test for foreign libs to depend on Haskell libs
    Also bump version requirements for foreign libs (ghc>=7.8)

--- cabal-testsuite/PackageTests/ForeignLibs/src/MyForeignLib/Hello.hs
+++ cabal-testsuite/PackageTests/ForeignLibs/src/MyForeignLib/Hello.hs
@@ -2,9 +2,12 @@
 module MyForeignLib.Hello (sayHi) where
 
 import MyForeignLib.SomeBindings
+import MyForeignLib.AnotherVal
 
 foreign export ccall sayHi :: IO ()
 
 -- | Say hi!
 sayHi :: IO ()
-sayHi = putStrLn $ "Hi from a foreign library! Foo has value " ++ show valueOfFoo
+sayHi = putStrLn $
+     "Hi from a foreign library! Foo has value " ++ show valueOfFoo
+  ++ " and anotherVal has value " ++ show anotherVal
