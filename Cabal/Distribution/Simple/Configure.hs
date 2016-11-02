db38c036f7286058594f5d92db5a3f467b44d763
Auke Booij <auke@tulcod.com>
2016-11-02 00:40:18 +0000

    Make new elf-version field Linux-only

--- Cabal/Distribution/Simple/Configure.hs
+++ Cabal/Distribution/Simple/Configure.hs
@@ -1897,6 +1897,9 @@ checkForeignLibSupported comp platform flib = go (compilerFlavor comp)
       | not (null (foreignLibModDefFile flib)) = unsupported [
             "Module definition file not supported on OSX"
           ]
+      | not (null (foreignLibELFVersion flib)) = unsupported [
+            "Foreign library versioning not currently supported on OSX"
+          ]
       | otherwise =
           Nothing
     goGhcOsx _ = unsupported [
@@ -1924,6 +1927,10 @@ checkForeignLibSupported comp platform flib = go (compilerFlavor comp)
           , "  if os(Windows)\n"
           , "    options: standalone\n"
           , "in your foreign-library stanza."
+          ]
+      | not (null (foreignLibELFVersion flib)) = unsupported [
+            "Foreign library versioning not currently supported on Windows.\n"
+          , "You can specify module definition files in the mod-def-file field."
           ]
       | otherwise =
          Nothing
