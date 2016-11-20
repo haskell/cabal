ff7c944f245f4343a32bca9d7ab99f5488e7a1b3
Duncan Coutts <duncan@community.haskell.org>
2016-11-20 18:04:39 +0000

    Rename BuildTarget to TargetSelector
    We're going to generalise it and the name will no longer be appropriate.
    We'll add the ability to select all packages, or to select only certain
    kinds of components in a package. So this makes it less like a resolved
    target and more or an expression that needs further work to resolve to
    a set of targets.

--- cabal-install/Distribution/Client/CmdBench.hs
+++ cabal-install/Distribution/Client/CmdBench.hs
@@ -129,7 +129,7 @@ benchAction (configFlags, configExFlags, installFlags, haddockFlags)
 -- For bench: select all buildable benchmarks
 -- Fail if there are no benchmarks or no buildable benchmarks.
 --
-selectPackageTargets :: BuildTarget PackageId
+selectPackageTargets :: TargetSelector PackageId
                      -> [AvailableTarget k] -> Either BenchTargetProblem [k]
 selectPackageTargets _bt ts
   | (_:_)  <- benchts    = Right benchts
@@ -142,7 +142,7 @@ selectPackageTargets _bt ts
     allbenchts'= [ fmap (const ()) t | t <- allbenchts ]
 
 
-selectComponentTarget :: BuildTarget PackageId
+selectComponentTarget :: TargetSelector PackageId
                       -> AvailableTarget k -> Either BenchTargetProblem  k
 selectComponentTarget bt t
   | CBenchName _ <- availableTargetComponentName t
