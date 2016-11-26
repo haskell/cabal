282d73781db08810a09959ea899aecfa04d21964
Edward Z. Yang <ezyang@cs.stanford.edu>
2016-11-25 19:50:31 -0500

    Port custom-setup/custom_setup_without_Cabal_doesnt_allow_Cabal_import to new test suite
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

---
+++ cabal-testsuite/PackageTests/CustomWithoutCabalDefaultMain/cabal.test.hs
@@ -0,0 +1,10 @@
+import Test.Cabal.Prelude
+main = cabalTest $ do
+    r <- fails $ cabal' "new-build" []
+    assertRegex "Should not have been able to import Cabal"
+                "(Could not find module|Failed to load interface for).*Distribution\\.Simple" r
+    -- When using --with-ghc, this message is not necessarily output
+    has_cabal <- hasCabalForGhc
+    when has_cabal $
+        assertRegex "It is a member of the hidden package .*Cabal-"
+                    "It is a member of the hidden package" r
