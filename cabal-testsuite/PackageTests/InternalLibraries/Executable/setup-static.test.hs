99d0bdf7d2559462ef43bdead9b893a24666c1c4
Edward Z. Yang <ezyang@cs.stanford.edu>
2016-11-27 01:21:45 -0800

    Reuse packagedb in InternalLibraries/Executable/setup-static.test.hs
    Otherwise, ghc-pkg will complain that it's reinitializing the
    package database.
    
    Possibly there is some refactor to make withPackageDb more robust
    if it is called multiple times.
    
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- cabal-testsuite/PackageTests/InternalLibraries/Executable/setup-static.test.hs
+++ cabal-testsuite/PackageTests/InternalLibraries/Executable/setup-static.test.hs
@@ -18,34 +18,36 @@ import System.Directory
 -- don't get installed, so this test doesn't work on Windows.)
 main = setupAndCabalTest $ do
     skipUnless =<< hasSharedLibraries
-    forM_ [False, True] $ \is_dynamic -> withPackageDb $ do
-        setup_install $ [ if is_dynamic then "--enable-executable-dynamic"
-                                        else "--disable-executable-dynamic"
-                        , "--enable-shared"]
-        dist_dir <- fmap testDistDir getTestEnv
-        lbi <- liftIO $ getPersistBuildConfig dist_dir
-        let pkg_descr = localPkgDescr lbi
-            compiler_id = compilerId (compiler lbi)
-            cname = CSubLibName $ mkUnqualComponentName "foo-internal"
-            [target] = componentNameTargets' pkg_descr lbi cname
-            uid = componentUnitId (targetCLBI target)
-            InstallDirs{libdir=dir,dynlibdir=dyndir} =
-              absoluteComponentInstallDirs pkg_descr lbi uid NoCopyDest
-        assertBool "interface files should be installed"
-            =<< liftIO (doesFileExist (dir </> "Foo.hi"))
-        assertBool "static library should be installed"
-            =<< liftIO (doesFileExist (dir </> mkLibName uid))
-        if is_dynamic
-          then
-            assertBool "dynamic library MUST be installed"
-                =<< liftIO (doesFileExist (dyndir </> mkSharedLibName
-                                           compiler_id uid))
-          else
-            assertBool "dynamic library should be installed"
-                =<< liftIO (doesFileExist (dyndir </> mkSharedLibName
-                                           compiler_id uid))
-        fails $ ghcPkg "describe" ["foo"]
-        -- clean away the dist directory so that we catch accidental
-        -- dependence on the inplace files
-        setup "clean" []
-        runInstalledExe' "foo" [] >>= assertOutputContains "46"
+    withPackageDb $ do
+        -- MULTI
+        forM_ [False, True] $ \is_dynamic -> do
+            setup_install $ [ if is_dynamic then "--enable-executable-dynamic"
+                                            else "--disable-executable-dynamic"
+                            , "--enable-shared"]
+            dist_dir <- fmap testDistDir getTestEnv
+            lbi <- liftIO $ getPersistBuildConfig dist_dir
+            let pkg_descr = localPkgDescr lbi
+                compiler_id = compilerId (compiler lbi)
+                cname = CSubLibName $ mkUnqualComponentName "foo-internal"
+                [target] = componentNameTargets' pkg_descr lbi cname
+                uid = componentUnitId (targetCLBI target)
+                InstallDirs{libdir=dir,dynlibdir=dyndir} =
+                  absoluteComponentInstallDirs pkg_descr lbi uid NoCopyDest
+            assertBool "interface files should be installed"
+                =<< liftIO (doesFileExist (dir </> "Foo.hi"))
+            assertBool "static library should be installed"
+                =<< liftIO (doesFileExist (dir </> mkLibName uid))
+            if is_dynamic
+              then
+                assertBool "dynamic library MUST be installed"
+                    =<< liftIO (doesFileExist (dyndir </> mkSharedLibName
+                                               compiler_id uid))
+              else
+                assertBool "dynamic library should be installed"
+                    =<< liftIO (doesFileExist (dyndir </> mkSharedLibName
+                                               compiler_id uid))
+            fails $ ghcPkg "describe" ["foo"]
+            -- clean away the dist directory so that we catch accidental
+            -- dependence on the inplace files
+            setup "clean" []
+            runInstalledExe' "foo" [] >>= assertOutputContains "46"
