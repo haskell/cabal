d6bcca7a608b91d0013bccaab984d036c91ae2fe
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2016-12-07 01:19:08 +0000

    Fix a message.
    [ci skip]

--- cabal-install/bootstrap.sh
+++ cabal-install/bootstrap.sh
@@ -422,7 +422,7 @@ do_Cabal_pkg () {
             echo "Cabal-${CABAL_VER} will be installed from the local Git clone."
             (cd ../Cabal && install_pkg ${CABAL_VER} ${CABAL_VER_REGEXP})
         else
-            echo "Cabal-${CABAL_VER} is already installed and the version is ok."
+            echo "Cabal is already installed and the version is ok."
         fi
     else
         info_pkg "Cabal"        ${CABAL_VER}   ${CABAL_VER_REGEXP}
