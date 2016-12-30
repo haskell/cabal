b758e1ad027e2a8bda7f473e95975441393ed2ea
Thomas Tuegel <ttuegel@gmail.com>
2016-12-29 20:48:34 -0600

    update Nix expressions

--- cabal-install/default.nix
+++ cabal-install/default.nix
@@ -1,22 +1,22 @@
 { mkDerivation, array, async, base, base16-bytestring, binary
 , bytestring, Cabal, containers, cryptohash-sha256, deepseq
-, directory, filepath, hackage-security, hashable, HTTP, mtl
-, network, network-uri, pretty, pretty-show, process, QuickCheck
-, random, regex-posix, stdenv, stm, tagged, tar, tasty, tasty-hunit
-, tasty-quickcheck, time, unix, zlib
+, directory, echo, edit-distance, filepath, hackage-security
+, hashable, HTTP, mtl, network, network-uri, pretty, pretty-show
+, process, QuickCheck, random, regex-posix, stdenv, stm, tagged
+, tar, tasty, tasty-hunit, tasty-quickcheck, time, unix, zlib
 }:
 mkDerivation {
   pname = "cabal-install";
   version = "1.25.0.0";
-  src = ./cabal-install;
+  src = ./.;
   isLibrary = false;
   isExecutable = true;
   setupHaskellDepends = [ base Cabal filepath process ];
   executableHaskellDepends = [
     array async base base16-bytestring binary bytestring Cabal
-    containers cryptohash-sha256 deepseq directory filepath
-    hackage-security hashable HTTP mtl network network-uri pretty
-    process random stm tar time unix zlib
+    containers cryptohash-sha256 deepseq directory echo edit-distance
+    filepath hackage-security hashable HTTP mtl network network-uri
+    pretty process random stm tar time unix zlib
   ];
   testHaskellDepends = [
     array async base base16-bytestring binary bytestring Cabal
@@ -25,6 +25,7 @@ mkDerivation {
     pretty-show process QuickCheck random regex-posix stm tagged tar
     tasty tasty-hunit tasty-quickcheck time unix zlib
   ];
+  doCheck = false;
   postInstall = ''
     mkdir $out/etc
     mv bash-completion $out/etc/bash_completion.d
