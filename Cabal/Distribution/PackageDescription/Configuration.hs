238c1aa60f281e63e7e81c86a288e53c037effbc
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-01-02 22:31:13 -0500

    Move DependencyMap to its own module.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- Cabal/Distribution/PackageDescription/Configuration.hs
+++ Cabal/Distribution/PackageDescription/Configuration.hs
@@ -54,6 +54,7 @@ import Distribution.Types.Dependency
 import Distribution.Types.UnqualComponentName
 import Distribution.Types.CondTree
 import Distribution.Types.Condition
+import Distribution.Types.DependencyMap
 
 import qualified Data.Map as Map
 import Data.Tree ( Tree(Node) )
@@ -307,26 +308,6 @@ toDepMapUnion ds =
 fromDepMapUnion :: DepMapUnion -> [Dependency]
 fromDepMapUnion m = [ Dependency p vr | (p,vr) <- Map.toList (unDepMapUnion m) ]
 
--- | A map of dependencies.  Newtyped since the default monoid instance is not
---   appropriate.  The monoid instance uses 'intersectVersionRanges'.
-newtype DependencyMap = DependencyMap { unDependencyMap :: Map PackageName VersionRange }
-  deriving (Show, Read)
-
-instance Monoid DependencyMap where
-    mempty = DependencyMap Map.empty
-    mappend = (<>)
-
-instance Semigroup DependencyMap where
-    (DependencyMap a) <> (DependencyMap b) =
-        DependencyMap (Map.unionWith intersectVersionRanges a b)
-
-toDepMap :: [Dependency] -> DependencyMap
-toDepMap ds =
-  DependencyMap $ Map.fromListWith intersectVersionRanges [ (p,vr) | Dependency p vr <- ds ]
-
-fromDepMap :: DependencyMap -> [Dependency]
-fromDepMap m = [ Dependency p vr | (p,vr) <- Map.toList (unDependencyMap m) ]
-
 freeVars :: CondTree ConfVar c a  -> [FlagName]
 freeVars t = [ f | Flag f <- freeVars' t ]
   where
@@ -366,22 +347,6 @@ overallDependencies enabled (TargetSet targets) = mconcat depss
             CTest _ -> CTestName   t
             CBench _ -> CBenchName t
     removeDisabledSections PDNull      = True
-
--- Apply extra constraints to a dependency map.
--- Combines dependencies where the result will only contain keys from the left
--- (first) map.  If a key also exists in the right map, both constraints will
--- be intersected.
-constrainBy :: DependencyMap  -- ^ Input map
-            -> DependencyMap  -- ^ Extra constraints
-            -> DependencyMap
-constrainBy left extra =
-    DependencyMap $
-      Map.foldWithKey tightenConstraint (unDependencyMap left)
-                                        (unDependencyMap extra)
-  where tightenConstraint n c l =
-            case Map.lookup n l of
-              Nothing -> l
-              Just vr -> Map.insert n (intersectVersionRanges vr c) l
 
 -- | Collect up the targets in a TargetSet of tagged targets, storing the
 -- dependencies as we go.
