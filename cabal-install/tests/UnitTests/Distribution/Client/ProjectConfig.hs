4459b103c86b9d13408318224c6c24bf9e6a8fe6
Daniel Wagner <dmwit@galois.com>
2017-01-26 13:37:02 -0800

    fix tests

--- cabal-install/tests/UnitTests/Distribution/Client/ProjectConfig.hs
+++ cabal-install/tests/UnitTests/Distribution/Client/ProjectConfig.hs
@@ -366,7 +366,7 @@ instance Arbitrary ProjectConfigShared where
               x00 x01 x02 x03 x04
               x05 x06 x07 x08 x09
               x10 x11 x12 x13 x14
-              x15 x16 x17 x18) =
+              x15 x16 x17 x18 x19) =
       [ ProjectConfigShared
           x00' x01' x02' (fmap getNonEmpty x03') (fmap getNonEmpty x04')
           x05' x06' x07' x08' (postShrink_Constraints x09')
