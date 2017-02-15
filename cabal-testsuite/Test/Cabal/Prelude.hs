32afce34e253af83d5a124cb9b66b09698901429
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-02-14 16:21:02 -0800

    Add hasProfiledLibraries check for tests using profiling.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- cabal-testsuite/Test/Cabal/Prelude.hs
+++ cabal-testsuite/Test/Cabal/Prelude.hs
@@ -576,6 +576,17 @@ hasSharedLibraries = do
     shared_libs_were_removed <- ghcVersionIs (>= mkVersion [7,8])
     return (not (buildOS == Windows && shared_libs_were_removed))
 
+hasProfiledLibraries :: TestM Bool
+hasProfiledLibraries = do
+    env <- getTestEnv
+    initWorkDir
+    ghc_path <- programPathM ghcProgram
+    let prof_test_hs = testWorkDir env </> "Prof.hs"
+    liftIO $ writeFile prof_test_hs "module Prof where"
+    r <- liftIO $ run (testVerbosity env) (Just (testCurrentDir env))
+                      (testEnvironment env) ghc_path ["-prof", "-c", prof_test_hs]
+    return (resultExitCode r == ExitSuccess)
+
 -- | Check if the GHC that is used for compiling package tests has
 -- a shared library of the cabal library under test in its database.
 --
