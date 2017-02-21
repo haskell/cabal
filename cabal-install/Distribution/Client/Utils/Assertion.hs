3196974c6b1de7451626018368b1c7fbc50081be
Kristen Kozak <grayjay@wordroute.com>
2017-02-20 18:25:49 -0800

    Fix redundant constraint warning.

--- cabal-install/Distribution/Client/Utils/Assertion.hs
+++ cabal-install/Distribution/Client/Utils/Assertion.hs
@@ -3,16 +3,16 @@ module Distribution.Client.Utils.Assertion (debugAssert) where
 
 #ifdef DEBUG_ASSERTIONS
 import Control.Exception (assert)
-#endif
-
 import Distribution.Compat.Stack
+#endif
 
 -- | Like 'assert', but only enabled with -fdebug-assertions. This function can
 -- be used for expensive assertions that should only be turned on during testing
 -- or debugging.
-debugAssert :: WithCallStack (Bool -> a -> a)
 #ifdef DEBUG_ASSERTIONS
+debugAssert :: WithCallStack (Bool -> a -> a)
 debugAssert = assert
 #else
+debugAssert :: Bool -> a -> a
 debugAssert _ = id
 #endif
