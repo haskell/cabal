536ae48f9f46e69664b887147b82c26bba63b58a
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-03-03 23:57:32 -0800

    Port new-build/monitor_cabal_files to cabal-testsuite.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- cabal-install/tests/IntegrationTests/new-build/monitor_cabal_files.sh
+++
@@ -1,8 +0,0 @@
-. ./common.sh
-cd monitor_cabal_files
-cp q/q-broken.cabal.in q/q.cabal
-echo "Run 1" | awk '{print;print > "/dev/stderr"}'
-! cabal new-build q
-cp q/q-fixed.cabal.in q/q.cabal
-echo "Run 2" | awk '{print;print > "/dev/stderr"}'
-cabal new-build q
