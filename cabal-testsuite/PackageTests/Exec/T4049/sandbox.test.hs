3ef56761f8a7025dd42aac9f4aaa659a5040d4ef
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-03-06 14:04:26 -0800

    Select correct library dir on Windows (ugh)
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- cabal-testsuite/PackageTests/Exec/T4049/sandbox.test.hs
+++ cabal-testsuite/PackageTests/Exec/T4049/sandbox.test.hs
@@ -4,11 +4,17 @@ main = cabalTest $ do
     withSandbox $ do
         cabal "install" ["--enable-shared"]
         env <- getTestEnv
+        is_windows <- isWindows
         let sandbox_dir = testSandboxDir env
             work_dir    = testWorkDir env
+            lib_dir =
+                -- This is dumb but it's been this way for a long time.
+                if is_windows
+                    then sandbox_dir
+                    else sandbox_dir </> "lib"
         ghc ["-no-hs-main", "UseLib.c"
             , "-o", work_dir </> "UseLib"
             , "-l" ++ "myforeignlib"
-            , "-L" ++ sandbox_dir </> "lib"]
+            , "-L" ++ lib_dir ]
         recordMode RecordAll $
             cabal "exec" ["-v0", work_dir </> "UseLib"]
