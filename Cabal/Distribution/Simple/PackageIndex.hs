c7cd985b97183156bc0ac803f4e8108cd8c3d7f3
John Ericson <Ericson2314@Yahoo.com>
2017-03-15 02:14:25 -0400

    Fix warning on 7.10

--- Cabal/Distribution/Simple/PackageIndex.hs
+++ Cabal/Distribution/Simple/PackageIndex.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 
 -----------------------------------------------------------------------------
