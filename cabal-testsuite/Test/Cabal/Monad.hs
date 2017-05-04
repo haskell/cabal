b2d52b145a20a3ba77bfc1561db137974e642d30
Francesco Gazzetta <francygazz@gmail.com>
2017-05-04 16:58:14 +0200

    fixed withFile import, again

--- cabal-testsuite/Test/Cabal/Monad.hs
+++ cabal-testsuite/Test/Cabal/Monad.hs
@@ -389,7 +389,7 @@ diff args path1 path2 = do
 -- | Write a file with no CRs, always.
 writeFileNoCR :: FilePath -> String -> IO ()
 writeFileNoCR f s =
-    E.withFile f WriteMode $ \h -> do
+    withFile f WriteMode $ \h -> do
         hSetNewlineMode h noNewlineTranslation
         hPutStr h s
 
