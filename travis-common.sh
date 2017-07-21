89ef359f8c0fa25e54ee79d4762472941098251e
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2017-07-21 14:11:16 +0100

    Bump lib:Cabal and cabal-testsuite versions to 2.0.0.2.
    Fixes #4611.

--- travis-common.sh
+++ travis-common.sh
@@ -1,13 +1,14 @@
 set -e
 
 HACKAGE_REPO_TOOL_VERSION="0.1.1"
-CABAL_VERSION="2.0.0.0"
+CABAL_VERSION="2.0.0.2"
+CABAL_INSTALL_VERSION="2.0.0.0"
 
 CABAL_STORE_DB="${HOME}/.cabal/store/ghc-${GHCVER}/package.db"
 CABAL_LOCAL_DB="${TRAVIS_BUILD_DIR}/dist-newstyle/packagedb/ghc-${GHCVER}"
 CABAL_BDIR="${TRAVIS_BUILD_DIR}/dist-newstyle/build/Cabal-${CABAL_VERSION}"
 CABAL_TESTSUITE_BDIR="${TRAVIS_BUILD_DIR}/dist-newstyle/build/cabal-testsuite-${CABAL_VERSION}"
-CABAL_INSTALL_BDIR="${TRAVIS_BUILD_DIR}/dist-newstyle/build/cabal-install-${CABAL_VERSION}"
+CABAL_INSTALL_BDIR="${TRAVIS_BUILD_DIR}/dist-newstyle/build/cabal-install-${CABAL_INSTALL_VERSION}"
 CABAL_INSTALL_SETUP="${CABAL_INSTALL_BDIR}/setup/setup"
 HACKAGE_REPO_TOOL_BDIR="${TRAVIS_BUILD_DIR}/dist-newstyle/build/hackage-repo-tool-${HACKAGE_REPO_TOOL_VERSION}"
 
