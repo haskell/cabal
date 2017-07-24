623f8b105cd708ffcfbb33b3abbd7f59f42b99b6
Alexander Biehl <abiehl@novomind.com>
2017-07-24 17:47:55 +0200

    Use correct rebuilt ProjectConfig
    (cherry picked from commit 16ff1c10588adb2c62415429a787e7ebf5b6063d)

--- cabal-install/Distribution/Client/ProjectOrchestration.hs
+++ cabal-install/Distribution/Client/ProjectOrchestration.hs
@@ -164,8 +164,7 @@ establishProjectBaseContext verbosity cliConfig = do
     projectRoot <- either throwIO return =<<
                    findProjectRoot Nothing mprojectFile
 
-    let cabalDirLayout = mkCabalDirLayout cabalDir mstoreDirectory mlogsDirectory
-        distDirLayout  = defaultDistDirLayout projectRoot
+    let distDirLayout  = defaultDistDirLayout projectRoot
                                               mdistDirectory
 
     (projectConfig, localPackages) <-
@@ -173,7 +172,16 @@ establishProjectBaseContext verbosity cliConfig = do
                            distDirLayout
                            cliConfig
 
-    let buildSettings = resolveBuildTimeSettings
+    let ProjectConfigBuildOnly {
+          projectConfigLogsDir,
+          projectConfigStoreDir
+        } = projectConfigBuildOnly projectConfig
+
+        mlogsDirectory  = Setup.flagToMaybe projectConfigLogsDir
+        mstoreDirectory = Setup.flagToMaybe projectConfigStoreDir
+        cabalDirLayout = mkCabalDirLayout cabalDir mstoreDirectory mlogsDirectory
+
+        buildSettings = resolveBuildTimeSettings
                           verbosity cabalDirLayout
                           projectConfig
 
@@ -187,18 +195,10 @@ establishProjectBaseContext verbosity cliConfig = do
   where
     mdistDirectory  = Setup.flagToMaybe projectConfigDistDir
     mprojectFile    = Setup.flagToMaybe projectConfigProjectFile
-    mlogsDirectory  = Setup.flagToMaybe projectConfigLogsDir
-    mstoreDirectory = Setup.flagToMaybe projectConfigStoreDir
     ProjectConfigShared {
       projectConfigDistDir,
       projectConfigProjectFile
     } = projectConfigShared cliConfig
-    ProjectConfigBuildOnly {
-      projectConfigLogsDir,
-      projectConfigStoreDir
-    } = projectConfigBuildOnly cliConfig
-
-
 
 
 -- | This holds the context between the pre-build, build and post-build phases.
