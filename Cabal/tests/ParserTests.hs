8322aa39aa3a705da6835466017bdab8ab4ae9c7
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-08-15 23:30:38 +0300

    make parser-tests regresions with tasty-golden

--- Cabal/tests/ParserTests.hs
+++ Cabal/tests/ParserTests.hs
@@ -4,12 +4,15 @@ module Main
 
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.Golden (goldenVsString)
 
 import Data.Maybe (isJust)
 import Distribution.PackageDescription.Parsec (parseGenericPackageDescription)
+import Distribution.PackageDescription.PrettyPrint (showGenericPackageDescription)
 import Distribution.Parsec.Types.Common (PWarnType (..), PWarning (..))
 import Distribution.Parsec.Types.ParseResult (runParseResult)
-import System.FilePath ((</>))
+import Distribution.Utils.Generic (toUTF8LBS)
+import System.FilePath ((</>), (-<.>))
 
 import qualified Data.ByteString as BS
 
@@ -67,13 +70,19 @@ regressionTests = testGroup "regressions"
     ]
 
 regressionTest :: FilePath -> TestTree
-regressionTest fp = testCase fp $ do
-    contents <- BS.readFile $ "tests" </> "ParserTests" </> "regressions" </> fp
+regressionTest fp = goldenVsString fp correct $ do
+    contents <- BS.readFile input
     let res =  parseGenericPackageDescription contents
     let (_, errs, x) = runParseResult res
 
-    assertBool ("parses successfully: " ++ show errs) $ isJust x
-    assertBool ("parses without errors: " ++ show errs) $ null errs
+    return $ toUTF8LBS $ case x of
+        Just gpd | null errs ->
+            showGenericPackageDescription gpd
+        _ ->
+            unlines $ "ERROR" : map show errs
+  where
+    input = "tests" </> "ParserTests" </> "regressions" </> fp
+    correct = input -<.> "golden"
 
 -------------------------------------------------------------------------------
 -- Main
