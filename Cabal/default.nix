b9f8e863b948064ea9d7d8472bd5b21a6359d85d
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2017-09-19 16:41:05 +0100

    Bump versions in preparation for release.

--- Cabal/default.nix
+++ Cabal/default.nix
@@ -4,7 +4,7 @@
 }:
 mkDerivation {
   pname = "Cabal";
-  version = "2.0.0.2";
+  version = "2.0.1.0";
   src = ./.;
   libraryHaskellDepends = [
     array base binary bytestring containers deepseq directory filepath
--- cabal-install/default.nix
+++ cabal-install/default.nix
@@ -7,7 +7,7 @@
 }:
 mkDerivation {
   pname = "cabal-install";
-  version = "2.0.0.0";
+  version = "2.0.0.1";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
