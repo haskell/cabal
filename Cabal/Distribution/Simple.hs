8d88dd9c666a33d7fce5e0728bd6b1a322d84f1e
Moritz Angermann <moritz.angermann@gmail.com>
2018-02-07 14:33:25 +0800

    Adds tryGetBuildInfo
    # Conflicts:
    #	Cabal/Distribution/Simple.hs

--- Cabal/Distribution/Simple.hs
+++ Cabal/Distribution/Simple.hs
@@ -59,6 +59,7 @@ module Distribution.Simple (
   ) where
 
 import Prelude ()
+import Control.Exception (try)
 import Distribution.Compat.Prelude
 
 -- local
@@ -328,7 +329,12 @@ haddockAction hooks flags args = do
 cleanAction :: UserHooks -> CleanFlags -> Args -> IO ()
 cleanAction hooks flags args = do
     distPref <- findDistPrefOrDefault (cleanDistPref flags)
-    let flags' = flags { cleanDistPref = toFlag distPref }
+
+    elbi <- tryGetBuildConfig hooks verbosity distPref
+    let flags' = flags { cleanDistPref = toFlag distPref
+                       , cleanCabalFilePath = case elbi of
+                           Left _ -> mempty
+                           Right lbi -> maybeToFlag (cabalFilePath lbi)}
 
     pbi <- preClean hooks args flags'
 
@@ -487,7 +493,13 @@ sanityCheckHookedBuildInfo pkg_descr (_, hookExes)
 
 sanityCheckHookedBuildInfo _ _ = return ()
 
+-- | Try to read the 'localBuildInfoFile'
+tryGetBuildConfig :: UserHooks -> Verbosity -> FilePath
+                  -> IO (Either ConfigStateFileError LocalBuildInfo)
+tryGetBuildConfig u v = try . getBuildConfig u v
 
+
+-- | Read the 'localBuildInfoFile' or throw an exception.
 getBuildConfig :: UserHooks -> Verbosity -> FilePath -> IO LocalBuildInfo
 getBuildConfig hooks verbosity distPref = do
   lbi_wo_programs <- getPersistBuildConfig distPref
