3e87beab06310116da6d96596a35756df98cac23
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2018-02-08 09:20:48 +0000

    Revert "Merge pull request #5012 from grayjay/issue-4823"
    This reverts commit 11d8a777466de7ceee0a49a0a5b9b764bf05df98, reversing
    changes made to 94a7374d7b1a9b55454209e92d5057ba81be7d6a.
    
    That change was causing failures on AppVeyor, see
    https://github.com/haskell/cabal/pull/5012#issuecomment-364060817

--- cabal-install/Distribution/Client/Dependency.hs
+++ cabal-install/Distribution/Client/Dependency.hs
@@ -64,7 +64,7 @@ module Distribution.Client.Dependency (
   ) where
 
 import Distribution.Solver.Modular
-         ( modularResolver, SolverConfig(..), PruneAfterFirstSuccess(..) )
+         ( modularResolver, SolverConfig(..) )
 import Distribution.Simple.PackageIndex (InstalledPackageIndex)
 import qualified Distribution.Simple.PackageIndex as InstalledPackageIndex
 import Distribution.Client.SolverInstallPlan (SolverInstallPlan)
@@ -719,7 +719,7 @@ resolveDependencies platform comp pkgConfigDB solver params =
   $ runSolver solver (SolverConfig reordGoals cntConflicts
                       indGoals noReinstalls
                       shadowing strFlags allowBootLibs maxBkjumps enableBj
-                      solveExes order verbosity (PruneAfterFirstSuccess False))
+                      solveExes order verbosity)
                      platform comp installedPkgIndex sourcePkgIndex
                      pkgConfigDB preferences constraints targets
   where
