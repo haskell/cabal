ab31ae12b930e96fad8712c680590d3f4e2fb8fd
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-03-11 12:41:58 +0200

    Add isAcceptedLicense to the test-suite

--- Cabal/tests/UnitTests/Distribution/SPDX.hs
+++ Cabal/tests/UnitTests/Distribution/SPDX.hs
@@ -18,6 +18,8 @@ spdxTests =
     , testProperty "LicenseRef roundtrip" licenseRefRoundtrip
     , testProperty "SimpleLicenseExpression roundtrip" simpleLicenseExpressionRoundtrip
     , testProperty "LicenseExpression roundtrip" licenseExpressionRoundtrip
+    , testProperty "isAcceptableLicense l = True"  shouldAcceptProp
+    , testProperty "isAcceptableLicense l = False" shouldRejectProp
     ]
 
 licenseIdRoundtrip :: LicenseId -> Property
@@ -54,6 +56,63 @@ reassoc (EAnd a b) = case reassoc a of
     EAnd x y -> EAnd x (reassoc (EAnd y b))
     x       -> EAnd x (reassoc b)
 reassoc l = l
+
+-------------------------------------------------------------------------------
+-- isAcceptableLicence
+-------------------------------------------------------------------------------
+
+shouldAccept :: [License]
+shouldAccept = map License
+    [ simpleLicenseExpression GPL_2_0_only
+    , simpleLicenseExpression GPL_2_0_or_later
+    , simpleLicenseExpression BSD_2_Clause
+    , simpleLicenseExpression BSD_3_Clause
+    , simpleLicenseExpression MIT
+    , simpleLicenseExpression ISC
+    , simpleLicenseExpression MPL_2_0
+    , simpleLicenseExpression Apache_2_0
+    , simpleLicenseExpression CC0_1_0
+    , simpleLicenseExpression BSD_4_Clause `EOr` simpleLicenseExpression MIT
+    ]
+
+shouldReject :: [License]
+shouldReject = map License
+    [ simpleLicenseExpression BSD_4_Clause
+    , simpleLicenseExpression BSD_4_Clause `EAnd` simpleLicenseExpression MIT
+    ]
+
+-- | A sketch of what Hackage could accept
+--
+-- * NONE is rejected
+--
+-- * "or later" syntax (+ postfix) is rejected
+--
+-- * "WITH exc" exceptions are rejected
+--
+-- * There should be a way to interpert license as (conjunction of)
+--   OSI-accepted licenses or CC0
+--
+isAcceptableLicense :: License -> Bool
+isAcceptableLicense NONE           = False
+isAcceptableLicense (License expr) = goExpr expr
+  where
+    goExpr (EAnd a b)            = goExpr a && goExpr b
+    goExpr (EOr a b)             = goExpr a || goExpr b
+    goExpr (ELicense _ (Just _)) = False -- Don't allow exceptions
+    goExpr (ELicense s Nothing)  = goSimple s
+
+    goSimple (ELicenseRef _)      = False -- don't allow referenced licenses
+    goSimple (ELicenseIdPlus _)   = False -- don't allow + licenses (use GPL-3.0-or-later e.g.)
+    goSimple (ELicenseId CC0_1_0) = True -- CC0 isn't OSI approved, but we allow it as "PublicDomain", this is eg. PublicDomain in http://hackage.haskell.org/package/string-qq-0.0.2/src/LICENSE
+    goSimple (ELicenseId lid)     = licenseIsOsiApproved lid -- allow only OSI approved licenses.
+
+shouldAcceptProp :: Property
+shouldAcceptProp = conjoin $
+    map (\l -> counterexample (prettyShow l) (isAcceptableLicense l)) shouldAccept
+
+shouldRejectProp :: Property
+shouldRejectProp = conjoin $
+    map (\l -> counterexample (prettyShow l) (not $ isAcceptableLicense l)) shouldReject
 
 -------------------------------------------------------------------------------
 -- Instances
