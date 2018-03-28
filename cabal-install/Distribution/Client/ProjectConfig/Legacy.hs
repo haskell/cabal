d095feb938e6a7020ba4b1ce1cf21dff37900a85
alexbiehl <alex.biehl@gmail.com>
2018-03-28 17:18:24 +0200

    Make --enable-documentation work again

--- cabal-install/Distribution/Client/ProjectConfig/Legacy.hs
+++ cabal-install/Distribution/Client/ProjectConfig/Legacy.hs
@@ -193,7 +193,8 @@ commandLineFlagsToProjectConfig globalFlags configFlags configExFlags
         splitConfig pc = (pc
                          , mempty { packageConfigProgramPaths = packageConfigProgramPaths pc
                                   , packageConfigProgramArgs  = packageConfigProgramArgs  pc
-                                  , packageConfigProgramPathExtra = packageConfigProgramPathExtra pc })
+                                  , packageConfigProgramPathExtra = packageConfigProgramPathExtra pc
+                                  , packageConfigDocumentation = packageConfigDocumentation pc })
 
 -- | Convert from the types currently used for the user-wide @~/.cabal/config@
 -- file into the 'ProjectConfig' type.
