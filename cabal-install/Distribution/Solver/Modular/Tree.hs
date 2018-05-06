2dece072f6b57be1d554b32ab894706492be6f0f
Kristen Kozak <grayjay@wordroute.com>
2018-05-05 18:29:32 -0700

    Solver: Enforce dependencies on libraries (fixes #779).
    This commit generalizes the fix for issue #4781
    (e86f83890d93068f7c27faea5bf07146b5452c23) by tracking dependencies on
    components instead of dependencies on executables.  That means that the solver
    always checks whether a package contains a library before using it to satisfy a
    build-depends dependency.  If a version of a package doesn't contain a library,
    the solver can try other versions.  Associating each dependency with a component
    also moves towards the design for component-based dependency solving described
    in issue #4087.

--- cabal-install/Distribution/Solver/Modular/Tree.hs
+++ cabal-install/Distribution/Solver/Modular/Tree.hs
@@ -31,7 +31,6 @@ import qualified Distribution.Solver.Modular.WeightedPSQ as W
 import Distribution.Solver.Types.ConstraintSource
 import Distribution.Solver.Types.Flag
 import Distribution.Solver.Types.PackagePath
-import Distribution.Types.UnqualComponentName
 import Language.Haskell.Extension (Extension, Language)
 
 type Weight = Double
@@ -101,8 +100,8 @@ data FailReason = UnsupportedExtension Extension
                 | MissingPkgconfigPackage PkgconfigName VR
                 | NewPackageDoesNotMatchExistingConstraint ConflictingDep
                 | ConflictingConstraints ConflictingDep ConflictingDep
-                | NewPackageIsMissingRequiredExe UnqualComponentName (DependencyReason QPN)
-                | PackageRequiresMissingExe QPN UnqualComponentName
+                | NewPackageIsMissingRequiredComponent ExposedComponent (DependencyReason QPN)
+                | PackageRequiresMissingComponent QPN ExposedComponent
                 | CannotInstall
                 | CannotReinstall
                 | Shadowed
@@ -123,7 +122,7 @@ data FailReason = UnsupportedExtension Extension
   deriving (Eq, Show)
 
 -- | Information about a dependency involved in a conflict, for error messages.
-data ConflictingDep = ConflictingDep (DependencyReason QPN) (Maybe UnqualComponentName) QPN CI
+data ConflictingDep = ConflictingDep (DependencyReason QPN) (PkgComponent QPN) CI
   deriving (Eq, Show)
 
 -- | Functor for the tree type. 'a' is the type of nodes' children. 'd' and 'c'
