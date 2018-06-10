14b7102ee7cdba0d11e44771305c21929932e0cd
Duncan Coutts <duncan@community.haskell.org>
2018-06-10 20:28:37 +0100

    Try to make the windows dir cleanup hacks work with directory < 1.2.7

--- cabal-install/tests/UnitTests/TempTestDir.hs
+++ cabal-install/tests/UnitTests/TempTestDir.hs
@@ -12,14 +12,15 @@ import Control.Monad (when)
 import Control.Exception (bracket, try, throwIO)
 import Control.Concurrent (threadDelay)
 
-import System.IO.Error (isPermissionError)
-import System.Directory (getTemporaryDirectory, removeDirectoryRecursive)
-#if MIN_VERSION_directory(1,2,7)
-import System.Directory (removePathForcibly)
-#endif
+import System.IO.Error
+import System.Directory
+import System.FilePath
 import qualified System.Info (os)
 
 
+-- | Much like 'withTemporaryDirectory' but with a number of hacks to make
+-- sure on windows that we can clean up the directory at the end.
+--
 withTestDir :: Verbosity -> String ->  (FilePath -> IO a) -> IO a
 withTestDir verbosity template action = do
     systmpdir <- getTemporaryDirectory
@@ -36,42 +37,66 @@ withTestDir verbosity template action = do
 -- directory. There is no sane way to wait on those locks being released,
 -- we just have to wait, try again and hope.
 --
+-- In addition, on Windows a file that is not writable also cannot be deleted,
+-- so we must try setting the permissions to readable before deleting files.
+-- Some VCS tools on Windows create files with read-only attributes.
+--
 removeDirectoryRecursiveHack :: Verbosity -> FilePath -> IO ()
-removeDirectoryRecursiveHack verbosity dir | isWindows = go 0
+removeDirectoryRecursiveHack verbosity dir | isWindows = go 1
   where
     isWindows = System.Info.os == "mingw32"
-    limit     = 30
+    limit     = 3
 
     go :: Int -> IO ()
     go n = do
-#if MIN_VERSION_directory(1,2,7)
       res <- try $ removePathForcibly dir
-#else
--- TODO: will fail on win32 w/ permission denied;
---       if the test-suite is supposed to work on GHC < 8.0, we need to inline
---       http://hackage.haskell.org/package/directory-1.3.2.2/docs/src/System.Directory.html#removePathForcibly
-      res <- try $ removeDirectoryRecursive dir
-#endif
       case res of
         Left e
             -- wait a second and try again
-          | isPermissionError e  &&  n+1 < limit -> do
+          | isPermissionError e  &&  n < limit -> do
               threadDelay 1000000
               go (n+1)
 
             -- but if we hit the limt warn and fail.
           | isPermissionError e -> do
               warn verbosity $ "Windows file locking hack: hit the retry limit "
-                            ++ show n ++ " while trying to remove " ++ dir
+                            ++ show limit ++ " while trying to remove " ++ dir
               throwIO e
 
             -- or it's a different error fail.
           | otherwise -> throwIO e
 
         Right () ->
-          when (n >= 3) $
-            warn verbosity $ "Windows file locking hack: had to try " ++ show n
-                          ++ " times to remove " ++ dir
+          when (n >= 1) $
+            warn verbosity $ "Windows file locking hack: had to try "
+                          ++ show n ++ " times to remove " ++ dir
 
 removeDirectoryRecursiveHack _ dir = removeDirectoryRecursive dir
+
+
+#if !(MIN_VERSION_directory(1,2,7))
+-- A simplified version that ought to work for our use case here, and does
+-- not rely on directory internals.
+removePathForcibly :: FilePath -> IO ()
+removePathForcibly path = do
+    makeRemovable path `catchIOError` \ _ -> pure ()
+    isDir <- doesDirectoryExist path
+    if isDir
+       then do
+         entries <- getDirectoryContents path
+         sequence_
+           [ removePathForcibly (path </> entry)
+           | entry <- entries, entry /= ".", entry /= ".." ]
+         removeDirectory path
+       else
+         removeFile path
+  where
+    makeRemovable :: FilePath -> IO ()
+    makeRemovable p =
+      setPermissions p emptyPermissions {
+        readable   = True,
+        searchable = True,
+        writable   = True
+      }
+#endif
 
