ef8ae9cebf6079cf0f3c6245c9dff85e6ee759e9
Alexis Williams <alexis@typedr.at>
2018-06-19 16:43:05 -0700

    Make test present errors as hex

--- cabal-testsuite/PackageTests/NewSdist/DeterministicTrivial/deterministic.test.hs
+++ cabal-testsuite/PackageTests/NewSdist/DeterministicTrivial/deterministic.test.hs
@@ -1,5 +1,6 @@
 import Test.Cabal.Prelude
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Base16 as BS16
 import qualified Crypto.Hash.SHA256 as SHA256
 import System.FilePath
     ( (</>) )
@@ -14,4 +15,4 @@ main = cabalTest $ do
     known <- liftIO (BS.readFile knownSdist)
     unknown <- liftIO (BS.readFile mySdist)
 
-    assertEqual "hashes didn't match for sdist" (SHA256.hash known) (SHA256.hash unknown)
+    assertEqual "hashes didn't match for sdist" (BS16.encode $ SHA256.hash known) (BS16.encode $ SHA256.hash unknown)
