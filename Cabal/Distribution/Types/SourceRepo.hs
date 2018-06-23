c277b13d2f13c98f84a698ac7516813a13b03c5e
Duncan Coutts <duncan@community.haskell.org>
2018-06-23 02:13:39 +0100

    Give SourceRepo an Ord instance
    So we can use it as a key in Maps (or more specifically continue to use
    PackageLocation as a key in maps, but add SourceRepo into a new package
    location constructor).

--- Cabal/Distribution/Types/SourceRepo.hs
+++ Cabal/Distribution/Types/SourceRepo.hs
@@ -83,7 +83,7 @@ data SourceRepo = SourceRepo {
   -- given the default is \".\" ie no subdirectory.
   repoSubdir   :: Maybe FilePath
 }
-  deriving (Eq, Generic, Read, Show, Typeable, Data)
+  deriving (Eq, Ord, Generic, Read, Show, Typeable, Data)
 
 emptySourceRepo :: RepoKind -> SourceRepo
 emptySourceRepo kind = SourceRepo
