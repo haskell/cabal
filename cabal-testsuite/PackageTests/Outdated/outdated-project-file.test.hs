3d408ff6a95a15909f4832a28160be98e9ad4bd2
quasicomputational <quasicomputational@gmail.com>
2018-07-30 17:45:46 +0100

    outdated: document and test last-one-wins behaviour of new-freeze flags

--- cabal-testsuite/PackageTests/Outdated/outdated-project-file.test.hs
+++ cabal-testsuite/PackageTests/Outdated/outdated-project-file.test.hs
@@ -4,3 +4,15 @@ main = cabalTest $ withRepo "repo" $ do
   assertOutputContains "base" res
   assertOutputDoesNotContain "template-haskell" res
 
+  -- Test last-one-wins behaviour.
+  res <- cabal' "outdated" ["--project-file", "cabal.project", "--project-file", "variant.project"]
+  assertOutputContains "base" res
+  assertOutputDoesNotContain "template-haskell" res
+
+  res <- cabal' "outdated" ["--new-freeze-file", "--project-file", "variant.project"]
+  assertOutputContains "base" res
+  assertOutputDoesNotContain "template-haskell" res
+
+  res <- cabal' "outdated" ["--project-file", "variant.project", "--new-freeze-file"]
+  assertOutputContains "base" res
+  assertOutputContains "template-haskell" res
