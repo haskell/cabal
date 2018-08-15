502a9991029641805536401ada39c16621c98d6e
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-08-15 17:05:12 +0300

    Remove this is not a lib note [ci skip]

--- cabal-install/cabal-install.cabal.pp
+++ cabal-install/cabal-install.cabal.pp
@@ -244,9 +244,6 @@ Description:
     The \'cabal\' command-line program simplifies the process of managing
     Haskell software by automating the fetching, configuration, compilation
     and installation of Haskell libraries and programs.
-    .
-    This package only provides an executable and cannot be used as a
-    library (ignore the module listing below.)
 homepage:           http://www.haskell.org/cabal/
 bug-reports:        https://github.com/haskell/cabal/issues
 License:            BSD3
