4d3ab9096aad77c676a74ed62361b02fab575d31
Edward Z. Yang <ezyang@mit.edu>
2018-09-05 09:45:28 -0400

    New S3 based build product upload (#5562)
    Instead of directly committing binaries to GitHub (GitHub doesn't like them)
    upload to Amazon S3. Any S3 bucket controlled by s3-bouncer
    is acceptable; we have hardcoded s3-bouncer.heroku.com.
    More info about s3-bouncer available at:
    https://github.com/ezyang/s3-bouncer
    
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- travis/upload.sh
+++ travis/upload.sh
@@ -69,14 +69,26 @@ cp $TRAVIS_BUILD_DIR/travis/id_rsa.rot13 .
 # Install all of the necessary files for testing
 cp $TRAVIS_BUILD_DIR/travis-install.sh .
 cp $TRAVIS_BUILD_DIR/travis-common.sh .
-# The binaries to test (statically linked, of course!)
+
 cp ${CABAL_BDIR}/c/unit-tests/build/unit-tests/unit-tests          Cabal
 cp ${CABAL_BDIR}/c/check-tests/build/check-tests/check-tests       Cabal
 cp ${CABAL_BDIR}/c/parser-tests/build/parser-tests/parser-tests    Cabal
 cp ${CABAL_BDIR}/c/hackage-tests/build/hackage-tests/hackage-tests Cabal
+BINARIES="Cabal/unit-tests Cabal/check-tests Cabal/parser-tests Cabal/hackage-tests"
 if [ "x$CABAL_LIB_ONLY" != "xYES" ]; then
     cp ${CABAL_INSTALL_EXE}                       cabal-install
+    BINARIES="cabal-install/cabal $BINARIES"
 fi
+
+# The binaries to test (statically linked, of course!)
+tar czf binaries.tgz $BINARIES
+rm $BINARIES # Don't check me in!
+
+# Upload to S3
+S3_URL=$(curl -X POST "https://s3-bouncer.herokuapp.com/put")
+curl "$S3_URL" --upload-file binaries.tgz
+rm binaries.tgz # Don't check me in!
+echo "$S3_URL" | xargs basename | cut -d '?' -f 1 > s3-object.txt
 
 # Add, commit, push
 git add .
