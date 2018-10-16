cc7957036a92d3ee17e80c15629a3b83b920c409
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-10-17 00:29:44 +0300

    Fix CustomWithoutCabalDefaultMain output for 8.6.1

--- cabal-testsuite/PackageTests/CustomWithoutCabalDefaultMain/cabal.test.hs
+++ cabal-testsuite/PackageTests/CustomWithoutCabalDefaultMain/cabal.test.hs
@@ -5,7 +5,7 @@ main = cabalTest $ do
     -- Compilation should fail because Setup.hs imports Distribution.Simple.
     r <- fails $ cabal' "new-build" ["custom-setup-without-cabal-defaultMain"]
     assertRegex "Should not have been able to import Cabal"
-                "(Could not find module|Failed to load interface for).*Distribution\\.Simple" r
+                "(Could not (find|load) module|Failed to load interface for).*Distribution\\.Simple" r
     {-
     -- TODO: With GHC 8.2, this no longer is displayed
     -- When using --with-ghc, this message is not necessarily output
