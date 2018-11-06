9d217525a1d20f8046716ad43f8412587a8e21be
Francesco Gazzetta <francygazz@gmail.com>
2018-11-06 13:47:59 +0100

    Add comment about absolute store path

--- cabal-install/Distribution/Client/DistDirLayout.hs
+++ cabal-install/Distribution/Client/DistDirLayout.hs
@@ -1,3 +1,4 @@
+let
 {-# LANGUAGE RecordWildCards #-}
 
 -- |
@@ -273,7 +274,7 @@ defaultCabalDirLayout cabalDir =
     mkCabalDirLayout cabalDir Nothing Nothing
 
 mkCabalDirLayout :: FilePath -- ^ Cabal directory
-                 -> Maybe FilePath -- ^ Store directory
+                 -> Maybe FilePath -- ^ Store directory. Must be absolute
                  -> Maybe FilePath -- ^ Log directory
                  -> CabalDirLayout
 mkCabalDirLayout cabalDir mstoreDir mlogDir =
