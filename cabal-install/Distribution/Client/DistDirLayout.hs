7f4e63545e604ade496caedc2e7b7534e8cd435a
Francesco Gazzetta <francygazz@gmail.com>
2018-11-06 15:10:39 +0100

    Revert "Add comment about absolute store path"
    This reverts commit 9d217525a1d20f8046716ad43f8412587a8e21be.
    
    I don't know where that 'let' came from :-/

--- cabal-install/Distribution/Client/DistDirLayout.hs
+++ cabal-install/Distribution/Client/DistDirLayout.hs
@@ -1,4 +1,3 @@
-let
 {-# LANGUAGE RecordWildCards #-}
 
 -- |
@@ -275,7 +274,7 @@ defaultCabalDirLayout cabalDir =
     mkCabalDirLayout cabalDir Nothing Nothing
 
 mkCabalDirLayout :: FilePath -- ^ Cabal directory
-                 -> Maybe FilePath -- ^ Store directory. Must be absolute
+                 -> Maybe FilePath -- ^ Store directory
                  -> Maybe FilePath -- ^ Log directory
                  -> CabalDirLayout
 mkCabalDirLayout cabalDir mstoreDir mlogDir =
