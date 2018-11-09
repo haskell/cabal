b337d0be07d5c7378c23830db04a3ede34b54618
Alp Mestanogullari <alpmestan@gmail.com>
2018-11-09 11:21:45 +0100

    temporarily disable extraDynLibFlavours version check

--- Cabal/Distribution/PackageDescription/Check.hs
+++ Cabal/Distribution/PackageDescription/Check.hs
@@ -1277,10 +1277,12 @@ checkCabalVersion pkg =
         ++ " and 'extra-library-flavours' requires the package "
         ++ " to specify at least 'cabal-version: >= 2.1'."
 
+  {-
   , checkVersion [2,5] (not . null $ buildInfoField extraDynLibFlavours) $
       PackageDistInexcusable $
            "The use of 'extra-dynamic-library-flavours' requires the package "
         ++ " to specify at least 'cabal-version: >= 2.5'."
+  -}
 
   , checkVersion [2,1] (any (not . null)
                         (buildInfoField virtualModules)) $
