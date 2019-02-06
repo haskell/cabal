haskell/cabal

commit 7a029caa7026f9b6b585a68062a4175eadb6ca4f

Some tests for new-sdist
They're not all passing, but I think that that's a bug in the code
rather than the tests.

cabal-install/Distribution/Client/CmdSdist.hs
@@ -45,7 +45,7 @@ import Distribution.Simple.Setup
 import Distribution.Simple.SrcDist
     ( listPackageSources )
 import Distribution.Simple.Utils
-    ( die', notice )
+    ( die', notice, withOutputMarker )
 import Distribution.Types.ComponentName
     ( ComponentName, showComponentName )
 import Distribution.Types.PackageName
@@ -177,8 +177,8 @@ sdistAction SdistFlags{..} targetStrings globalFlags = do
     
     let 
         format =
-            if | listSources, nulSeparated -> SourceList '\n'
-               | listSources               -> SourceList '\0'
+            if | listSources, nulSeparated -> SourceList '\0'
+               | listSources               -> SourceList '\n'
                | otherwise                 -> Archive archiveFormat
 
         ext = case format of
@@ -222,12 +222,13 @@ packageToSdist verbosity format outputFile pkg = do
     (norm NoExec -> nonexec, norm Exec -> exec) <- 
         listPackageSources verbosity (flattenPackageDescription $ packageDescription pkg) knownSuffixHandlers
 
-    let write = if outputFile == "-" then BSL.putStr else BSL.writeFile outputFile
+    let write = if outputFile == "-"
+          then putStr . withOutputMarker verbosity . BSL.unpack
+          else BSL.writeFile outputFile
         files =  nub . sortOn snd $ nonexec ++ exec
 
     case format of
-        SourceList nulSep -> do
-            notice verbosity $ "File manifest for package " ++ prettyShow (packageId pkg) ++ ":\n"
+        SourceList nulSep ->
             write (BSL.pack . (++ [nulSep]) . intercalate [nulSep] . fmap snd $ files)
         Archive TargzFormat -> do
             let entriesM :: StateT (Set.Set FilePath) (WriterT [Tar.Entry] IO) ()

Cabal/Distribution/Simple/Utils.hs
@@ -40,6 +40,7 @@ module Distribution.Simple.Utils (
         chattyTry,
         annotateIO,
         printRawCommandAndArgs, printRawCommandAndArgsAndEnv,
+        withOutputMarker,
 
         -- * exceptions
         handleDoesNotExist,

cabal-testsuite/PackageTests/NewSdist/MultiTarget/a/Main.hs
@@ -1,0 +1,1 @@
+main = putStrLn "a"

cabal-testsuite/PackageTests/NewSdist/MultiTarget/a/Test.hs
@@ -1,0 +1,1 @@
+main = putStrLn "a-test"

cabal-testsuite/PackageTests/NewSdist/MultiTarget/all-output-dir.test.hs
@@ -1,0 +1,10 @@
+import Test.Cabal.Prelude
+import System.Directory
+main = cabalTest $ withSourceCopy $ do
+  cwd <- testCurrentDir <$> getTestEnv
+  liftIO $ createDirectoryIfMissing False $ cwd </> "archives"
+  cabal "new-sdist" ["all", "--output-dir", "archives"]
+  shouldNotExist $ cwd </> "dist-newstyle/sdist/a-0.1.tar.gz"
+  shouldNotExist $ cwd </> "dist-newstyle/sdist/b-0.1.tar.gz"
+  shouldExist $ cwd </> "archives/a-0.1.tar.gz"
+  shouldExist $ cwd </> "archives/b-0.1.tar.gz"

cabal-testsuite/PackageTests/NewSdist/MultiTarget/all-test-sute.test.hs
@@ -1,0 +1,6 @@
+import Test.Cabal.Prelude
+main = cabalTest $ withSourceCopy $ do
+  cwd <- testCurrentDir <$> getTestEnv
+  fails $ cabal "new-sdist" ["all:tests"]
+  shouldNotExist $ cwd </> "dist-newstyle/sdist/a-0.1.tar.gz"
+  shouldNotExist $ cwd </> "dist-newstyle/sdist/b-0.1.tar.gz"

cabal-testsuite/PackageTests/NewSdist/MultiTarget/all.test.hs
@@ -1,0 +1,6 @@
+import Test.Cabal.Prelude
+main = cabalTest $ withSourceCopy $ do
+  cwd <- testCurrentDir <$> getTestEnv
+  cabal "new-sdist" ["all"]
+  shouldExist $ cwd </> "dist-newstyle/sdist/a-0.1.tar.gz"
+  shouldExist $ cwd </> "dist-newstyle/sdist/b-0.1.tar.gz"

cabal-testsuite/PackageTests/NewSdist/MultiTarget/b/Main.hs
@@ -1,0 +1,1 @@
+main = putStrLn "b"

commit 92f4a9361bc9dfe8f5ad8fb808305bb287c3e8d4

Fix sdist output when sent to stdout
This was broken by 7a029caa7026f9b6b585a68062a4175eadb6ca4f

We can not use `ByteString.pack` and then print it as `String`, which
will get encoded again based on the system setting, and very likely,
we get different things unless the encoding is something like ISO-8859-1, e.g.
in UTF-8 environment, the magic of gz "\x1f\x8b" will get encoded again
into "\x1f\xc2\x8b".

Before:

```
$ /cabal sdist --output-dir=- | tar -t -z -f -
tar: This does not look like a tar archive
```

And now:

```
$ cabal sdist --output-dir=- | tar -t -z -f -  | tail -n 1
cabal-install-2.5.0.0/tests/README.md
```

cabal-install/Distribution/Client/CmdSdist.hs
@@ -77,7 +77,7 @@ import qualified Data.ByteString.Lazy.Char8 as BSL
 import Data.Either
     ( partitionEithers )
 import Data.List
-    ( find, sortOn, nub, intercalate )
+    ( find, sortOn, nub )
 import qualified Data.Set as Set
 import System.Directory
     ( getCurrentDirectory, setCurrentDirectory
@@ -213,15 +213,20 @@ packageToSdist verbosity projectRootDir format outputFile pkg = do
              RemoteTarballPackage {}               -> death
              RepoTarballPackage {}                 -> death
 
-    let write = if outputFile == "-"
-          then putStr . withOutputMarker verbosity . BSL.unpack
-          else BSL.writeFile outputFile
+    let -- Write String to stdout or file, using the default TextEncoding.
+        write
+          | outputFile == "-" = putStr . withOutputMarker verbosity
+          | otherwise = writeFile outputFile
+        -- Write raw ByteString to stdout or file as it is, without encoding.
+        writeLBS
+          | outputFile == "-" = BSL.putStr
+          | otherwise = BSL.writeFile outputFile
 
     case dir0 of
       Left tgz -> do
         case format of
           TarGzArchive -> do
-            write =<< BSL.readFile tgz
+            writeLBS =<< BSL.readFile tgz
             when (outputFile /= "-") $
               notice verbosity $ "Wrote tarball sdist to " ++ outputFile ++ "\n"
           _ -> die' verbosity ("cannot convert tarball package to " ++ show format)
@@ -239,7 +244,7 @@ packageToSdist verbosity projectRootDir format outputFile pkg = do
         case format of
             SourceList nulSep -> do
                 let prefix = makeRelative projectRootDir dir
-                write (BSL.pack . (++ [nulSep]) . intercalate [nulSep] . fmap ((prefix </>) . snd) $ files)
+                write $ concat [prefix </> i ++ [nulSep] | (_, i) <- files]
                 when (outputFile /= "-") $
                     notice verbosity $ "Wrote source list to " ++ outputFile ++ "\n"
             TarGzArchive -> do
@@ -281,7 +286,7 @@ packageToSdist verbosity projectRootDir format outputFile pkg = do
                     -- after the epoch is during 2001-09-09, so that does
                     -- nicely. See #5596.
                     setModTime entry = entry { Tar.entryTime = 1000000000 }
-                write . normalize . GZip.compress . Tar.write $ fmap setModTime entries
+                writeLBS . normalize . GZip.compress . Tar.write $ fmap setModTime entries
                 when (outputFile /= "-") $
                     notice verbosity $ "Wrote tarball sdist to " ++ outputFile ++ "\n"
 