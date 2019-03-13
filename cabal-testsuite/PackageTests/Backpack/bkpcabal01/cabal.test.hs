011de3d2a4bb9e411831f5227273b3bc946e13ce
Herbert Valerio Riedel <hvr@gnu.org>
2019-03-13 07:34:44 +0100

    Skip bkpcabal01 test on GHC 8.2.2 due to #4908
    Also tweaks testcase's .cabal metadata

--- cabal-testsuite/PackageTests/Backpack/bkpcabal01/cabal.test.hs
+++ cabal-testsuite/PackageTests/Backpack/bkpcabal01/cabal.test.hs
@@ -1,4 +1,5 @@
 import Test.Cabal.Prelude
 main = cabalTest $ do
-    skipUnless =<< ghcVersionIs (>= mkVersion [8,2])
+    -- GHC 8.2.2 had a regression ("unknown package: hole"), see also #4908
+    skipUnless =<< ghcVersionIs (\v -> v >= mkVersion [8,2] && v /= mkVersion [8,2,2])
     cabal "v2-build" ["all"]
