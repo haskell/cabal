e2139255aff98216d317b966c1bd9facb3bcfa38
fendor <power.walross@gmail.com>
2019-06-13 21:26:35 +0200

    Remove/adapt inaccurate test cases

--- cabal-testsuite/PackageTests/ShowBuildInfo/A/build-info-unknown.test.hs
+++ cabal-testsuite/PackageTests/ShowBuildInfo/A/build-info-unknown.test.hs
@@ -2,13 +2,13 @@ import           Test.Cabal.Prelude
 
 main = cabalTest $ do
     r <- fails $ cabal' "new-show-build-info" ["exe:B"]
-    assertOutputContains "cabal: Internal error in target matching." r
+    assertOutputContains "Internal error in target matching." r
 
     r <- fails $ cabal' "new-show-build-info" ["--unit-ids-json=B-inplace-0.1.0.0"]
-    assertOutputContains "cabal: No unit B-inplace-0.1.0.0" r
+    assertOutputContains "No unit B-inplace-0.1.0.0" r
 
     r <- fails $ cabal' "new-show-build-info" ["--unit-ids-json=A-0.1.0.0-inplace B-inplace-0.1.0.0"]
-    assertOutputContains "cabal: No unit B-inplace-0.1.0.0" r
+    assertOutputContains "No unit B-inplace-0.1.0.0" r
 
     r <- fails $ cabal' "new-show-build-info" ["--unit-ids-json=A-0.1.0.0-inplace", "exe:B"]
-    assertOutputContains "cabal: Internal error in target matching." r
+    assertOutputContains "Internal error in target matching." r
