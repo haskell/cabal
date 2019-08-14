09210e3ab6df3098723cf2c2897c27e0188be5be
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-08-14 14:26:22 +0300

    Add cabal-version: 3.2

--- Cabal/Distribution/SPDX/LicenseListVersion.hs
+++ Cabal/Distribution/SPDX/LicenseListVersion.hs
@@ -13,6 +13,7 @@ data LicenseListVersion
   deriving (Eq, Ord, Show, Enum, Bounded)
 
 cabalSpecVersionToSPDXListVersion :: CabalSpecVersion -> LicenseListVersion
+cabalSpecVersionToSPDXListVersion CabalSpecV3_2 = LicenseListVersion_3_6
 cabalSpecVersionToSPDXListVersion CabalSpecV3_0 = LicenseListVersion_3_6
 cabalSpecVersionToSPDXListVersion CabalSpecV2_4 = LicenseListVersion_3_2
 cabalSpecVersionToSPDXListVersion _             = LicenseListVersion_3_0
