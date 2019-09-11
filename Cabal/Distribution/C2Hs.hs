e74115ec41ac7cd305c88a725ee98542aa0cda84
Vanessa McHale <vamchale@gmail.com>
2019-09-10 22:30:47 -0500

    Style improvements + slimming

--- Cabal/Distribution/C2Hs.hs
+++ Cabal/Distribution/C2Hs.hs
@@ -1,74 +1,53 @@
-module Distribution.C2Hs ( fixDeps ) where
+-- Based on https://github.com/gtk2hs/gtk2hs/blob/master/tools/src/Gtk2HsSetup.hs#L414
+module Distribution.C2Hs ( reorderC2Hs ) where
 
-import qualified Data.Map                        as M
-import qualified Data.Set                        as S
-import           Distribution.C2Hs.Lexer
-import           Distribution.ModuleName         (ModuleName, components)
-import           Distribution.PackageDescription (PackageDescription)
-import qualified Distribution.PackageDescription as PD
-import           Distribution.Simple.Utils       (dieNoVerbosity,
-                                                  findFileWithExtension,
-                                                  withUTF8FileContents)
-import           Distribution.Text               (simpleParse)
-import           System.FilePath                 (joinPath)
+import Data.List (foldl')
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Distribution.C2Hs.Lexer
+import Distribution.ModuleName (ModuleName, components)
+import Distribution.Parsec (simpleParsec)
+import Distribution.Simple.Utils (dieNoVerbosity, findFileWithExtension, withUTF8FileContents)
+import System.FilePath (joinPath)
 
-fixDeps :: PackageDescription -> IO PackageDescription
-fixDeps pd@PD.PackageDescription {
-          PD.library = Just lib@PD.Library {
-            PD.exposedModules = expMods,
-            PD.libBuildInfo = bi@PD.BuildInfo {
-              PD.hsSourceDirs = srcDirs,
-              PD.otherModules = othMods
-            }}} = do
+reorderC2Hs :: [FilePath] -> [ModuleName] -> IO [ModuleName]
+reorderC2Hs dirs preMods = do
 
-  let findModule = findFileWithExtension [".chs"] srcDirs . joinPath . components
+  let findModule = findFileWithExtension [".chs"] dirs . joinPath . components
 
-  mExpFiles <- traverse findModule expMods
-  mOthFiles <- traverse findModule othMods
+  mFiles <- traverse findModule preMods
 
-  let modDeps = zipWith (ModDep True []) expMods mExpFiles ++
-                zipWith (ModDep False []) othMods mOthFiles
-  modDeps <- traverse extractDeps modDeps
-  let (othMods, expMods) = span (not . mdExposed) $ sortTopological modDeps
-  return pd { PD.library = Just lib {
-    PD.exposedModules = map mdOriginal (reverse expMods),
-    PD.libBuildInfo = bi { PD.otherModules = map mdOriginal (reverse othMods) }
-  }}
-fixDeps pd = pure pd
+  let preDeps = zipWith (ModDep []) preMods mFiles
 
-data ModDep = ModDep {
-  mdExposed  :: Bool,
-  mdRequires :: [ModuleName],
-  mdOriginal :: ModuleName,
-  mdLocation :: Maybe FilePath
-}
+  modDeps <- traverse extractDeps preDeps
 
-instance Show ModDep where
-  show x = show (mdLocation x)
+  let mods = reverse (sortTopological modDeps)
 
-instance Eq ModDep where
-  ModDep { mdOriginal = m1 } == ModDep { mdOriginal = m2 } = m1==m2
-instance Ord ModDep where
-  compare ModDep { mdOriginal = m1 } ModDep { mdOriginal = m2 } = compare m1 m2
+  pure (mdOriginal <$> mods)
+
+data ModDep = ModDep { mdRequires :: [ModuleName]
+                     , mdOriginal :: ModuleName
+                     , mdLocation :: Maybe FilePath
+                     }
 
 extractDeps :: ModDep -> IO ModDep
-extractDeps md@ModDep { mdLocation = Nothing } = return md
-extractDeps md@ModDep { mdLocation = Just f } = withUTF8FileContents f $ \con -> do
+extractDeps md@(ModDep _ _ Nothing) = pure md
+extractDeps md@(ModDep _ _ (Just f)) = withUTF8FileContents f $ \con -> do
   mods <- case getImports con of
-        Right ms -> case traverse simpleParse ms of
-            Just ms -> pure ms
+        Right ms -> case traverse simpleParsec ms of
+            Just ms' -> pure ms'
             Nothing -> dieNoVerbosity ("Cannot parse module name in c2hs file " ++ f)
         Left err -> dieNoVerbosity ("Cannot parse c2hs import in " ++ f ++ ": " ++ err)
-  return md { mdRequires = mods }
+  pure (md { mdRequires = mods })
 
 sortTopological :: [ModDep] -> [ModDep]
-sortTopological ms = fst $ foldl visit (([]), S.empty) (mdOriginal <$> ms)
+sortTopological ms = fst $ foldl' visit (([]), S.empty) (mdOriginal <$> ms)
   where
-  set = M.fromList (map (\m -> (mdOriginal m, m)) ms)
-  visit (out,visited) m
-    | m `S.member` visited = (out,visited)
-    | otherwise = case m `M.lookup` set of
-        Nothing -> (out, m `S.insert` visited)
-        Just md -> (md:out', visited')
-          where
-            (out',visited') = foldl visit (out, m `S.insert` visited) (mdRequires md)
+    set = M.fromList (fmap (\m -> (mdOriginal m, m)) ms)
+    visit (out,visited) m
+      | m `S.member` visited = (out,visited)
+      | otherwise = case m `M.lookup` set of
+          Nothing -> (out, m `S.insert` visited)
+          Just md -> (md:out', visited')
+            where
+              (out',visited') = foldl' visit (out, m `S.insert` visited) (mdRequires md)
