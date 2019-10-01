021689e10bbf1234bbfd48837fe768f5debf13c3
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-10-01 17:17:46 +0300

    Skip few tests on windows

--- cabal-testsuite/PackageTests/Backpack/Includes2/cabal-internal.test.hs
+++ cabal-testsuite/PackageTests/Backpack/Includes2/cabal-internal.test.hs
@@ -2,6 +2,7 @@ import Test.Cabal.Prelude
 
 main = cabalTest $ do
     skipUnless =<< ghcVersionIs (>= mkVersion [8,1])
+    skipIf =<< isWindows -- TODO: https://github.com/haskell/cabal/issues/6271
     withProjectFile "cabal.internal.project" $ do
         cabal "v2-build" ["exe"]
         withPlan $ do
