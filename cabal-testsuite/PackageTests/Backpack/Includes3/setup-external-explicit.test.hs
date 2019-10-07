15d5c013a81ffefa4f406c175344bee8ac40bf5a
Edward Z. Yang <ezyang@mit.edu>
2019-10-07 09:06:03 -0400

    Restructure Includes3 tests so that I can also test Hackage case.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- cabal-testsuite/PackageTests/Backpack/Includes3/setup-external-explicit.test.hs
+++ cabal-testsuite/PackageTests/Backpack/Includes3/setup-external-explicit.test.hs
@@ -3,5 +3,5 @@ import Test.Cabal.Prelude
 main = setupTest $ do
     skipUnless =<< ghcVersionIs (>= mkVersion [8,1])
     withPackageDb $ do
-      withDirectory "sigs" $ setup_install_with_docs ["--cid", "sigs-0.1.0.0", "lib:sigs"]
-      withDirectory "indef" $ setup_install_with_docs ["--cid", "indef-0.1.0.0", "--dependency=sigs=sigs-0.1.0.0", "lib:indef"]
+      withDirectory "repo/sigs-0.1.0.0" $ setup_install_with_docs ["--cid", "sigs-0.1.0.0", "lib:sigs"]
+      withDirectory "repo/indef-0.1.0.0" $ setup_install_with_docs ["--cid", "indef-0.1.0.0", "--dependency=sigs=sigs-0.1.0.0", "lib:indef"]
