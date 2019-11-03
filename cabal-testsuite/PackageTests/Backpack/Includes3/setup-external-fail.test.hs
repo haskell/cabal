129775a781f224fb2758b80f1ff2902fd2999b87
Edward Z. Yang <ezyang@mit.edu>
2019-11-03 09:49:17 -0500

    Compute correct install directories for instantiated Backpack components  (#6317)
    * Restructure Includes3 tests so that I can also test Hackage case.
    
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>
    
    * Compute correct install directories for instantiated Backpack components
    
    Previously, we would compute elabInstallDirs once when configuring a
    component, and then reuse the exactly same install directories for
    every instantiation of the package.  But this is wrong, since we're
    installing the header/object files for each instantiation to a different
    directory.  We refactor install directory computation into a helper
    function and then call it again at instantiation time to refresh
    the install directories.
    
    For some reason, this bug ONLY manifests for packages installed from
    Hackage; it seems install dirs are not respected for inplace packages.
    
    Fixes #6005
    
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>
    
    * Test fix
    
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>
    
    * Copy backpack version-guard from other backpack testcases

--- cabal-testsuite/PackageTests/Backpack/Includes3/setup-external-fail.test.hs
+++ cabal-testsuite/PackageTests/Backpack/Includes3/setup-external-fail.test.hs
@@ -2,10 +2,10 @@ import Test.Cabal.Prelude
 main = setupAndCabalTest $ do
     skipUnless =<< ghcVersionIs (>= mkVersion [8,1])
     withPackageDb $ do
-      withDirectory "sigs" $ setup_install []
-      withDirectory "indef" $ setup_install []
+      withDirectory "repo/sigs-0.1.0.0" $ setup_install []
+      withDirectory "repo/indef-0.1.0.0" $ setup_install []
       -- Forgot to build the instantiated versions!
-      withDirectory "exe" $ do
+      withDirectory "repo/exe-0.1.0.0" $ do
         -- Missing package message includes a unit identifier,
         -- which wobbles when version numbers change
         r <- recordMode DoNotRecord . fails $ setup' "configure" []
--- cabal-testsuite/PackageTests/Backpack/Includes3/setup-internal.test.hs
+++ cabal-testsuite/PackageTests/Backpack/Includes3/setup-internal.test.hs
@@ -3,7 +3,7 @@ main = setupAndCabalTest $ do
     skipUnless =<< ghcVersionIs (>= mkVersion [8,1])
     withPackageDb $ do
       setup_install []
-      _ <- runM "touch" ["indef/Foo.hs"]
+      _ <- runM "touch" ["repo/indef-0.1.0.0/Foo.hs"]
       setup "build" []
       runExe' "exe" [] >>= assertOutputContains "fromList [(0,2),(2,4)]"
 
