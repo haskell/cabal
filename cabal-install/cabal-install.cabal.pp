ef1d0c58994feff83cf8ac4d162c92d459541bfa
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-12-22 21:51:31 +0200

    Add --ignore-project flag to v2-run

--- cabal-install/cabal-install.cabal.pp
+++ cabal-install/cabal-install.cabal.pp
@@ -106,6 +106,7 @@ Version:            3.0.1.0
         Distribution.Client.CmdInstall.ClientInstallFlags
         Distribution.Client.CmdRepl
         Distribution.Client.CmdRun
+        Distribution.Client.CmdRun.ClientRunFlags
         Distribution.Client.CmdTest
         Distribution.Client.CmdLegacy
         Distribution.Client.CmdSdist
