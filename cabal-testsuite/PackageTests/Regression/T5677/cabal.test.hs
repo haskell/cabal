d3a2f961040b948c3c9c5fd8e6dac1788a998bdb
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-12-25 01:44:00 +0200

    T5677 rename prog to program, case-sensitive type-systems break

--- cabal-testsuite/PackageTests/Regression/T5677/cabal.test.hs
+++ cabal-testsuite/PackageTests/Regression/T5677/cabal.test.hs
@@ -2,5 +2,4 @@ import Test.Cabal.Prelude
 main = cabalTest $ do
   -- -Wmissing-export-lists is new in 8.4.
   skipUnless =<< ghcVersionIs (>= mkVersion [8,3])
-  skipIf =<< isWindows -- TODO: https://github.com/haskell/cabal/issues/6271
   cabal "v2-build" ["all"]
