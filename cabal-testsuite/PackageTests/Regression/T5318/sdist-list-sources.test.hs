93dd4518b669477e4b4fc2c80f475d5e62eeaab6
Oleg Grenrus <oleg.grenrus@iki.fi>
2020-04-02 18:52:14 +0300

    Make tests use v2-sdist

--- cabal-testsuite/PackageTests/Regression/T5318/sdist-list-sources.test.hs
+++ cabal-testsuite/PackageTests/Regression/T5318/sdist-list-sources.test.hs
@@ -1,7 +1,7 @@
 import Test.Cabal.Prelude
 main = cabalTest $ do
   tmpdir <- fmap testTmpDir getTestEnv
-  let fn = tmpdir </> "sources"
-  cabal "v1-sdist" ["--list-sources=" ++ fn]
+  let fn = tmpdir </> "empty-data-dir-0.list"
+  cabal "v2-sdist" ["--ignore-project", "--list-only", "--output-directory", tmpdir]
   -- --list-sources outputs with slashes on posix and backslashes on Windows. 'normalise' converts our needle to the necessary format.
   assertFileDoesContain fn $ normalise "foo.dat"
--- cabal-testsuite/PackageTests/SDist/T5195/cabal.test.hs
+++ cabal-testsuite/PackageTests/SDist/T5195/cabal.test.hs
@@ -1,6 +1,5 @@
 import Test.Cabal.Prelude
 main = cabalTest $ do
   tmpdir <- fmap testTmpDir getTestEnv
-  let fn = tmpdir </> "sources"
-  res <- fails $ cabal' "v1-sdist" ["--list-sources=" ++ fn]
+  res <- fails $ cabal' "v2-sdist" ["--ignore-project", "--list-only", "--output-directory", tmpdir]
   assertOutputContains "filepath wildcard './actually-a-directory' does not match any files" res
