25aef9911f0ef766a874b20463eb5a1dfbd77d2a
Luke Lau <luke_lau@icloud.com>
2020-06-08 21:17:32 +0100

    Generate autogen files
    These are needed by tooling to setup GHC sessions.

--- cabal-install/Distribution/Client/CmdShowBuildInfo.hs
+++ cabal-install/Distribution/Client/CmdShowBuildInfo.hs
@@ -55,11 +55,18 @@ import Distribution.Simple.Configure
 import Distribution.Simple.ShowBuildInfo
 import Distribution.Utils.Json
 
-import Distribution.Simple.BuildTarget (readTargetInfos)
-import Distribution.Types.LocalBuildInfo (neededTargetsInBuildOrder')
-import Distribution.Compat.Graph (IsNode(nodeKey))
-import Distribution.Simple.Setup (BuildFlags(buildArgs))
-import Distribution.Types.TargetInfo (TargetInfo(targetCLBI))
+import Distribution.Simple.BuildTarget
+        ( readTargetInfos )
+import Distribution.Types.LocalBuildInfo
+        ( neededTargetsInBuildOrder' )
+import Distribution.Compat.Graph
+        ( IsNode(nodeKey) )
+import Distribution.Simple.Setup
+        ( BuildFlags(..) )
+import Distribution.Types.TargetInfo
+        ( TargetInfo(..) )
+import Distribution.Simple.Build
+        ( componentInitialBuildSteps )
 
 showBuildInfoCommand :: CommandUI (NixStyleFlags ShowBuildInfoFlags)
 showBuildInfoCommand = CommandUI {
@@ -225,6 +232,12 @@ getComponentInfo verbosity baseCtx buildCtx lock pkgs targetUnitId =
       let pkgDesc = elabPkgDescription pkg
       targets <- readTargetInfos verbosity pkgDesc lbi (buildArgs flags)
       let targetsToBuild = neededTargetsInBuildOrder' pkgDesc lbi (map nodeKey targets)
+
+      -- generate autogen files which will be needed by tooling
+      flip mapM_ targetsToBuild $ \target ->
+        componentInitialBuildSteps (Cabal.fromFlag (buildDistPref flags))
+          pkgDesc lbi (targetCLBI target) verbosity
+
       return $ map (mkComponentInfo pkgDesc lbi . targetCLBI) targetsToBuild
 
     where
