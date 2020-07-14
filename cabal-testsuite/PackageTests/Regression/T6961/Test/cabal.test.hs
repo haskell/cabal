ec34e61172dce6ae146adccaaf2d81457f945859
Oleg Grenrus <oleg.grenrus@iki.fi>
2020-07-14 22:16:08 +0300

    Fix #6961. Depending on a private component from the same package is fine.

--- cabal-testsuite/PackageTests/Regression/T6961/Test/cabal.test.hs
+++ cabal-testsuite/PackageTests/Regression/T6961/Test/cabal.test.hs
@@ -1,5 +1,3 @@
 import Test.Cabal.Prelude
 main = cabalTest $
-  -- fails, but it shouldn't.
-  -- https://github.com/haskell/cabal/issues/6961
-  fails $ cabal "v2-build" ["all", "--dry-run", "--enable-tests"]
+  cabal "v2-build" ["all", "--dry-run", "--enable-tests"]
