a7d0148cf13f199b8a556bc7241298079f60e8fa
Oleg Grenrus <oleg.grenrus@iki.fi>
2020-11-19 15:04:24 +0200

    Remove v1-exec command

--- cabal-testsuite/PackageTests/Exec/legacy-no-args.test.hs
+++
@@ -1,2 +0,0 @@
-import Test.Cabal.Prelude
-main = cabalTest $ fails (cabal' "v1-exec" []) >>= assertOutputContains "Please specify an executable to run"
--- cabal-testsuite/PackageTests/Exec/legacy.test.hs
+++
@@ -1,5 +0,0 @@
-import Test.Cabal.Prelude
-main = cabalTest $ do
-    cabal "v1-configure" []
-    cabal' "v1-exec" ["echo", "find_me_in_output"]
-        >>= assertOutputContains "find_me_in_output"
