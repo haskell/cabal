957479895da15a7e26f60a99461087596a1608ba
Fendor <power.walross@gmail.com>
2021-02-22 17:36:01 +0100

    Add failing test-case for preferred versions in outdated command

--- cabal-testsuite/PackageTests/Outdated/outdated-project-file.test.hs
+++ cabal-testsuite/PackageTests/Outdated/outdated-project-file.test.hs
@@ -3,11 +3,13 @@ main = cabalTest $ withRepo "repo" $ do
   res <- cabal' "outdated" ["--v2-freeze-file", "--project-file", "variant.project"]
   assertOutputContains "base" res
   assertOutputDoesNotContain "template-haskell" res
+  assertOutputDoesNotContain "binary" res
 
   -- Test last-one-wins behaviour.
   res <- cabal' "outdated" ["--v2-freeze-file", "--project-file", "cabal.project", "--project-file", "variant.project"]
   assertOutputContains "base" res
   assertOutputDoesNotContain "template-haskell" res
+  assertOutputDoesNotContain "binary" res
 
   -- Test for erroring on --project-file without --v2-freeze-file
   fails $ cabal "outdated" ["--project-file", "variant.project"]
--- cabal-testsuite/PackageTests/Outdated/outdated_freeze.test.hs
+++ cabal-testsuite/PackageTests/Outdated/outdated_freeze.test.hs
@@ -5,14 +5,17 @@ main = cabalTest $ withRepo "repo"
   cabal' "outdated" [arg] >>=
     (\out -> do
         assertOutputContains "base" out
-        assertOutputContains "template-haskell" out)
+        assertOutputContains "template-haskell" out
+        assertOutputContains "binary" out)
 
-  cabal' "outdated" [arg, "--ignore=base,template-haskell"] >>=
+  cabal' "outdated" [arg, "--ignore=base,template-haskell,binary"] >>=
     (\out -> do
         assertOutputDoesNotContain "base" out
-        assertOutputDoesNotContain "template-haskell" out)
+        assertOutputDoesNotContain "template-haskell" out
+        assertOutputDoesNotContain "binary" out)
 
-  cabal' "outdated" [arg, "--minor=base,template-haskell"] >>=
+  cabal' "outdated" [arg, "--minor=base,template-haskell,binary"] >>=
     (\out -> do
         assertOutputDoesNotContain "base" out
-        assertOutputContains "template-haskell" out)
+        assertOutputContains "template-haskell" out
+        assertOutputContains "binary" out)
