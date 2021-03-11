e7ba7834715855714feca008465588be678b2deb
Bodigrim <andrew.lelechenko@gmail.com>
2021-03-11 21:45:46 +0000

    Revert "Require base16-bytestring >= 1.0"
    This reverts commit 981f6d9dfaacbb74f0e1a577e0ad6e5b83f5df1a.

--- cabal-install/src/Distribution/Client/HttpUtils.hs
+++ cabal-install/src/Distribution/Client/HttpUtils.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP          #-}
 -----------------------------------------------------------------------------
 -- | Separate module for HTTP actions, using a proxy server if one exists.
 -----------------------------------------------------------------------------
@@ -183,7 +184,11 @@ downloadURI transport verbosity uri path = do
         _ <- P.string "#sha256="
         str <- some P.hexDigit
         let bs = Base16.decode (BS8.pack str)
+#if MIN_VERSION_base16_bytestring(1,0,0)
         either fail return bs
+#else
+        return (fst bs)
+#endif
 
 ------------------------------------------------------------------------------
 -- Utilities for repo url management
