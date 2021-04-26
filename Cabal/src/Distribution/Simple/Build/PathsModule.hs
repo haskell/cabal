de378ca64fc4c44400ebbac8d72528e4b6c14d4c
Matthew Bauer <mjbauer95@gmail.com>
2021-04-26 12:10:11 -0500

    Remove unused fields

--- Cabal/src/Distribution/Simple/Build/PathsModule.hs
+++ Cabal/src/Distribution/Simple/Build/PathsModule.hs
@@ -61,8 +61,6 @@ generatePathsModule pkg_descr lbi clbi = Z.render Z.Z
     supports_cpp                 = supports_language_pragma
     supports_rebindable_syntax   = ghc_newer_than (mkVersion [7,0,1])
     supports_language_pragma     = ghc_newer_than (mkVersion [6,6,1])
-    supports_missing_safehaskell = ghc_newer_than (mkVersion [8,10,1])
-    supports_prepositive_qualified_module = ghc_newer_than (mkVersion [8,10,1])
 
     ghc_newer_than minVersion =
         case compilerCompatVersion GHC (compiler lbi) of
