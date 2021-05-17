f3f16efd812f6d55bb679f6fd9f8af626aa13739
Mikolaj Konarski <mikolaj@well-typed.com>
2021-05-17 09:54:43 +0200

    Add a dummy --installdir to T5782Diamond test

--- cabal-testsuite/PackageTests/Regression/T5782Diamond/cabal.test.hs
+++ cabal-testsuite/PackageTests/Regression/T5782Diamond/cabal.test.hs
@@ -1,4 +1,4 @@
--- when Module.f is changed, with cabal <= 3.2 this non-deterministically fails
+-- When Module.f is changed, with cabal <= 3.2 this non-deterministically fails
 -- to compile and, if it doesn't fail, it also non-deterministically gives
 -- a wrong answer (ignoring the change to Module.f in the output, despite
 -- recompiling, so probably the wrong library is linked in); when running
@@ -6,27 +6,30 @@
 -- the error, often two are enough, even with cabal 3.2, even to get
 -- compilation error
 --   "Ambiguous module name `Module': it was found in multiple packages: issue5782-0.1 issue5782-0.1"
--- not only the wrong result from exe run
+-- not only the wrong result from exe run.
+--
+-- The dummy "--installdir=." is needed for cabal <= 3.2
+-- and also to match cabal output on different OSes
+-- (default installdir is different on various OSes).
 
 import Test.Cabal.Prelude
-main = cabalTest $
+main = withShorterPathForNewBuildStore $ \storeDir ->
+  cabalTest $
     withSourceCopy . withDelay $ do
         writeSourceFile "issue5782/src/Module.hs" "module Module where\nf = \"AAA\""
-        cabal "v2-install" ["--overwrite-policy=always", "issue5782"]
-        -- this is needed for cabal 3.2 to fail for the right reason:
-        -- cabal "v2-install" ["--installdir=.", "--overwrite-policy=always", "issue5782"]
+        cabalG ["--store-dir=" ++ storeDir, "--installdir=" ++ storeDir, "--overwrite-policy=always"] "v2-install" ["issue5782"]
         withPlan $
             runPlanExe' "issue5782" "E" []
                 >>= assertOutputContains "AAA"
         delay
         writeSourceFile "issue5782/src/Module.hs" "module Module where\nf = \"BBB\""
-        cabal "v2-install" ["--overwrite-policy=always", "issue5782"]
+        cabalG ["--store-dir=" ++ storeDir, "--installdir=" ++ storeDir, "--overwrite-policy=always"] "v2-install" ["issue5782"]
         withPlan $
             runPlanExe' "issue5782" "E" []
                 >>= assertOutputContains "BBB"
         writeSourceFile "issue5782/src/Module.hs" "module Module where\nf = \"CCC\""
         delay  -- different spot to try another scenario
-        cabal "v2-install" ["--overwrite-policy=always", "issue5782"]
+        cabalG ["--store-dir=" ++ storeDir, "--installdir=" ++ storeDir, "--overwrite-policy=always"] "v2-install" ["issue5782"]
         withPlan $
             runPlanExe' "issue5782" "E" []
                 >>= assertOutputContains "CCC"
