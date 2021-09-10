e730b7248e238dc51c7f6fb59ccd14c645abee8a
Julian Ospald <hasufell@posteo.de>
2021-09-11 00:31:39 +0200

    Add darwin aarch64 support to CI (#7636)

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -2,110 +2,42 @@
 
 set -Eeuo pipefail
 
-TOP="$(pwd)"
-source "$TOP/.gitlab/common.sh"
+source "$CI_PROJECT_DIR/.gitlab/common.sh"
 
-# We don't have sudo access in GitLab CI, so we can't just dump binaries in
-# the usual PATH locations.
-toolchain="$(pwd)/toolchain"
-mkdir -p "$toolchain/bin"
-export PATH="$toolchain/bin:$PATH"
 
-export CABAL_DIR="$TOP/cabal"
-mkdir -p "$CABAL_DIR"
+export GHCUP_INSTALL_BASE_PREFIX="$CI_PROJECT_DIR/toolchain"
+export CABAL_DIR="$CI_PROJECT_DIR/cabal"
 
-# Platform-specific bits
-MAKE="make"
 case "$(uname)" in
     MSYS_*|MINGW*)
-        CABAL_DIR="$(cygpath -w "$CABAL_DIR")"
+        export CABAL_DIR="$(cygpath -w "$CABAL_DIR")"
+		GHCUP_BINDIR="${GHCUP_INSTALL_BASE_PREFIX}/ghcup/bin"
         ;;
-    FreeBSD)
-        MAKE="gmake"
-        ;;
+	*)
+		GHCUP_BINDIR="${GHCUP_INSTALL_BASE_PREFIX}/.ghcup/bin"
+		;;
 esac
 
-fetch_cabal_install_unix() {
-    local cabal_url="$1"
-    run curl -L -o cabal.tar.xz "$cabal_url"
-    run tar --directory "$toolchain/bin/" -xf cabal.tar.xz cabal
-    chmod +x "$toolchain/bin/cabal"
-    CABAL="$toolchain/bin/cabal"
-}
+mkdir -p "$CABAL_DIR"
+mkdir -p "$GHCUP_BINDIR"
+export PATH="$GHCUP_BINDIR:$PATH"
 
-setup_cabal_install() {
-    if [ -z "${CABAL:-}" ]; then
-        info "Fetching GHC..."
-        case "$(uname)" in
-            FreeBSD)
-                fetch_cabal_install_unix "https://hasufell.de/d/d3e215db133e4fcaa61e/files/?p=/cabal-install-$CABAL_INSTALL_VERSION-x86_64-portbld-freebsd.tar.xz&dl=1" ;;
-            Darwin)
-                fetch_cabal_install_unix "https://downloads.haskell.org/~cabal/cabal-install-$CABAL_INSTALL_VERSION/cabal-install-$CABAL_INSTALL_VERSION-x86_64-apple-darwin17.7.0.tar.xz" ;;
-            MSYS_*|MINGW*)
-                cabal_url="https://downloads.haskell.org/~cabal/cabal-install-$CABAL_INSTALL_VERSION/cabal-install-$CABAL_INSTALL_VERSION-x86_64-unknown-mingw32.zip"
-                run curl -L -o cabal.zip "$cabal_url"
-                run unzip cabal.zip
-                cp cabal.exe "$toolchain/bin/cabal.exe"
-                chmod +x "$toolchain/bin/cabal.exe"
-                CABAL="$toolchain/bin/cabal.exe"
-                ;;
-            *) fail "no cabal-install bindist for $(uname)"
-        esac
+export BOOTSTRAP_HASKELL_NONINTERACTIVE=1
+export BOOTSTRAP_HASKELL_GHC_VERSION=$GHC_VERSION
+export BOOTSTRAP_HASKELL_CABAL_VERSION=$CABAL_INSTALL_VERSION
+export BOOTSTRAP_HASKELL_VERBOSE=1
+export BOOTSTRAP_HASKELL_ADJUST_CABAL_CONFIG=yes
 
-    fi
-    export CABAL
-    run "$CABAL" --version
-}
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 
-
-fetch_ghc_unix() {
-    local ghc_url="$1"
-    run curl -sSfL -o ghc.tar.xz "$ghc_url"
-    run tar -xf ghc.tar.xz
-
-    pushd "ghc-${GHC_VERSION}"
-    run ./configure --prefix="$toolchain"
-    run "$MAKE" install
-    popd
-    GHC="$toolchain/bin/ghc"
-}
-
-setup_ghc() {
-    if [ -z "${GHC:-}" ]; then
-        info "Fetching GHC..."
-        case "$(uname)" in
-            FreeBSD)
-                fetch_ghc_unix "https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-x86_64-unknown-freebsd.tar.xz" ;;
-            Darwin)
-                fetch_ghc_unix "https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-x86_64-apple-darwin.tar.xz" ;;
-            MSYS_*|MINGW*)
-                ghc_url="https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-x86_64-unknown-mingw32.tar.xz"
-                run curl -sSfL -o ghc.tar.xz "$ghc_url"
-                run tar -xf ghc.tar.xz
-                cp -R ghc-$GHC_VERSION/* "$toolchain"
-                GHC="$toolchain/bin/ghc.exe"
-                run "$GHC" --version
-                GHC="$(cygpath -w $GHC)"
-                return
-                ;;
-            *) fail "no GHC bindist for $(uname)"
-        esac
-    fi
-
-    export GHC
-    run "$GHC" --version
-}
-
-setup_cabal_install
-setup_ghc
-
-run "$CABAL" update
-run "$CABAL" v2-install cabal-install \
-    -w "$GHC" \
-    --installdir="$TOP/out" \
+run cabal v2-install cabal-install \
+    -w "ghc-$GHC_VERSION" \
+    --installdir="$CI_PROJECT_DIR/out" \
     --install-method=copy \
     --overwrite-policy=always \
     --enable-executable-static \
     --disable-profiling \
     --enable-split-sections \
     --enable-executable-stripping
+
+cp dist-newstyle/cache/plan.json "$CI_PROJECT_DIR/out/plan.json"
