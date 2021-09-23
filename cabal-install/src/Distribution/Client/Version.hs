c857e3c81bf7dc1ffc74dca5291154ab9c813c60
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-09-23 20:29:12 +0200

    Fix #7569 for cabal-install: bump to 3.7

--- cabal-install/src/Distribution/Client/Version.hs
+++ cabal-install/src/Distribution/Client/Version.hs
@@ -9,4 +9,4 @@ import Distribution.Version
 -- TODO: write a test around this. Don't abuse Paths_cabal_install.
 --
 cabalInstallVersion :: Version
-cabalInstallVersion = mkVersion [3,6]
+cabalInstallVersion = mkVersion [3,7]
