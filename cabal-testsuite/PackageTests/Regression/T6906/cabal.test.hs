b36e3981214a62a02196b785f05c75a6750b2ead
jneira <atreyu.bbb@gmail.com>
2021-10-17 23:13:22 +0200

    Add tests over pks hash inputs

--- cabal-testsuite/PackageTests/Regression/T6906/cabal.test.hs
+++ cabal-testsuite/PackageTests/Regression/T6906/cabal.test.hs
@@ -1,4 +1,9 @@
 import Test.Cabal.Prelude
+
 main = cabalTest $
-  -- this should not fail, just warn.
-   cabal' "v2-install" []
+   cabalG' ["--config=cabal.config"] "v2-install" ["-v3"]
+   assertOutputContains "File with the inputs used to compute the package hash:"
+   assertOutputContains "extra-lib-dirs: bar"
+   expectBroken 6906 $ assertOutputDoesNotContain "extra-lib-dirs: bar bar"
+   assertOutputContains "extra-include-dirs: foo"
+   expectBroken 6906 $ assertOutputDoesNotContain "extra-include-dirs: foo foo"
