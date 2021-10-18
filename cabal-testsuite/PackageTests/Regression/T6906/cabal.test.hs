18deedea8c777cc339b254487e7a32820264c12c
Javier Neira <atreyu.bbb@gmail.com>
2021-10-18 18:30:48 +0200

    Remove knownBroken

--- cabal-testsuite/PackageTests/Regression/T6906/cabal.test.hs
+++ cabal-testsuite/PackageTests/Regression/T6906/cabal.test.hs
@@ -4,6 +4,6 @@ main = cabalTest $ do
    res <- cabalG' ["--config=cabal.config"] "v2-install" ["-v3"]
    assertOutputContains "creating file with the inputs used to compute the package hash:" res
    assertOutputContains "extra-lib-dirs: bar" res
-   expectBroken 6906 $ assertOutputDoesNotContain "extra-lib-dirs: bar bar" res
+   assertOutputDoesNotContain "extra-lib-dirs: bar bar" res
    assertOutputContains "extra-include-dirs: foo" res
-   expectBroken 6906 $ assertOutputDoesNotContain "extra-include-dirs: foo foo" res
+   assertOutputDoesNotContain "extra-include-dirs: foo foo" res
