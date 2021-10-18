b1f7bbe3bb856a650718e3fcffd5330047f00b6e
jneira <atreyu.bbb@gmail.com>
2021-10-18 23:37:29 +0200

    Try recordMode DoNotRecord

--- cabal-testsuite/PackageTests/Regression/T6906/cabal.test.hs
+++ cabal-testsuite/PackageTests/Regression/T6906/cabal.test.hs
@@ -1,7 +1,7 @@
 import Test.Cabal.Prelude
 
 main = cabalTest $ do
-   res <- cabalG' ["--config=cabal.config"] "v2-install" ["-v3"]
+   res <- recordMode DoNotRecord $ cabalG' ["--config=cabal.config"] "v2-install" ["-v3"]
    assertOutputContains "creating file with the inputs used to compute the package hash:" res
    assertOutputContains "extra-lib-dirs: bar" res
    assertOutputDoesNotContain "extra-lib-dirs: bar bar" res
