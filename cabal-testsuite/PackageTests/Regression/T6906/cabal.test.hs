9ed92cc811039bbf4c0cb323c9d3ce5e66d24239
jneira <atreyu.bbb@gmail.com>
2021-10-18 08:25:17 +0200

    Add missing cabal result arg

--- cabal-testsuite/PackageTests/Regression/T6906/cabal.test.hs
+++ cabal-testsuite/PackageTests/Regression/T6906/cabal.test.hs
@@ -1,9 +1,9 @@
 import Test.Cabal.Prelude
 
 main = cabalTest $ do
-   cabalG' ["--config=cabal.config"] "v2-install" ["-v3"]
-   assertOutputContains "File with the inputs used to compute the package hash:"
-   assertOutputContains "extra-lib-dirs: bar"
-   expectBroken 6906 $ assertOutputDoesNotContain "extra-lib-dirs: bar bar"
-   assertOutputContains "extra-include-dirs: foo"
-   expectBroken 6906 $ assertOutputDoesNotContain "extra-include-dirs: foo foo"
+   res <- cabalG' ["--config=cabal.config"] "v2-install" ["-v3"]
+   assertOutputContains "File with the inputs used to compute the package hash:" res
+   assertOutputContains "extra-lib-dirs: bar" res
+   expectBroken 6906 $ assertOutputDoesNotContain "extra-lib-dirs: bar bar" res
+   assertOutputContains "extra-include-dirs: foo" res
+   expectBroken 6906 $ assertOutputDoesNotContain "extra-include-dirs: foo foo" res
