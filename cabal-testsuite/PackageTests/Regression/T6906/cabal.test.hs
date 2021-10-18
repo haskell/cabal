f7730e184737ccbc7f78b8bbf17202de5958dfe1
jneira <atreyu.bbb@gmail.com>
2021-10-18 09:50:59 +0200

    Update log msg creating file

--- cabal-testsuite/PackageTests/Regression/T6906/cabal.test.hs
+++ cabal-testsuite/PackageTests/Regression/T6906/cabal.test.hs
@@ -2,7 +2,7 @@ import Test.Cabal.Prelude
 
 main = cabalTest $ do
    res <- cabalG' ["--config=cabal.config"] "v2-install" ["-v3"]
-   assertOutputContains "File with the inputs used to compute the package hash:" res
+   assertOutputContains "creating file with the inputs used to compute the package hash:" res
    assertOutputContains "extra-lib-dirs: bar" res
    expectBroken 6906 $ assertOutputDoesNotContain "extra-lib-dirs: bar bar" res
    assertOutputContains "extra-include-dirs: foo" res
