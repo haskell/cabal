5675bcbcb1f1d780e7571a838c250208ec336a0c
Samuel Gélineau <gelisam+github@gmail.com>
2021-11-20 20:34:40 -0500

    test current cabal-test behaviour
    I plan to add an explicit --only-buildable-tests flag, and then to add a
    test for it. I will do so by adding a case to this test, which currently
    tests the two other flags, --enable-tests and --disable-tests, as well
    as the default behaviour.

---
+++ cabal-testsuite/PackageTests/NewBuild/CmdTest/OnlyBuildableTests/cabal.test.hs
@@ -0,0 +1,23 @@
+import Test.Cabal.Prelude
+
+main = cabalTest $ do
+    -- "--enable-tests" and "--disable-tests" are not the only two
+    -- possibilities, they are only the two extremes: build _all_ the tests,
+    -- and build _none_ of the tests.
+    --
+    -- The default, which does not currently correspond to a named option, is
+    -- to only build the tests for which a build plan can be found, and to
+    -- silently ignore the tests for which a build plan cannot be found.
+    
+    -- This project has two package; one with a buildable test, and one with an
+    -- unbuildable test. If we request both tests to be built and run, then
+    -- "cabal test" command should fail because of the unbuildable test.
+    fails $ cabal "v2-test" ["--enable-tests", "all"]
+    
+    -- If we request zero tests to be built, then "cabal test" should fail
+    -- because there are no tests to run.
+    fails $ cabal "v2-test" ["--disable-tests", "all"]
+
+    -- If we request the buildable tests to be built and run, then "cabal test"
+    -- should successfully build one test.
+    cabal "v2-test" ["all"]
