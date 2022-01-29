81cac8403cbb09daa21e54730c29425a466843f2
Mel Zuser <mel@mzero.dev>
2022-01-28 19:29:56 -0500

    Rename test file b/c of Windows path length

--- cabal-testsuite/PackageTests/NewBuild/CmdRun/ScriptNoExtention/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/CmdRun/ScriptNoExtention/cabal.test.hs
@@ -1,4 +1,4 @@
 import Test.Cabal.Prelude
 
 main = cabalTest . void $ do
-    cabal' "v2-run" ["with space"] >>= assertOutputContains "Hello World"
+    cabal' "v2-run" ["with sp"] >>= assertOutputContains "Hello World"
