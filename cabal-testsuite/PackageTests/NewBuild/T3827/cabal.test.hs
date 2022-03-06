572197a600bbcd94be0e9c04f4f863a45b87c295
jneira <atreyu.bbb@gmail.com>
2022-03-06 18:50:47 +0100

    Fix unexpected ok for windows

--- cabal-testsuite/PackageTests/NewBuild/T3827/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/T3827/cabal.test.hs
@@ -1,7 +1,8 @@
 import Test.Cabal.Prelude
 main = cabalTest $ do
+  win <- isWindows
   missesProfiling <- isGhcVersion ">= 9.2.1"
   osx <- isOSX
   missesProfilingOsx <- isGhcVersion ">= 8.10.7"
-  expectBrokenIf (missesProfiling || osx && missesProfilingOsx) 8032 $
+  expectBrokenIf (missesProfiling && not win || osx && missesProfilingOsx) 8032 $
     cabal "v2-build" ["exe:q"]
