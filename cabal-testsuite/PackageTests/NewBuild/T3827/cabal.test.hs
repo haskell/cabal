e1690c19218a21fffbcdba6c57512a6a8a2476fb
jneira <atreyu.bbb@gmail.com>
2022-03-07 13:15:10 +0100

    Skip T3827 for ghc-9.0.2, see #8032

--- cabal-testsuite/PackageTests/NewBuild/T3827/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/T3827/cabal.test.hs
@@ -1,8 +1,8 @@
 import Test.Cabal.Prelude
 main = cabalTest $ do
-  win <- isWindows
-  missesProfiling <- isGhcVersion ">= 9.2.1"
+  linux <- isLinux
+  missesProfilingLinux <- isGhcVersion ">= 9.0.2"
   osx <- isOSX
   missesProfilingOsx <- isGhcVersion ">= 8.10.7"
-  expectBrokenIf (missesProfiling && not win || osx && missesProfilingOsx) 8032 $
+  expectBrokenIf (linux && missesProfilingLinux || osx && missesProfilingOsx) 8032 $
     cabal "v2-build" ["exe:q"]
