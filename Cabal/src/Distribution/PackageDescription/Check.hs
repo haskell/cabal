b1b019fc24e080b552ed3e5b722cc46a5a9be32d
Musab Guma'a <u2104643@live.warwick.ac.uk>
2022-03-29 14:45:50 +0100

    fix failing tests

--- Cabal/src/Distribution/PackageDescription/Check.hs
+++ Cabal/src/Distribution/PackageDescription/Check.hs
@@ -1193,7 +1193,7 @@ checkCabalVersion pkg =
 
   -- check use of build-tool-depends
   , checkVersion CabalSpecV2_0
-    (not (null (buildToolDepends pkg))) $
+    (not (null (buildToolDepends $ allBuildInfo pkg))) $
       PackageBuildWarning $
         "To use the 'build-tool-depends' field the package needs to specify "
         ++ "at least 'cabal-version: >= 2.0'"
