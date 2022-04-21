fe1e7dc552bb8c63ea1e8db674ae34925e888cae
Eric Lindblad <askeblad@outlook.com>
2022-04-21 14:50:15 -0500

    reinsert unneed

--- cabal-testsuite/PackageTests/InternalVersions/BuildDependsExtra/setup.test.hs
+++ cabal-testsuite/PackageTests/InternalVersions/BuildDependsExtra/setup.test.hs
@@ -1,5 +1,5 @@
 import Test.Cabal.Prelude
--- Test unneeded version bound on internal build-tools deps
+-- Test unneed version bound on internal build-tools deps
 main = setupAndCabalTest . expectBroken 7470 $ do
     setup' "configure" []
     assertOutputContains "extraneous version range"
