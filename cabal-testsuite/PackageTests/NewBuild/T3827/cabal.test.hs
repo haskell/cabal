bcd7829d6a7136292fa83be531906ff183aba3a9
Mikolaj Konarski <mikolaj@well-typed.com>
2022-08-04 02:02:23 +0200

    Turn off T3827 for Linux and OSX due to heisenbugs not caused by cabal (#8338)
    * Turn off T3827 for new GHCs due to heisenbugs not caused by cabal
    
    * Disable the test totally on Linux until we stop taking GHC from GHA
    
    See https://github.com/haskell/cabal/issues/8032#issuecomment-1204113187
    
    * It failed on OSX now, so let's disable it everywhere except on Windows
    
    Who would have thought.
    
    (cherry picked from commit 91a343f586281f9d9373e8e57a87ce17078fef33)

--- cabal-testsuite/PackageTests/NewBuild/T3827/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/T3827/cabal.test.hs
@@ -1,8 +1,6 @@
 import Test.Cabal.Prelude
 main = cabalTest $ do
   linux <- isLinux
-  missesProfilingLinux <- isGhcVersion ">= 9.0.2"
   osx <- isOSX
-  missesProfilingOsx <- isGhcVersion ">= 8.10.7"
-  expectBrokenIf (linux && missesProfilingLinux || osx && missesProfilingOsx) 8032 $
-    cabal "v2-build" ["exe:q"]
+  skipIf "8032 heisenbug profiling" (linux || osx)
+  cabal "v2-build" ["exe:q"]
