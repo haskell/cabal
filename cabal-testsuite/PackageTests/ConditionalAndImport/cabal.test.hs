8a0ce3ec7864341bb984cac9bfb64f77eb3e57ec
Gershom Bazerman <gershom@arista.com>
2022-08-11 16:32:37 -0400

    poke tests around

--- cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
+++ cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
@@ -1,10 +1,10 @@
 import Test.Cabal.Prelude
 main = cabalTest $
   withRepo "repo" $ do
-     cabal "v2-run" [ "some-exe" ]
-     fails $ cabal "v2-build" [ "--project=cabal-cyclical.project" ]
-     fails $ cabal "v2-build" [ "--project=cabal-bad-conditional.project" ]
-     cabal "v2-clean" []
      -- this shouldn't succeed, but it does, because this doesn't override path correctly, sigh.
      fails $ withEnv [("PATH",Just "")] $ cabal "v2-sdist" []
      withEnv [("PATH", Just "")] $ cabal "v2-sdist" ["--project-file=boring.project"]
+
+     cabal "v2-run" [ "some-exe" ]
+     fails $ cabal "v2-build" [ "--project=cabal-cyclical.project" ]
+     fails $ cabal "v2-build" [ "--project=cabal-bad-conditional.project" ]
