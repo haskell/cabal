b84979d87515e51b9496598f7672bab5d4827a22
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-08-15 22:03:55 -0400

    fix doctest

--- Cabal/src/Distribution/Simple/Setup.hs
+++ Cabal/src/Distribution/Simple/Setup.hs
@@ -2527,7 +2527,7 @@ configureProg verbosity programDb prog = do
 -- It's supposed to handle quoted things sensibly, eg:
 --
 -- >>> splitArgs "--foo=\"C:/Program Files/Bar/\" --baz"
--- ["--foo=C:/Program Files/Bar/", "--baz"]
+-- ["--foo=C:/Program Files/Bar/","--baz"]
 --
 -- >>> splitArgs "\"-DMSGSTR=\\\"foo bar\\\"\" --baz"
 -- ["-DMSGSTR=\"foo bar\"","--baz"]
