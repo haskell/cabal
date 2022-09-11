78cbeba1a6639ae870d5f3ca03c7dd4603b2cdcb
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-09-11 10:20:33 -0400

    more Windows tests failing on GHC 9.4 marked broken

--- cabal-testsuite/PackageTests/NewBuild/CmdTest/OptionsFlag/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/CmdTest/OptionsFlag/cabal.test.hs
@@ -1,6 +1,9 @@
 import Test.Cabal.Prelude
 
 main = cabalTest $ do
+  isWin <- isWindows
+  ghc94 <- isGhcVersion "== 9.4.*"
+  expectBrokenIf (isWin && ghc94) 8451 $ do
     cabal "v2-test"
       [ "--test-option=1"
       , "--test-options=\"2 3\""
