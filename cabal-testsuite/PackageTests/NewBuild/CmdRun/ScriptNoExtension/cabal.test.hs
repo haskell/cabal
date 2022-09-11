46a52f191cad6f7b61792d35a9f7bdd2191de57f
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2022-09-11 10:20:33 -0400

    more Windows tests failing on GHC 9.4 marked broken

--- cabal-testsuite/PackageTests/NewBuild/CmdRun/ScriptNoExtension/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/CmdRun/ScriptNoExtension/cabal.test.hs
@@ -1,7 +1,4 @@
 import Test.Cabal.Prelude
 
 main = cabalTest . void $ do
-  isWin <- isWindows
-  ghc94 <- isGhcVersion "== 9.4.*"
-  expectBrokenIf (isWin && ghc94) 8451 $ do
     cabal' "v2-run" ["with sp"] >>= assertOutputContains "Hello World"
--- cabal-testsuite/PackageTests/NewBuild/CmdRun/ScriptWithProjectBlock/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/CmdRun/ScriptWithProjectBlock/cabal.test.hs
@@ -1,9 +1,6 @@
 import Test.Cabal.Prelude
 
 main = cabalTest $ do
-  isWin <- isWindows
-  ghc94 <- isGhcVersion "== 9.4.*"
-  expectBrokenIf (isWin && ghc94) 8451 $ do
     -- script is called "s.hs" to avoid Windows long path issue in CI
     res <- cabal' "v2-run" ["s.hs"]
     assertOutputContains "Hello World" res
