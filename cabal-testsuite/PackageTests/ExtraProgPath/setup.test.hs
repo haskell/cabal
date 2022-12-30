a21c039775badcf135a6f3f25f8c52606131bc00
Gershom Bazerman <gershom@arista.com>
2022-12-29 22:42:50 -0500

    skip on windows

--- cabal-testsuite/PackageTests/ExtraProgPath/setup.test.hs
+++ cabal-testsuite/PackageTests/ExtraProgPath/setup.test.hs
@@ -2,5 +2,6 @@ import Test.Cabal.Prelude
 
 -- Test that setup shows all the 'autogen-modules' warnings.
 main = cabalTest $ do
+  skipIfWindows
   cdir <- testCurrentDir `fmap` getTestEnv
   fails $ cabal "v2-build" ["--extra-prog-path="++cdir]
