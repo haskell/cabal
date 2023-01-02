de8ddd2efe53449ba1e05469f7687953365d0af5
gbaz <gershomb@gmail.com>
2023-01-02 14:41:18 -0500

    reenable windows skip

--- cabal-testsuite/PackageTests/ExtraProgPath/setup.test.hs
+++ cabal-testsuite/PackageTests/ExtraProgPath/setup.test.hs
@@ -2,6 +2,6 @@ import Test.Cabal.Prelude
 
 -- Test that extra-prog-path overrides the path for pkg-config
 main = cabalTest $ do
-  -- skipIfWindows
+  skipIfWindows
   cdir <- testCurrentDir `fmap` getTestEnv
   fails $ cabal "v2-build" ["--extra-prog-path="++cdir]
