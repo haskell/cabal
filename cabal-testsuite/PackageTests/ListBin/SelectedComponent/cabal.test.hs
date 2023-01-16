55d6f3213344d4572f32a0eb7367d282c68fa743
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-01-16 11:49:39 +0100

    Fix #8400 by marking output of `cabal list-bin` (#8670)
    * Fix #8400 by marking output of `cabal list-bin`
    
    The regular output of `cabal list-bin` should go to stdout always, but
    be marked for the sake of the testsuite.
    
    * Make test Windows-compatible
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- cabal-testsuite/PackageTests/ListBin/SelectedComponent/cabal.test.hs
+++ cabal-testsuite/PackageTests/ListBin/SelectedComponent/cabal.test.hs
@@ -1,8 +1,12 @@
+import System.FilePath ( joinPath )
+
 import Test.Cabal.Prelude
 
 -- https://github.com/haskell/cabal/issues/7679
+-- https://github.com/haskell/cabal/issues/8400
+
 main = cabalTest . void $ do
     res <- cabal' "list-bin" ["exe:testexe"]
 
-    assertOutputContains "SelectedComponent-1.0.0" res
-    assertOutputContains "testexe" res
+    let path = joinPath ["SelectedComponent-1.0.0", "build", "testexe", "testexe"]
+    assertOutputContains path res
