d3b69bd65feb557b4a0b296f42491deeed964d4a
BasLaa <baslaarakker@gmail.com>
2023-01-29 16:41:08 +0100

    Add executable in RTS warning test

--- cabal-testsuite/PackageTests/NewBuild/CmdRun/WarningRTS/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/CmdRun/WarningRTS/cabal.test.hs
@@ -2,11 +2,11 @@ import Test.Cabal.Prelude
 
 main = cabalTest $ do
     -- your test code here
-    res <- cabal' "run" ["+RTS"]
+    res <- cabal' "run" ["foo", "+RTS"]
     assertOutputContains       "Warning" res
 
-    res <- cabal' "run" [ "+RTS", "--"]
+    res <- cabal' "run" ["foo", "+RTS", "--"]
     assertOutputContains       "Warning" res
 
-    res <- cabal' "run" [ "--", "+RTS"]
+    res <- cabal' "run" ["foo", "--", "+RTS"]
     assertOutputDoesNotContain "Warning" res
