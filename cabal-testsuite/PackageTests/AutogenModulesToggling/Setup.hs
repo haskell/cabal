664e17db2458b67378d0d0e3bd7de8096d5e55d0
Sergey Vinokurov <1149355+sergv@users.noreply.github.com>
2023-02-10 01:35:05 +0000

    For ghc calls pass Cabal's build/ directory after user's src/ directory so that contents of src/ takes precedence (#8690)
    * Add test
    
    * Pass build/ directory after src/ so that whatever’s in src/ will override any files we autogenerated
    
    * Add expected output. Constrait test to run on recent enough ghc that will have Cabal updated
    
    * Force test to requre development version of Cabal
    
    Otherwise output will not be correct. It’s tricky to pass proper
    Cabal/ version to this test because it needs it for the custom setup.
    
    * Add changelog
    
    * Fix other tests

---
+++ cabal-testsuite/PackageTests/AutogenModulesToggling/Setup.hs
@@ -0,0 +1,24 @@
+
+module Main (main) where
+
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.PreProcess
+import Distribution.Simple.Program
+import Distribution.Types.BuildInfo
+import Distribution.Verbosity
+
+import System.Directory
+
+ppHGen :: BuildInfo -> LocalBuildInfo -> ComponentLocalBuildInfo -> PreProcessor
+ppHGen _bi lbi _clbi = PreProcessor
+  { platformIndependent = True
+  , ppOrdering          = unsorted
+  , runPreProcessor     = mkSimplePreProcessor $ \inFile outFile verbosity ->
+      copyFile inFile outFile
+  }
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+  { hookedPreProcessors = ("hgen", ppHGen) : hookedPreProcessors simpleUserHooks
+  }
