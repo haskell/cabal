420ccd80d99b165ac143726b1cf9a47a3689f7f6
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-04-02 02:14:10 +0200

    After moving GHC 9.4.2->9.4.4 some broken Windows tests work
    Partially revert 78cbeba1a6639ae870d5f3ca03c7dd4603b2cdcb.

--- cabal-testsuite/PackageTests/FFI/ForeignOptsC/cabal.test.hs
+++ cabal-testsuite/PackageTests/FFI/ForeignOptsC/cabal.test.hs
@@ -1,7 +1,4 @@
 import Test.Cabal.Prelude
 main = cabalTest $ do
-  isWin <- isWindows
-  ghc94 <- isGhcVersion "== 9.4.*"
-  expectBrokenIf (isWin && ghc94) 8451 $ do
     cabal "v2-build" ["foreign-opts-c-exe"]
     withPlan $ runPlanExe "foreign-opts-c" "foreign-opts-c-exe" []
--- cabal-testsuite/PackageTests/NewBuild/CmdTest/OptionsFlag/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/CmdTest/OptionsFlag/cabal.test.hs
@@ -1,9 +1,6 @@
 import Test.Cabal.Prelude
 
 main = cabalTest $ do
-  isWin <- isWindows
-  ghc94 <- isGhcVersion "== 9.4.*"
-  expectBrokenIf (isWin && ghc94) 8451 $ do
     cabal "v2-test"
       [ "--test-option=1"
       , "--test-options=\"2 3\""
