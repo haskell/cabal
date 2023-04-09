9adeffd5d753787d96ca4f97d3ac40251c2e2e4e
Mel Zuser <mel@mzero.dev>
2023-04-09 19:45:07 -0400

    Reenable script tests for Windows/ghc-9.4.*

--- cabal-testsuite/PackageTests/NewBuild/CmdRun/ScriptLiterate/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/CmdRun/ScriptLiterate/cabal.test.hs
@@ -1,8 +1,5 @@
 import Test.Cabal.Prelude
 
 main = cabalTest $ do
-  isWin <- isWindows
-  ghc94 <- isGhcVersion "== 9.4.*"
-  expectBrokenIf (isWin && ghc94) 8451 $ do
     res <- cabal' "v2-run" ["script.lhs"]
     assertOutputContains "Hello World" res
