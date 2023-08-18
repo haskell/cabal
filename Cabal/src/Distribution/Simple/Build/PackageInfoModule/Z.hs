9964b51cbbf1efe0d5ca43131156e7f6a6e7b7eb
Liang-Ting Chen <liang.ting.chen.tw@gmail.com>
2023-08-18 15:42:22 +0800

    Add more to the autogened module `PackageInfo_<pkgname>`

--- Cabal/src/Distribution/Simple/Build/PackageInfoModule/Z.hs
+++ Cabal/src/Distribution/Simple/Build/PackageInfoModule/Z.hs
@@ -5,10 +5,17 @@ import Distribution.ZinzaPrelude
 data Z
     = Z {zPackageName :: PackageName,
          zVersionDigits :: String,
-         zSynopsis :: String,
-         zCopyright :: String,
          zLicense :: String,
+         zCopyright :: String,
+         zMaintainer :: String,
+         zAuthor :: String,
+         zStability :: String,
          zHomepage :: String,
+         zPkgUrl :: String,
+         zBugReports :: String,
+         zSynopsis :: String,
+         zDescription :: String,
+         zCategory :: String,
          zSupportsNoRebindableSyntax :: Bool,
          zManglePkgName :: (PackageName -> String),
          zShow :: (String -> String)}
@@ -28,10 +35,17 @@ render z_root = execWriter $ do
   tell "\n"
   tell "  ( name\n"
   tell "  , version\n"
-  tell "  , synopsis\n"
-  tell "  , copyright\n"
   tell "  , license\n"
+  tell "  , copyright\n"
+  tell "  , maintainer\n"
+  tell "  , author\n"
+  tell "  , stability\n"
   tell "  , homepage\n"
+  tell "  , pkgUrl\n"
+  tell "  , bugReports\n"
+  tell "  , synopsis\n"
+  tell "  , description\n"
+  tell "  , category\n"
   tell "  ) where\n"
   tell "\n"
   tell "import Data.Version (Version(..))\n"
@@ -47,9 +61,9 @@ render z_root = execWriter $ do
   tell (zVersionDigits z_root)
   tell " []\n"
   tell "\n"
-  tell "synopsis :: String\n"
-  tell "synopsis = "
-  tell (zSynopsis z_root)
+  tell "license :: String\n"
+  tell "license = "
+  tell (zLicense z_root)
   tell "\n"
   tell "\n"
   tell "copyright :: String\n"
@@ -57,13 +71,47 @@ render z_root = execWriter $ do
   tell (zCopyright z_root)
   tell "\n"
   tell "\n"
-  tell "license :: String\n"
-  tell "license = "
-  tell (zLicense z_root)
+  tell "maintainer :: String\n"
+  tell "maintainer = "
+  tell (zMaintainer z_root)
+  tell "\n"
+  tell "\n"
+  tell "author :: String\n"
+  tell "author = "
+  tell (zAuthor z_root)
+  tell "\n"
+  tell "\n"
+  tell "stability :: String\n"
+  tell "stability = "
+  tell (zStability z_root)
   tell "\n"
   tell "\n"
   tell "homepage :: String\n"
   tell "homepage = "
   tell (zHomepage z_root)
   tell "\n"
+  tell "\n"
+  tell "pkgUrl :: String\n"
+  tell "pkgUrl = "
+  tell (zPkgUrl z_root)
+  tell "\n"
+  tell "\n"
+  tell "bugReports :: String\n"
+  tell "bugReports = "
+  tell (zBugReports z_root)
+  tell "\n"
+  tell "\n"
+  tell "synopsis :: String\n"
+  tell "synopsis = "
+  tell (zSynopsis z_root)
+  tell "\n"
+  tell "\n"
+  tell "description :: String\n"
+  tell "description = "
+  tell (zDescription z_root)
+  tell "\n"
+  tell "\n"
+  tell "category :: String\n"
+  tell "category = "
+  tell (zCategory z_root)
   tell "\n"
