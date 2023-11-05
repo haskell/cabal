8218839456fad9168184857b0ee86e0253767cd7
Sergey Vinokurov <serg.foo@gmail.com>
2023-11-05 18:55:01 +0000

    GHC 9.8 is not sufficient to run AutogenModulesToggling
    The 9.8.1 comes with Cabal-3.10.2.0 which dosen’t have the fixes
    needed to make this test work. The test relies on Custom setup and
    thus depends on Cabal-the-library version.

--- cabal-testsuite/PackageTests/AutogenModulesToggling/cabal.test.hs
+++ cabal-testsuite/PackageTests/AutogenModulesToggling/cabal.test.hs
@@ -2,6 +2,6 @@ import Test.Cabal.Prelude
 
 main :: IO ()
 main = cabalTest . recordMode RecordMarked $ do
-  skipUnlessGhcVersion ">= 9.7"
+  skipUnlessGhcVersion ">= 9.9"
   cabal "v2-run" ["-fgenerate", "autogen-toggle-test"]
   cabal "v2-run" ["-f-generate", "autogen-toggle-test"]
