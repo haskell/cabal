95a3884b7d07a0d0c55a03305ca02beebd98c36d
Hécate Moonlight <Kleidukos@users.noreply.github.com>
2023-11-11 12:58:33 +0100

    fix conflicts

--- cabal-install/src/Distribution/Client/Version.hs
+++ cabal-install/src/Distribution/Client/Version.hs
@@ -11,8 +11,4 @@ import qualified Paths_cabal_install as PackageInfo
 -- |
 -- This value determines the output of `cabal-install --version`.
 cabalInstallVersion :: Version
-<<<<<<< HEAD
-cabalInstallVersion = mkVersion [3,10,2,1]
-=======
 cabalInstallVersion = mkVersion' PackageInfo.version
->>>>>>> 5df009cae (Use Paths_cabal_install for cabal-install version number (#9421))
