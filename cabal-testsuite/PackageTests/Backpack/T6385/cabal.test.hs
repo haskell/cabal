555e791a92082c455a7f82531dac3835300c1a8c
Matthew Pickering <matthewtpickering@gmail.com>
2023-11-16 12:21:15 +0000

    testsuite: Refactor withShorterPathForNewBuildStore
    This makes `withShorterPathForNewBuildStore` fit more nicely into the
    rest of the testing infrastructure.
    
    * Move `withShorterPathForNewBuildStore` to `TestM` monad
    * Move responsibility for passing `--store-dir` to `cabalGArgs` function
    * Move `findDependencyInStore` into `TestM`, and remove requirement to
      pass path to store directory.
    * Introduce `testStoreDir` function which returns the store location
      (and honours `withShorterPathForNewBuildStore`)
    * Migrate tests which use `withShorterPathForNewBuildStore`.

--- cabal-testsuite/PackageTests/Backpack/T6385/cabal.test.hs
+++ cabal-testsuite/PackageTests/Backpack/T6385/cabal.test.hs
@@ -1,7 +1,7 @@
 import Test.Cabal.Prelude
-main = withShorterPathForNewBuildStore $ \storeDir ->
-  cabalTest $ do
+main =
+  cabalTest $ withShorterPathForNewBuildStore $ do
     skipUnlessGhcVersion ">= 8.1"
     skipIfWindows -- TODO: https://github.com/haskell/cabal/issues/6271
     withRepo "repo" $ do
-      cabalG ["--store-dir=" ++ storeDir] "v2-build" ["T6385"]
+      cabal "v2-build" ["T6385"]
--- cabal-testsuite/PackageTests/HaddockArgs/quickjump.test.hs
+++ cabal-testsuite/PackageTests/HaddockArgs/quickjump.test.hs
@@ -3,14 +3,13 @@ import Test.Cabal.Prelude
 import System.Directory
 import System.FilePath
 
-main = withShorterPathForNewBuildStore $ \storeDir -> cabalTest $ withRepo "repo" $ do
-  cabalG ["--store-dir=" ++ storeDir] "v2-build"
+main = cabalTest $ withShorterPathForNewBuildStore $ withRepo "repo" $ do
+  cabal "v2-build"
     [ "example"
     , "--enable-documentation"
     , "--haddock-quickjump"
     ]
-  liftIO $ do
-    libDir <- findDependencyInStore storeDir "indef"
-    assertFileDoesContain (libDir </> "cabal-hash.txt") "haddock-quickjump: True"
-    docIndexJsonExists <- doesFileExist (libDir </> "share" </> "doc" </> "html" </> "doc-index.json")
-    assertBool "doc-index.json doesn't exist, --quickjump is probably not passed to haddock" docIndexJsonExists
+  libDir <- findDependencyInStore "indef"
+  assertFileDoesContain (libDir </> "cabal-hash.txt") "haddock-quickjump: True"
+  docIndexJsonExists <- liftIO $ doesFileExist (libDir </> "share" </> "doc" </> "html" </> "doc-index.json")
+  assertBool "doc-index.json doesn't exist, --quickjump is probably not passed to haddock" docIndexJsonExists
