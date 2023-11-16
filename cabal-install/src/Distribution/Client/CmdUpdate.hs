9f5c8e8f472c7ef22211de4782893d0598622d18
Andrea Bedini <andrea.bedini@tweag.io>
2023-11-17 00:31:21 +0800

    Backport TUF security bugfix to 3.8
    cherry-picks dcfdc9cffd74cade4e8cf3df37c5993413ffd30f

--- cabal-install/src/Distribution/Client/CmdUpdate.hs
+++ cabal-install/src/Distribution/Client/CmdUpdate.hs
@@ -199,10 +199,12 @@ updateRepo verbosity _updateFlags repoCtxt (repo, indexState) = do
       -- NB: always update the timestamp, even if we didn't actually
       -- download anything
       writeIndexTimestamp index indexState
-      ce <- if repoContextIgnoreExpiry repoCtxt
-              then Just `fmap` getCurrentTime
-              else return Nothing
-      updated <- Sec.uncheckClientErrors $ Sec.checkForUpdates repoSecure ce
+      -- typically we get the current time to check expiry against
+      -- but if the flag is set, we don't.
+      now' <- case repoContextIgnoreExpiry repoCtxt of
+                 False -> Just <$> getCurrentTime
+                 True  -> pure Nothing
+      updated <- Sec.uncheckClientErrors $ Sec.checkForUpdates repoSecure now'
       -- this resolves indexState (which could be HEAD) into a timestamp
       new_ts <- currentIndexTimestamp (lessVerbose verbosity) repoCtxt repo
       let rname = remoteRepoName (repoRemote repo)
