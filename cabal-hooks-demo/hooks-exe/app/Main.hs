56d65bef89fc55eb510357bfe25afad3a4f85630
Matthew Pickering <matthewtpickering@gmail.com>
2023-11-23 12:42:20 +0000

    Test project

---
+++ cabal-hooks-demo/hooks-exe/app/Main.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE CPP #-}
+module Main where
+
+import System.Environment
+
+import Prelude hiding (getContents, putStr, hPutStr)
+import Data.ByteString.Lazy (getContents, putStr, hPutStr)
+import System.Environment (getArgs)
+import System.IO (stderr, hPrint)
+import Data.String
+import Data.Binary
+import Lib
+
+-- | Create an executable which accepts the name of a hook as the argument,
+-- then reads arguments to the hook over stdin and writes the results of the hook
+-- to stdout.
+main :: IO ()
+main = do
+  [hookName] <- getArgs
+  case hookName of
+   -- TODO: version of lib
+   "version" -> do
+    putStr $ encode (VERSION_lib01 :: String)
+   "show" -> do
+      s <- getContents
+      let a1 :: A = decode s
+      res <- return $ show a1
+      putStr (encode res)
+   "inc" -> do
+      s <- getContents
+      let a1 :: A = decode s
+      res <- return $ inc a1
+      putStr (encode res)
+   _ -> error "Hook not yet implemented"
---
+++ cabal-hooks-demo/lib01-0.1.0.0/lib-0.1/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for lib01
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
