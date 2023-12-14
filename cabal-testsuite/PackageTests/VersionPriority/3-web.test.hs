5c40dc613a49d854c284b45d14988c92d6747a07
Phil de Joux <philderbeast@gmail.com>
2023-12-14 15:14:47 -0500

    Don't do the web tests with ghc-9.6.3

--- cabal-testsuite/PackageTests/VersionPriority/3-web.test.hs
+++ cabal-testsuite/PackageTests/VersionPriority/3-web.test.hs
@@ -1,4 +1,8 @@
 import Test.Cabal.Prelude
 
 main = cabalTest . withRepo "repo" . withProjectFile "3-web.project" $ do
+    -- To avoid this diff:
+    --   -Build profile: -w ghc-9.6.3 -O1
+    --   +Build profile: -w ghc-<GHCVER> -O1
+    skipIfGhcVersion "== 9.6.3"
     cabal "v2-build" ["--dry-run"]
