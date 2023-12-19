9bb3af33e0c06ed9c177cb02cdfa01b6972ed01c
Andrea Bedini <andrea.bedini@tweag.io>
2023-12-19 14:48:12 +0800

    Use a local secure repositories in the test-suite.
    The test-suite prelude provides the function `withRemoteRepo` to set up
    a secure remote repository using hackage-repo-tool. The created
    repository is then served using python3 internal http server.
    
    Creating the http server and waiting for it to be ready turns out to be
    very fragile. Moreover what we really need in the test-suite it not to
    check any remote connectivity but to test features related to
    hackage-security and index-state. This can be more reliably achieved
    using a local secure repository (e.g. file://).
    
    This change:
    
    - Renames withRemoteRepo to withSecureRepo, to better indicate the
      main difference from withRepo.
    - Configures the repository with a file:// url rather than http://
    - Removes the need for python3 and any retry logic.
    - At variance with withRepo, withSecureRepo should work on Windows.
    - Invokes hackage-repo-tool in such a way index timestamps can be made
      predictable and controllable from the test.

--- cabal-testsuite/PackageTests/NewUpdate/UpdateIndexState/update-index-state.test.hs
+++ cabal-testsuite/PackageTests/NewUpdate/UpdateIndexState/update-index-state.test.hs
@@ -1,12 +1,15 @@
 import Test.Cabal.Prelude
 
-main = cabalTest $ withRemoteRepo "repo" $ do
-  -- The _first_ update call causes a warning about missing mirrors, the warning
-  -- is platform-dependent and it's not part of the test expectations, so we
-  -- check the output manually.
-  res <- recordMode DoNotRecord $
-           cabal' "update" ["repository.localhost,2022-01-28T02:36:41Z"]
-  assertOutputContains "The index-state is set to 2022-01-28T02:36:41Z" res
-  assertOutputDoesNotContain "revert" res
-  cabal "update" ["repository.localhost,2016-09-24T17:47:48Z"]
-  cabal "update" ["repository.localhost,2022-01-28T02:36:41Z"]
+main = cabalTest $ do
+  -- This is the head index-state
+  iso8601ParseM "2023-01-01T00:00:00Z"
+    >>= setModificationTime ("repo" </> "pkg-1.0/pkg.cabal")
+
+  withSecureRepo "repo" $ do
+    cabal "update" []
+
+    -- Check that we mention the previous timestamp
+    res <- cabal' "update" ["test-local-repo,2022-01-01T00:00:00Z"]
+    assertOutputContains "test-local-repo,2023-01-01T00:00:00Z" res
+
+    cabal "update" ["test-local-repo,2022-01-01T00:00:00Z"]
