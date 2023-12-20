bd9e98a56967c4e5c17b79d5d942b5c4a1f4d1b2
Phil de Joux <philderbeast@gmail.com>
2023-12-20 17:05:37 -0500

    Test projects with import then constraints and vice-versa

--- cabal-testsuite/PackageTests/VersionPriority/2-local.test.hs
+++ cabal-testsuite/PackageTests/VersionPriority/2-local.test.hs
@@ -1,10 +1,15 @@
 import Test.Cabal.Prelude
 
-main = cabalTest . withRepo "repo" . withProjectFile "2-local.project" . recordMode RecordMarked $ do
-    let log = recordHeader . pure
-    log "--version-win not supplied, default"
-    cabal "v2-build" ["--dry-run"]
-    log "--version-win=shallowest"
-    cabal "v2-build" ["--dry-run", "--version-win=shallowest"]
-    log "--version-win=latest"
-    cabal "v2-build" ["--dry-run", "--version-win=latest"]
+testVersionWin project =
+    withProjectFile project $ do
+        let log = recordHeader . pure . ((project <> " ") <>)
+        log "--version-win not supplied, default"
+        cabal "v2-build" ["--dry-run"]
+        log "--version-win=shallowest"
+        cabal "v2-build" ["--dry-run", "--version-win=shallowest"]
+        log "--version-win=latest"
+        cabal "v2-build" ["--dry-run", "--version-win=latest"]
+
+main = cabalTest . withRepo "repo" . recordMode RecordMarked $ do
+    testVersionWin "2-local-constraints-import.project"
+    testVersionWin "2-local-import-constraints.project"
