781161eaa8456b9f8f921fd5dfc353fc0d6cb237
Phil de Joux <philderbeast@gmail.com>
2023-12-20 16:54:07 -0500

    Add --version-win option to tests

--- cabal-testsuite/PackageTests/VersionPriority/2-local.test.hs
+++ cabal-testsuite/PackageTests/VersionPriority/2-local.test.hs
@@ -1,4 +1,10 @@
 import Test.Cabal.Prelude
 
-main = cabalTest . withRepo "repo" . withProjectFile "2-local.project" $ do
+main = cabalTest . withRepo "repo" . withProjectFile "2-local.project" . recordMode RecordMarked $ do
+    let log = recordHeader . pure
+    log "--version-win not supplied, default"
     cabal "v2-build" ["--dry-run"]
+    log "--version-win=shallowest"
+    cabal "v2-build" ["--dry-run", "--version-win=shallowest"]
+    log "--version-win=latest"
+    cabal "v2-build" ["--dry-run", "--version-win=latest"]
--- cabal-testsuite/PackageTests/VersionPriority/2-web.test.hs
+++ cabal-testsuite/PackageTests/VersionPriority/2-web.test.hs
@@ -1,8 +1,14 @@
 import Test.Cabal.Prelude
 
-main = cabalTest . withRepo "repo" . withProjectFile "2-web.project" $ do
+main = cabalTest . withRepo "repo" . withProjectFile "2-web.project" . recordMode RecordMarked $ do
     -- To avoid this diff:
     --   -Build profile: -w ghc-9.6.3 -O1
     --   +Build profile: -w ghc-<GHCVER> -O1
     skipIfGhcVersion "== 9.6.3"
+    let log = recordHeader . pure
+    log "--version-win not supplied, default"
     cabal "v2-build" ["--dry-run"]
+    log "--version-win=shallowest"
+    cabal "v2-build" ["--dry-run", "--version-win=shallowest"]
+    log "--version-win=latest"
+    cabal "v2-build" ["--dry-run", "--version-win=latest"]
