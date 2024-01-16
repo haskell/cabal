f80bb15112404dc65d09cbd7853ee85bd26984f0
Phil de Joux <philderbeast@gmail.com>
2024-01-16 06:21:28 -0500

    Add /installed-x.y.z to expectations in unit tests

--- cabal-install/tests/UnitTests/Distribution/Solver/Modular/Solver.hs
+++ cabal-install/tests/UnitTests/Distribution/Solver/Modular/Solver.hs
@@ -930,8 +930,8 @@ tests =
                     , Left $ exInst "A" 2 "A-2.0.0" []
                     , Right $ exAv "B" 1 [ExFix "A" 3]
                     ]
-                  rejecting = "rejecting: A-2.0.0"
-                  skipping = "skipping: A-1.0.0"
+                  rejecting = "rejecting: A-2.0.0/installed-2.0.0"
+                  skipping = "skipping: A-1.0.0/installed-1.0.0"
                in mkTest db "show skipping singleton, installed" ["B"] $
                     solverFailure (\msg -> rejecting `isInfixOf` msg && skipping `isInfixOf` msg)
           , runTest $
@@ -952,7 +952,7 @@ tests =
                     , Left $ exInst "A" 3 "A-3.0.0" []
                     , Right $ exAv "B" 1 [ExFix "A" 4]
                     ]
-                  rejecting = "rejecting: A-3.0.0"
+                  rejecting = "rejecting: A-3.0.0/installed-3.0.0"
                   skipping = "skipping: A-2.0.0/installed-2.0.0, A-1.0.0/installed-1.0.0"
                in mkTest db "show skipping versions list, installed" ["B"] $
                     solverFailure (\msg -> rejecting `isInfixOf` msg && skipping `isInfixOf` msg)
