84c30c263c15546834e39520afb8bd61c4ac20f5
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-03-01 14:39:32 +0000

    Find build-tool installed programs before programs in path
    We must consider the path to the installed build-tool before the path to
    existing versions of the build tool in paths such as `extra-prog-path`
    or in the system path.
    
    This was previously fixed by #8972 but undone by #9527.
    
    This also renames `appendProgramSearchPath` to
    `prependProgramSearchPath` to describe correctly what that function
    does.
    
    Fixes #9756

--- Cabal/src/Distribution/Simple/ConfigureScript.hs
+++ Cabal/src/Distribution/Simple/ConfigureScript.hs
@@ -169,7 +169,7 @@ runConfigureScript verbosity flags lbi = do
       maybeHostFlag = if hp == buildPlatform then [] else ["--host=" ++ show (pretty hp)]
       args' = configureFile' : args ++ ["CC=" ++ ccProgShort] ++ maybeHostFlag
       shProg = simpleProgram "sh"
-  progDb <- appendProgramSearchPath verbosity extraPath emptyProgramDb
+  progDb <- prependProgramSearchPath verbosity extraPath emptyProgramDb
   shConfiguredProg <-
     lookupProgram shProg
       `fmap` configureProgram verbosity shProg progDb
