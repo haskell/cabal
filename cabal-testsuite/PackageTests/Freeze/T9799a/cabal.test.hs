71de5334a8927349458a2264b2942b218cb34e27
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-03-14 12:04:57 +0000

    Add tests for #9799
    Add tests for #9799 about freeze qualifying all packages with 'any'
    constraint scope.

---
+++ cabal-testsuite/PackageTests/Freeze/T9799a/cabal.test.hs
@@ -0,0 +1,9 @@
+import Test.Cabal.Prelude
+main = cabalTest $ do
+  expectBroken 9799 $ withRepo "repo" $ do
+    cabal "v2-freeze" []
+    cwd <- fmap testCurrentDir getTestEnv
+    -- Guarantee that freeze writes scope-qualified constraints, not 'any'
+    -- qualified constraints.
+    assertFileDoesNotContain (cwd </> "cabal.project.freeze") "any.libA"
+    assertFileDoesContain (cwd </> "cabal.project.freeze") "setup.libA == 0.2.0.0"
