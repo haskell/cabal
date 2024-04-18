a8b6b22336d5f4955d2eada576a51c032c201812
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-04-18 18:47:54 +0100

    hooks: Implicitly depend on hooks-exe
    The `hooks-exe` package enables `SetupHooks` values to be converted into
    a `Setup.hs` executable which can be executed independently of Cabal.
    The `Setup.hs` executable wrapping `SetupHooks` is quite important to
    preserve the interface used by other tools when packages migrate to
    `Hooks` from `Custom`.
    
    Even though `hooks-exe` is an internal dependency required by the `Setup.hs`
    wrapper around `SetupHooks`, it is a dependency nonetheless.
    
    Given the internal nature of `hooks-exe`, we don't want to impose on our
    users the obligation to add a dependency on `hooks-exe` in their
    setup-depends field. Instead, we want `hooks-exe` to be implicitly added
    to the setup dependencies. This commit does that exactly.

--- cabal-install/src/Distribution/Client/ProjectPlanning.hs
+++ cabal-install/src/Distribution/Client/ProjectPlanning.hs
@@ -117,6 +117,7 @@ import Distribution.Client.ProjectPlanOutput
 import Distribution.Client.ProjectPlanning.SetupPolicy
   ( NonSetupLibDepSolverPlanPackage (..)
   , mkDefaultSetupDeps
+  , mkHooksSetupImplicitDeps
   , packageSetupScriptSpecVersion
   , packageSetupScriptStyle
   )
@@ -1263,6 +1264,13 @@ planPackages
           . removeLowerBounds solverSettingAllowOlder
           . removeUpperBounds solverSettingAllowNewer
           . addDefaultSetupDependencies
+            extendSetupInfoDeps
+            ( mkHooksSetupImplicitDeps
+                . PD.packageDescription
+                . srcpkgDescription
+            )
+          . addDefaultSetupDependencies
+            setImplicitSetupInfo
             ( mkDefaultSetupDeps comp platform
                 . PD.packageDescription
                 . srcpkgDescription
