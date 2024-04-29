5487b006922a8b221ca023537a5504844a5cc0c7
sheaf <sam.derbyshire@gmail.com>
2024-04-29 15:00:23 +0200

    cabal-install in-library Cabal: test changes
    This commit contains testsuite changes to account for the change in
    how cabal-install calls Cabal.
    
    The most common change is that we emit warnings at a slightly different
    point during compilation.

--- cabal-testsuite/PackageTests/NewBuild/CmdRun/Script/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/CmdRun/Script/cabal.test.hs
@@ -1,6 +1,6 @@
 import Test.Cabal.Prelude
 
-main = cabalTest $ do
+main = cabalTest $ recordMode DoNotRecord $ do
     res <- cabal' "v2-run" ["script.hs"]
     assertOutputContains "Hello World" res
 
