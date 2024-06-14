71578c8108b6b2d219eeb9dde39b8f34b07fd23d
Lennart Augustsson <lennart.augustsson@epicgames.com>
2024-06-14 13:01:06 +0200

    Update more checksums.

--- Cabal-tests/tests/UnitTests/Distribution/Utils/Structured.hs
+++ Cabal-tests/tests/UnitTests/Distribution/Utils/Structured.hs
@@ -33,7 +33,7 @@ md5Check proxy md5Int = structureHash proxy @?= md5FromInteger md5Int
 md5CheckGenericPackageDescription :: Proxy GenericPackageDescription -> Assertion
 md5CheckGenericPackageDescription proxy = md5Check proxy
 #if MIN_VERSION_base(4,19,0)
-    0x4acd7857947385180d814f36dc1a759e
+    0x44f8430d7366cf849e09669627573040
 #else
     0x8ed837568017bde3abb4fcee244b9c9f
 #endif
@@ -41,7 +41,7 @@ md5CheckGenericPackageDescription proxy = md5Check proxy
 md5CheckLocalBuildInfo :: Proxy LocalBuildInfo -> Assertion
 md5CheckLocalBuildInfo proxy = md5Check proxy
 #if MIN_VERSION_base(4,19,0)
-    0x552eca9ce2e4a34e74deff571f279fc4
+    0xdff58fe5e7f9568c67cd982eaba7edc2
 #else
     0x4e50a4a95779b862edde3d6696797251
 #endif
