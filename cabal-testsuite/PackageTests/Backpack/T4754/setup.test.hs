bcf16748332b1e45caf2a5d48eb36bf80e6f89c2
Javier Sagredo <jasataco@gmail.com>
2024-07-24 17:06:31 +0200

    Make `skip` and `broken` messages uniform

--- cabal-testsuite/PackageTests/Backpack/T4754/setup.test.hs
+++ cabal-testsuite/PackageTests/Backpack/T4754/setup.test.hs
@@ -1,6 +1,6 @@
 import Test.Cabal.Prelude
 main = setupAndCabalTest $ do
     skipUnlessGhcVersion ">= 8.1"
-    skipUnless "no profiling libs" =<< hasProfiledLibraries
+    skipIfNoProfiledLibraries
     setup "configure" ["--enable-profiling"]
     setup "build" []
