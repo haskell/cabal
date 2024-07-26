e14eacd2c6899ede64928157a750f49c4ba3c794
Javier Sagredo <jasataco@gmail.com>
2024-07-26 17:12:19 +0200

    Skip non-terminating tests in Windows CI

--- cabal-testsuite/PackageTests/NewUpdate/UpdateIndexState/update-index-state.test.hs
+++ cabal-testsuite/PackageTests/NewUpdate/UpdateIndexState/update-index-state.test.hs
@@ -1,13 +1,13 @@
 import Test.Cabal.Prelude
 
-main = cabalTest $ flakyIfCI 9530 $ withRemoteRepo "repo" $ do
+main = skipIfCIAndWindows 10230 >> cabalTest (flakyIfCI 9530 $ withRemoteRepo "repo" $ do
 
-    -- The _first_ update call causes a warning about missing mirrors, the warning
-    -- is platform-dependent and it's not part of the test expectations, so we
-    -- check the output manually.
-    res <- recordMode DoNotRecord $
-            cabal' "update" ["repository.localhost,2022-01-28T02:36:41Z"]
-    assertOutputContains "The index-state is set to 2022-01-28T02:36:41Z" res
-    assertOutputDoesNotContain "revert" res
-    cabal "update" ["repository.localhost,2016-09-24T17:47:48Z"]
-    cabal "update" ["repository.localhost,2022-01-28T02:36:41Z"]
+  -- The _first_ update call causes a warning about missing mirrors, the warning
+  -- is platform-dependent and it's not part of the test expectations, so we
+  -- check the output manually.
+  res <- recordMode DoNotRecord $
+          cabal' "update" ["repository.localhost,2022-01-28T02:36:41Z"]
+  assertOutputContains "The index-state is set to 2022-01-28T02:36:41Z" res
+  assertOutputDoesNotContain "revert" res
+  cabal "update" ["repository.localhost,2016-09-24T17:47:48Z"]
+  cabal "update" ["repository.localhost,2022-01-28T02:36:41Z"])
