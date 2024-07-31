6e58a778374fb5dd230571e24688f128998e5408
Javier Sagredo <jasataco@gmail.com>
2024-07-31 23:09:43 +0200

    Fix GHCJS BuildRunner test on Windows

--- cabal-testsuite/PackageTests/GHCJS/BuildRunner/cabal.test.hs
+++ cabal-testsuite/PackageTests/GHCJS/BuildRunner/cabal.test.hs
@@ -1,7 +1,7 @@
 import Test.Cabal.Prelude
 
 main = do
-  cabalTest . expectBrokenIfWindows 10179 . recordMode DoNotRecord $ do
+  cabalTest . recordMode DoNotRecord $ do
     cwd <- fmap testCurrentDir getTestEnv
     testInvokedWithBuildRunner cwd "test" []
     testInvokedWithBuildRunner cwd "run" ["ghcjs-exe"]
@@ -14,6 +14,8 @@ testInvokedWithBuildRunner cwd cabalCmd extraArgs = do
         [ "--ghcjs"
         , "--with-compiler", cwd </> fakeGhcjsPath
         ]
+        -- On windows point cabal to the right cc
+        ++ if isWindows then ["--with-gcc", "scripts/cc.bat"] else []
     assertOutputContains magicString output
   where
-    fakeGhcjsPath = "scripts/fake-ghcjs.sh"
+    fakeGhcjsPath = if isWindows then "scripts/fake-ghcjs.exe" else "scripts/fake-ghcjs.sh"
