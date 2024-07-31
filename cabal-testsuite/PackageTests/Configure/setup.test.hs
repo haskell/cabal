9d57a07129d949519aceb1ef43e96e134f2e4dbb
Javier Sagredo <jasataco@gmail.com>
2024-07-31 22:16:56 +0200

    Enable autoreconf tests on Windows

--- cabal-testsuite/PackageTests/Configure/setup.test.hs
+++ cabal-testsuite/PackageTests/Configure/setup.test.hs
@@ -2,9 +2,25 @@ import Test.Cabal.Prelude
 import Control.Monad.IO.Class
 import Data.Maybe
 import System.Directory
+import System.Environment
+
 -- Test for 'build-type: Configure' example from the setup manual.
-main = setupTest $ do
-    hasAutoreconf <- liftIO $ fmap isJust $ findExecutable "autoreconf"
-    skipUnless "no autoreconf" hasAutoreconf
-    _ <- shell "autoreconf" ["-i"]
-    setup_build []
+main = setupTest $
+    if isWindows
+    then do
+        (mCI, mSh) <- liftIO $ (,) <$> lookupEnv "CI" <*> lookupEnv "SHELL"
+        case (mCI, mSh) of
+            (Nothing, Nothing) -> skip "Missing $SHELL"
+            (Nothing, Just sh) -> do
+                env <- getTestEnv
+                void $ shell sh [ "-l", "-c", "cd $(cygpath -m '" <> testTmpDir env <> "') && autoreconf -i"]
+                setup_build []
+            (Just{}, _) -> do
+                env <- getTestEnv
+                void $ shell "C:\\msys64\\usr\\bin\\bash.exe" [ "-l", "-c", "cd $(cygpath -m '" <> testTmpDir env <> "') && autoreconf -i"]
+                setup_build []
+    else do
+        hasAutoreconf <- liftIO $ fmap isJust $ findExecutable "autoreconf"
+        skipUnless "no autoreconf" hasAutoreconf
+        _ <- shell "autoreconf" ["-i"]
+        setup_build []
