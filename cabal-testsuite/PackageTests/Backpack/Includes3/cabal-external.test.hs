4aade2d8a17704cfcb2fa6cf9fb59478c17a0653
Javier Sagredo <jasataco@gmail.com>
2024-08-28 13:48:53 +0200

    Re-enable Windows CI

--- cabal-testsuite/PackageTests/Backpack/Includes3/cabal-external.test.hs
+++ cabal-testsuite/PackageTests/Backpack/Includes3/cabal-external.test.hs
@@ -3,7 +3,8 @@ import Test.Cabal.Prelude
 main = cabalTest $ do
     ghcVer <- isGhcVersion ">= 9.10"
     skipUnlessGhcVersion ">= 8.1"
-    expectBrokenIf (isWindows && ghcVer) 10191 $ withProjectFile "cabal.external.project" $ do
+    skipIf "Windows + 9.10.1 (#10191)" (isWindows && ghcVer)
+    withProjectFile "cabal.external.project" $ do
         cabal "v2-build" ["exe"]
         withPlan $ do
             r <- runPlanExe' "exe" "exe" []
