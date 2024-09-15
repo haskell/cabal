66d401b5debc766ffc447e6b268284abb7141d48
ffaf1 <fa-ml@ariis.it>
2024-09-15 19:43:01 +0200

    Add OrPatterns extensions (#10339)
    * Add OrPatterns extension
    
    * Add changelog
    
    * Add missing full stop
    
    * Add missing NoMultilineStrings in vim syntax file
    
    * Make test happy
    
    * Remove changelog entry
    
    We don't need it, changes have been manually
    incorporated in 3.14 release notes (#10338)
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- Cabal-syntax/src/Language/Haskell/Extension.hs
+++ Cabal-syntax/src/Language/Haskell/Extension.hs
@@ -551,8 +551,11 @@ data KnownExtension
   | -- | Allow the use of built-in syntax for list, tuple and sum type constructors
     -- rather than being exclusive to data constructors.
     ListTuplePuns
-  | -- | Support multiline strings
+  | -- | Support multiline strings.
     MultilineStrings
+  | -- | Allow use of or-pattern syntax, condensing multiple patterns
+    -- into a single one.
+    OrPatterns
   deriving (Generic, Show, Read, Eq, Ord, Enum, Bounded, Typeable, Data)
 
 instance Binary KnownExtension
