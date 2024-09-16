55c61aaf50f7bd8289601bd94a0a5f8c36bf1f1a
ffaf1 <fa-ml@ariis.it>
2024-09-16 18:22:13 +0200

    Update SPDX license list to version 3.25 (#10341)
    * Handle 3D Slicer licence
    
    * Update SPDX license list to version 3.25
    
    * Remove changelog entry
    
    We don't need it, changes have been manually
    incorporated in 3.14 release notes (#10338)
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- cabal-dev-scripts/src/GenSPDXExc.hs
+++ cabal-dev-scripts/src/GenSPDXExc.hs
@@ -35,6 +35,7 @@ main = generate =<< O.execParser opts where
         <*> licenses "3.10"
         <*> licenses "3.16"
         <*> licenses "3.23"
+        <*> licenses "3.25"
 
     template = O.strArgument $ mconcat
         [ O.metavar "SPDX.LicenseExceptionId.template.hs"
