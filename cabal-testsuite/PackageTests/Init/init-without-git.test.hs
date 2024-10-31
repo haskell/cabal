032f5b13ad0aa176bd57d7e59501077b02cf8c61
noiioiu <noiioiu@cocaine.ninja>
2024-10-30 23:43:33 -0500

    Remove withSourceCopyDir from test

--- cabal-testsuite/PackageTests/Init/init-without-git.test.hs
+++ cabal-testsuite/PackageTests/Init/init-without-git.test.hs
@@ -13,15 +13,14 @@ main = do
         ghc_path <- programPathM ghcProgram
         cabal_path <- programPathM cabalProgram
         withSymlink ghc_path (bin </> "ghc") . withSymlink cabal_path (bin </> "cabal") .
-          withEnv [("PATH", Just bin)] .
-            withSourceCopyDir "app" $ do
-              cwd <- fmap testSourceCopyDir getTestEnv
+          withEnv [("PATH", Just bin)] $ do
+            cwd <- fmap testSourceCopyDir getTestEnv
 
-              buildOut <- withDirectory cwd $ do
-                cabalWithStdin "init" ["-i"]
-                  "2\n\n5\n\n\n2\n\n\n\n\n\n\n\n\n\n"
-                setup "configure" []
-                setup' "build" ["app"]
+            buildOut <- withDirectory cwd $ do
+              cabalWithStdin "init" ["-i"]
+                "2\n\n5\n\n\n2\n\n\n\n\n\n\n\n\n\n"
+              setup "configure" []
+              setup' "build" ["app"]
 
-              assertFileDoesContain (cwd </> "app.cabal")   "3.0"
-              assertOutputContains "Linking" buildOut
+            assertFileDoesContain (cwd </> "app.cabal")   "3.0"
+            assertOutputContains "Linking" buildOut
