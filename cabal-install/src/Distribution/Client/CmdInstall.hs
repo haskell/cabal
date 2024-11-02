ee3c31398c776ce210fd6bbb5000a7532ea43c31
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-11-02 19:41:31 -0400

    fix `cabal install --program-suffix/prefix` (fix #10290 and #10476) (#10483)
    * fix cabal install --program-suffix/prefix (fix #10290 and #10476)
    
    When checking for existing installations, cabal would not account for
    an affix (suffix or prefix). So, if you had a `hello` binary installed, installing a
    second one with a non-empty affix (a perfectly legal operation) would
    fail.
    
    The reason seemed to be a typo in
    09c04e9aca5de2ca391eb859a5b295fdd617f5c6, which passed the arguments to
    the Symlink structure in a wrong order.
    
    When failing to install a binary because of an existing one, cabal would
    report suffix-less existing target even if a suffix was set.
    
    * Add regression tests for overwrite policies and porgram-affixes
    
    Add regression tests for the `program-prefix` and `program-suffix` flags
    combined with the overwrite-policy.
    In short, the overwrite-policy needs to take potential program affixes
    into account when deciding whether it will need to overwrite a program
    path during installation.
    
    ---------
    
    Co-authored-by: Fendor <fendor@posteo.de>

--- cabal-install/src/Distribution/Client/CmdInstall.hs
+++ cabal-install/src/Distribution/Client/CmdInstall.hs
@@ -1126,8 +1126,8 @@ symlink
       overwritePolicy
       installDir
       (mkSourceBinDir unit)
-      (mkExeName exe)
       (mkFinalExeName exe)
+      (mkExeName exe)
 
 -- |
 -- -- * When 'InstallCheckOnly', warn if install would fail overwrite policy
@@ -1172,7 +1172,7 @@ installCheckUnitExes
       errorMessage installdir exe = case overwritePolicy of
         NeverOverwrite ->
           "Path '"
-            <> (installdir </> prettyShow exe)
+            <> (installdir </> mkFinalExeName exe)
             <> "' already exists. "
             <> "Use --overwrite-policy=always to overwrite."
         -- This shouldn't even be possible, but we keep it in case symlinking or
