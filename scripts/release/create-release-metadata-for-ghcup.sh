255d712911935c0bf081eb7c73aea70aa7774c91
Hécate Moonlight <hecate+github@glitchbra.in>
2024-12-17 00:54:26 +0100

    dlSubdir does not work in practice

--- scripts/release/create-release-metadata-for-ghcup.sh
+++ scripts/release/create-release-metadata-for-ghcup.sh
@@ -45,31 +45,25 @@ cat <<EOF > /dev/stdout
           Linux_Debian:
             '( >= 9 && < 10)': &cabal-${RELEASE//./}-64-debian
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-x86_64-linux-deb9.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-x86_64-linux-deb9.tar.xz" | awk '{ print $1 }')
             '( == 10 && < 11)':
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-x86_64-linux-deb10.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-x86_64-linux-deb10.tar.xz" | awk '{ print $1 }')
             '( >= 11)':
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-x86_64-linux-deb11.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-x86_64-linux-deb11.tar.xz" | awk '{ print $1 }')
             unknown_versioning: &cabal-${RELEASE//./}-64-debian
           Linux_Fedora:
             '>= 33':
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-x86_64-linux-fedora33.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-x86_64-linux-fedora33.tar.xz" | awk '{ print $1 }')
             unknown_versioning: &cabal-${RELEASE//./}-64-centos7
           Linux_Ubuntu:
             '< 20': &cabal-${RELEASE//./}-64-ubuntu18
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-x86_64-linux-ubuntu18_04.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-x86_64-linux-ubuntu18_04.tar.xz" | awk '{ print $1 }')
             '>= 20': &cabal-${RELEASE//./}-64-ubuntu20
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-x86_64-linux-ubuntu20_04.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-x86_64-linux-ubuntu20_04.tar.xz" | awk '{ print $1 }')
             unknown_versioning: *cabal-${RELEASE//./}-64-ubuntu18
           Linux_Mint:
@@ -79,46 +73,38 @@ cat <<EOF > /dev/stdout
           Darwin:
             unknown_versioning:
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-x86_64-darwin.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-x86_64-darwin.tar.xz" | awk '{ print $1 }')
           Windows:
             unknown_versioning:
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-x86_64-windows.zip
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-x86_64-windows.zip" | awk '{ print $1 }')
           FreeBSD:
             unknown_versioning:
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-x86_64-freebsd.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-x86_64-freebsd.tar.xz" | awk '{ print $1 }')
         A_32:
           Linux_UnknownLinux:
             unknown_versioning: &cabal-${RELEASE//./}-32
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-i386-linux-alpine3_12.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-i386-linux-alpine3_12.tar.xz" | awk '{ print $1 }')
           Linux_Alpine:
             unknown_versioning: *cabal-${RELEASE//./}-32
           Linux_Debian:
             '( >= 9 )':
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-i386-linux-deb9.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-i386-linux-deb9.tar.xz" | awk '{ print $1 }')
             unknown_versioning: *cabal-${RELEASE//./}-32
         A_ARM64:
           Darwin:
             unknown_versioning:
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-aarch64-darwin.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-aarch64-darwin.tar.xz" | awk '{ print $1 }')
           Linux_Debian:
             '( >= 10 && < 11)': &cabal-${RELEASE//./}-arm64
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-aarch64-linux-deb10.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-aarch64-linux-deb10.tar.xz" | awk '{ print $1 }')
             '( >= 11)':
               dlUri: https://downloads.haskell.org/~cabal/cabal-install-$RELEASE/cabal-install-$RELEASE-aarch64-linux-deb11.tar.xz
-              dlSubdir: cabal-install-$RELEASE
               dlHash: $(sha256sum "cabal-install-$RELEASE-aarch64-linux-deb11.tar.xz" | awk '{ print $1 }')
             unknown_versioning: *cabal-${RELEASE//./}-arm64
           Linux_UnknownLinux:
