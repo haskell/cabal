da9a4443aae30049a4c72818466d9701b2527ba0
Phil de Joux <philderbeast@gmail.com>
2024-12-16 10:57:10 -0500

    Add if, elif and else test projects

--- cabal-testsuite/PackageTests/ProjectImport/ParseErrorProvenance/cabal.test.hs
+++ cabal-testsuite/PackageTests/ProjectImport/ParseErrorProvenance/cabal.test.hs
@@ -3,8 +3,14 @@ import Test.Cabal.Prelude
 main = cabalTest . recordMode RecordMarked $ do
   let log = recordHeader . pure
 
-  out <- fails $ cabal' "v2-build" [ "all", "--dry-run" ]
+  outIf <- fails $ cabal' "v2-build" [ "all", "--dry-run", "--project-file=if.project" ]
+  assertOutputContains "Error parsing project file dir-if/if.config:3" outIf
 
-  assertOutputContains "Warnings found while parsing the project file, cabal.project:" out
+  outElif <- fails $ cabal' "v2-build" [ "all", "--dry-run", "--project-file=elif.project" ]
+  assertOutputContains "Error parsing project file dir-elif/elif.config:4" outElif
+
+  outElse <- fails $ cabal' "v2-build" [ "all", "--dry-run", "--project-file=else.project" ]
+  assertOutputContains "Warnings found while parsing the project file, else.project:" outElse
+  assertOutputContains "- dir-else/else.config: Unrecognized section '_' on line 3" outElse
 
   return ()
