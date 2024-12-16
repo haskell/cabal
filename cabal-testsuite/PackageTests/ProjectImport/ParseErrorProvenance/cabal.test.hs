926f0892da270a3ad09d19d316061ef586f6c3bf
Phil de Joux <philderbeast@gmail.com>
2024-12-16 11:59:49 -0500

    Rerun expected output with provenance
    .
    
    .

--- cabal-testsuite/PackageTests/ProjectImport/ParseErrorProvenance/cabal.test.hs
+++ cabal-testsuite/PackageTests/ProjectImport/ParseErrorProvenance/cabal.test.hs
@@ -3,14 +3,21 @@ import Test.Cabal.Prelude
 main = cabalTest . recordMode RecordMarked $ do
   let log = recordHeader . pure
 
+  outDefault <- fails $ cabal' "v2-build" [ "all", "--dry-run", "--project-file=cabal.project" ]
+  assertOutputContains "Error parsing project file cabal.project:3" outDefault
+  assertOutputDoesNotContain "imported by:" outDefault
+
   outIf <- fails $ cabal' "v2-build" [ "all", "--dry-run", "--project-file=if.project" ]
   assertOutputContains "Error parsing project file dir-if/if.config:3" outIf
+  assertOutputContains "imported by:" outIf
 
   outElif <- fails $ cabal' "v2-build" [ "all", "--dry-run", "--project-file=elif.project" ]
   assertOutputContains "Error parsing project file dir-elif/elif.config:4" outElif
+  assertOutputContains "imported by:" outElif
 
   outElse <- fails $ cabal' "v2-build" [ "all", "--dry-run", "--project-file=else.project" ]
   assertOutputContains "Warnings found while parsing the project file, else.project:" outElse
   assertOutputContains "- dir-else/else.config: Unrecognized section '_' on line 3" outElse
+  assertOutputContains "When using configuration from:" outElse
 
   return ()
