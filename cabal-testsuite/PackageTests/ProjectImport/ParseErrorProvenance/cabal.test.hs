0a710b72a4eaa61db14a3f4e9da46b0b98de3c79
Phil de Joux <philderbeast@gmail.com>
2024-12-18 12:26:07 -0500

    Add test of string expectation start and end marking

--- cabal-testsuite/PackageTests/ProjectImport/ParseErrorProvenance/cabal.test.hs
+++ cabal-testsuite/PackageTests/ProjectImport/ParseErrorProvenance/cabal.test.hs
@@ -3,13 +3,33 @@ import Test.Cabal.Prelude
 main = cabalTest . recordMode RecordMarked $ do
   let log = recordHeader . pure
 
+  let expectMulti =
+        "When using configuration from:\n\
+        \  - else.project\n\
+        \  - dir-else/else.config\n\
+        \The following errors occurred:\n\
+        \  - The package location 'no-pkg-here' does not exist."
+
   outElse <- fails $ cabal' "v2-build" [ "all", "--dry-run", "--project-file=else.project" ]
-  assertOutputContainsOn unConcatOutput unConcatOutput concatOutput
-    (concatOutput "When using configuration from:\n\
-    \  - else.project\n\
-    \  - dir-else/else.config\n\
-    \The following errors occurred:\n\
-    \  - The package location 'no-pkg-here' does not exist.")
-    outElse
+
+  let expectSingle = filter (/= '\n') expectMulti
+
+  log "Multiline string marking:"
+  mapM_ log (lines . unConcatOutput $ concatOutput expectMulti)
+
+  log "Pseudo multiline string marking:"
+  mapM_ log (lines . unConcatOutput $ concatOutput expectSingle)
+
+  assertOutputContainsMultiline expectMulti outElse
+  assertOutputDoesNotContainMultiline expectSingle outElse
+
+  assertOutputDoesNotContain expectMulti outElse
+  assertOutputDoesNotContain expectSingle outElse
+
+  assertOutputContainsOn unConcatOutput concatOutput unConcatOutput concatOutput expectMulti outElse
+  assertOutputDoesNotContainOn id id unConcatOutput concatOutput expectSingle outElse
+
+  assertOutputContainsOn id id id id expectMulti outElse
+  assertOutputDoesNotContainOn id id id id expectSingle outElse
 
   return ()
