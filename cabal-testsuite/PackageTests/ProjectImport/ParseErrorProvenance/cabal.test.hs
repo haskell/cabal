661dd1b2a2bd46d1b086a5c76d74f5129b86bded
Phil de Joux <philderbeast@gmail.com>
2024-12-18 07:04:22 -0800

    Use \n in multiline string expectation

--- cabal-testsuite/PackageTests/ProjectImport/ParseErrorProvenance/cabal.test.hs
+++ cabal-testsuite/PackageTests/ProjectImport/ParseErrorProvenance/cabal.test.hs
@@ -4,11 +4,11 @@ main = cabalTest . recordMode RecordMarked $ do
   let log = recordHeader . pure
 
   outElse <- fails $ cabal' "v2-build" [ "all", "--dry-run", "--project-file=else.project" ]
-  assertOutputContains
-    (concatOutput "When using configuration from: \
-    \  - else.project \
-    \  - dir-else/else.config \
-    \The following errors occurred: \
+  assertOutputContainsOn unConcatOutput unConcatOutput concatOutput
+    (concatOutput "When using configuration from:\n\
+    \  - else.project\n\
+    \  - dir-else/else.config\n\
+    \The following errors occurred:\n\
     \  - The package location 'no-pkg-here' does not exist.")
     outElse
 
