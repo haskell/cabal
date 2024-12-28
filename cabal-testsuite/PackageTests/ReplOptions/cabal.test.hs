bc936c1517713c3d257aca8290e2b171f741c843
Phil de Joux <philderbeast@gmail.com>
2024-12-28 07:15:13 -0500

    Fix two issues when starting the repl
    Issue 1: In 3.14 there was a poor error when starting a repl in a
    project context without any targets. In 3.16, this error regressed, so
    cabal just exited cleanly.
    Issue 2: The repl was broken when started from a global context.
    
    Issue 1 is fixed by checking to see if there are any user targets, and
    issuing a proper error if there are none.
    
    Issue 2 is fixed by specifying the correct fake target which is
    constructed when starting the repl in the global context.
    
    Both are reported in #11107 and fixed in this patch.
    
    Co-authored-by: Matthew Pickering <matthewtpickering@gmail.com>
    (cherry picked from commit 5e269325886758ac5cfc874d12c9b488d91737d3)

--- cabal-testsuite/PackageTests/ReplOptions/cabal.test.hs
+++ cabal-testsuite/PackageTests/ReplOptions/cabal.test.hs
@@ -1,29 +1,46 @@
 import Test.Cabal.Prelude
 
+singleOpts = ["--repl-options=-fwrite-interface"]
+multiOpts = "--repl-options=-fdefer-typed-holes" : singleOpts
+altProject = ("--project-file=alt.project" :)
+
 main = do
   cabalTest' "single-repl-options" $ do
     cabal' "clean" []
-    res <- cabalWithStdin "v2-repl" ["--repl-options=-fwrite-interface"] ":set"
+    res <- cabalWithStdin "v2-repl" singleOpts ":set"
     assertOutputContains "Ok, two modules loaded." res
-    assertOutputContains "  -fwrite-interface" res
+
+  cabalTest' "alt-single-repl-options" $ do
+    cabal' "clean" []
+    -- We can't 'cabal repl' without a target when the project has a single package.
+    void $ fails $ cabalWithStdin "v2-repl" (altProject singleOpts) ":set"
+
   cabalTest' "multiple-repl-options" $ do
     cabal' "clean" []
-    res <- cabalWithStdin "v2-repl" ["--repl-options=-fwrite-interface", "--repl-options=-fdefer-typed-holes"] ":set"
+    res <- cabalWithStdin "v2-repl" multiOpts ":set"
     assertOutputContains "Ok, two modules loaded." res
     assertOutputContains "  -fwrite-interface" res
     assertOutputContains "  -fdefer-typed-holes" res
+
+  cabalTest' "alt-multiple-repl-options" $ do
+    cabal' "clean" []
+    -- We can't 'cabal repl' without a target when the project has a single package.
+    void $ fails $ cabalWithStdin "v2-repl" (altProject multiOpts) ":set"
+
   cabalTest' "single-repl-options-multiple-flags" $ do
     cabal' "clean" []
     res <- cabalWithStdin "v2-repl" ["--repl-options=-fdefer-typed-holes -fwrite-interface"] ":set"
     assertOutputContains "Ok, two modules loaded." res
     assertOutputContains "  -fwrite-interface" res
     assertOutputContains "  -fdefer-typed-holes" res
+
   cabalTest' "single-repl-options-multiple-flags-negative" $ do
     cabal' "clean" []
     res <- fails $ cabalWithStdin "v2-repl" ["--repl-options=-fwrite-interface -fdiagnostics-show-baret"] ":set"
     assertOutputDoesNotContain "Ok, two modules loaded." res
     assertOutputContains "unrecognised flag: -fdiagnostics-show-baret" res
     assertOutputContains "did you mean one of:" res
+
   cabalTest' "multiple-repl-options-multiple-flags" $ do
     cabal' "clean" []
     res <- cabalWithStdin "v2-repl" [
