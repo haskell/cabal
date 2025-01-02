3acacfb978c26aa721f212234b9f28564223d904
Phil de Joux <philderbeast@gmail.com>
2025-01-02 05:20:50 -0500

    Add tests of all:exes and all:tests

--- cabal-testsuite/PackageTests/Target/cabal.test.hs
+++ cabal-testsuite/PackageTests/Target/cabal.test.hs
@@ -8,3 +8,11 @@ main = do
   cabalTest' "explicit-all" $ do
     cabal "clean" []
     cabal "v2-target" ["all"]
+
+  cabalTest' "all-exes" $ do
+    cabal "clean" []
+    cabal "v2-target" ["all:exes"]
+
+  cabalTest' "all-tests" $ do
+    cabal "clean" []
+    cabal "v2-target" ["all:tests"]
