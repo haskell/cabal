895d4f01e2b19e20000434729a900a573b00c500
Phil de Joux <philderbeast@gmail.com>
2025-01-02 05:27:29 -0500

    Add tests with --enable-tests and --enable-benchmarks

--- cabal-testsuite/PackageTests/Target/cabal.test.hs
+++ cabal-testsuite/PackageTests/Target/cabal.test.hs
@@ -9,6 +9,18 @@ main = do
     cabal "clean" []
     cabal "v2-target" ["all"]
 
+  cabalTest' "all-enable-tests" $ do
+    cabal "clean" []
+    cabal "v2-target" ["all", "--enable-tests"]
+
+  cabalTest' "all-enable-benches" $ do
+    cabal "clean" []
+    cabal "v2-target" ["all", "--enable-benchmarks"]
+
+  cabalTest' "everything" $ do
+    cabal "clean" []
+    cabal "v2-target" ["all", "--enable-tests", "--enable-benchmarks"]
+
   cabalTest' "all-exes" $ do
     cabal "clean" []
     cabal "v2-target" ["all:exes"]
