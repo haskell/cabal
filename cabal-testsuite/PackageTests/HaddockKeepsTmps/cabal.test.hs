de6403902ef9b72dfb1dee529f2c89b8b7d026a5
Matthew Pickering <matthewtpickering@gmail.com>
2025-02-25 16:12:26 +0000

    testsuite: Set TMPDIR to a specific folder rather than the test root
    If things start getting created in the TMPDIR, then it is better to not
    place them directly in the project root next to the source and test
    files. This also makes it easier to write tests which are sensitive to
    what is created in the CWD.
    
    (cherry picked from commit c64a32d7cbf75b54806b3e4378fa24a2ef0e0978)
    
    # Conflicts:
    #	cabal-testsuite/PackageTests/HaddockKeepTmpsCustom/cabal.test.hs
    #	cabal-testsuite/PackageTests/HaddockKeepsTmps/cabal.test.hs
    #	cabal-testsuite/src/Test/Cabal/Monad.hs

--- cabal-testsuite/PackageTests/HaddockKeepsTmps/cabal.test.hs
+++ cabal-testsuite/PackageTests/HaddockKeepsTmps/cabal.test.hs
@@ -13,9 +13,18 @@ main = cabalTest $ recordMode DoNotRecord $ withProjectFile "cabal.project" $ do
 
     cwd <- fmap testCurrentDir getTestEnv
 
+<<<<<<< HEAD
     -- Windows has multiple response files, and only the last one (alphabetically) is the important one.
     (safeLast . sort . globMatches <$> liftIO (runDirFileGlob silent Nothing cwd (GlobDirRecursive [WildCard, Literal "txt"]))) >>= \case
       Nothing -> error "Expecting a response file to exist"
       Just m -> do
         -- Assert the matched response file is not empty, and indeed a haddock rsp
         assertFileDoesContain (cwd </> m) "--package-name"
+=======
+    -- Check that there is a response file.
+    responseFiles <- assertGlobMatchesTestDir testSystemTmpDir glob
+
+    -- Check that the matched response file is not empty, and is indeed a Haddock
+    -- response file.
+    assertAnyFileContains responseFiles "--package-name"
+>>>>>>> c64a32d7c (testsuite: Set TMPDIR to a specific folder rather than the test root)
