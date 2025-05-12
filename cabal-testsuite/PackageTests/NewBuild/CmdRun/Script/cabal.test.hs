45466b4970fb028927f835527f48402cf22b558f
sheaf <sam.derbyshire@gmail.com>
2025-05-12 13:35:03 +0200

    WIP: try to refactor Cabal configure further to avoid duplicating code in cabal-install

--- cabal-testsuite/PackageTests/NewBuild/CmdRun/Script/cabal.test.hs
+++ cabal-testsuite/PackageTests/NewBuild/CmdRun/Script/cabal.test.hs
@@ -1,15 +1,10 @@
 import Test.Cabal.Prelude
 
-<<<<<<< HEAD
 main = cabalTest $ do
     -- NB: Uses cabal_raw' here to direct simulate what a user would write (#10772)
     res <- defaultRecordMode RecordMarked $ do
       recordHeader ["cabal", "v2-run"]
       cabal_raw' ["v2-run", "script.hs", marked_verbose] Nothing
-=======
-main = cabalTest $ recordMode DoNotRecord $ do
-    res <- cabal' "v2-run" ["script.hs"]
->>>>>>> 5487b0069 (cabal-install in-library Cabal: test changes)
     assertOutputContains "Hello World" res
 
     env      <- getTestEnv
