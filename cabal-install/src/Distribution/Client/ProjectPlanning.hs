50dcb1887d918acc48fc493428bc0122127217f8
sheaf <sam.derbyshire@gmail.com>
2025-05-21 13:05:27 +0200

    cabal-install: dynExe+profExe requires prof+dyn
    This commit updates the logic in cabal-install's 'elaborateInstallPlan'
    function to ensure that we don't try to build a profiled dynamic
    executable if the compiler doesn't support the profiling dynamic way.
    
    This brings the logic in cabal-install in sync with the Cabal
    'configureProfiling' function, which sets 'withDynExe' to false if the
    user wants a profiled executable but prof+dyn is not supported by the
    compiler.

--- cabal-install/src/Distribution/Client/ProjectPlanning.hs
+++ cabal-install/src/Distribution/Client/ProjectPlanning.hs
@@ -2295,9 +2295,13 @@ elaborateInstallPlan
                 , withSharedLib = canBuildSharedLibs && pkgid `Set.member` pkgsUseSharedLibrary
                 , withStaticLib = perPkgOptionFlag pkgid False packageConfigStaticLib
                 , withDynExe = perPkgOptionFlag pkgid False packageConfigDynExe
+                                 && (okProfDyn || not profExe)
+                                   -- We can't produce a dynamic executable if the user
+                                   -- wants to enable executable profiling but the
+                                   -- compiler doesn't support prof+dyn.
                 , withFullyStaticExe = perPkgOptionFlag pkgid False packageConfigFullyStaticExe
                 , withGHCiLib = perPkgOptionFlag pkgid False packageConfigGHCiLib -- TODO: [required feature] needs to default to enabled on windows still
-                , withProfExe = perPkgOptionFlag pkgid False packageConfigProf
+                , withProfExe = profExe
                 , withProfLib = canBuildProfilingLibs && pkgid `Set.member` pkgsUseProfilingLibrary
                 , withProfLibShared = canBuildProfilingSharedLibs && pkgid `Set.member` pkgsUseProfilingLibraryShared
                 , exeCoverage = perPkgOptionFlag pkgid False packageConfigCoverage
@@ -2312,6 +2316,8 @@ elaborateInstallPlan
                 , withProfLibDetail = elabProfExeDetail
                 , withProfExeDetail = elabProfLibDetail
                 }
+            okProfDyn = profilingDynamicSupportedOrUnknown compiler
+            profExe = perPkgOptionFlag pkgid False packageConfigProf
 
             ( elabProfExeDetail
               , elabProfLibDetail
