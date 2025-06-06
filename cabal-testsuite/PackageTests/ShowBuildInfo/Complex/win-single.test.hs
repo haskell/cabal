3b8a89e123307c67db6cad2561e28ff4490bf108
Ilia Baryshnikov <zlonast3@gmail.com>
2025-06-06 18:59:29 +0300

    fix for windows ci :(

--- cabal-testsuite/PackageTests/ShowBuildInfo/Complex/win-single.test.hs
+++ cabal-testsuite/PackageTests/ShowBuildInfo/Complex/win-single.test.hs
@@ -1,47 +1,78 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Test.Cabal.Prelude
-import           Test.Cabal.DecodeShowBuildInfo
+
+import Test.Cabal.DecodeShowBuildInfo
+import Test.Cabal.Prelude
 
 main = do
   skipUnlessWindows
   cabalTest $ do
     -- the With GHC-9.2+ output contains -this-unit-id
     -- the With GHC-9.4+ output contains -pgmc by default
-    skipUnlessGhcVersion ">= 9.4"
+    skipUnlessGhcVersion ">= 9.10"
     withRepo "repo" $ do
-      runShowBuildInfo ["exe:Complex"] >> withPlan (do
-        recordBuildInfo "Complex" (exe "Complex")
-        assertComponent "Complex" (exe "Complex") defCompAssertion
-          { modules = ["Other", "Paths_Complex"]
-          , sourceFiles = ["Main.lhs"]
-          , sourceDirs = ["app"]
-          })
+      runShowBuildInfo ["exe:Complex"]
+        >> withPlan
+          ( do
+              recordBuildInfo "Complex" (exe "Complex")
+              assertComponent
+                "Complex"
+                (exe "Complex")
+                defCompAssertion
+                  { modules = ["Other", "Paths_Complex"]
+                  , sourceFiles = ["Main.lhs"]
+                  , sourceDirs = ["app"]
+                  }
+          )
 
-      runShowBuildInfo ["lib:Complex"] >> withPlan (do
-        recordBuildInfo "Complex" mainLib
-        assertComponent "Complex" mainLib defCompAssertion
-          { modules = ["A", "B", "C", "D", "Paths_Complex"]
-          , sourceDirs = ["src", "doesnt-exist"]
-          })
+      runShowBuildInfo ["lib:Complex"]
+        >> withPlan
+          ( do
+              recordBuildInfo "Complex" mainLib
+              assertComponent
+                "Complex"
+                mainLib
+                defCompAssertion
+                  { modules = ["A", "B", "C", "D", "Paths_Complex"]
+                  , sourceDirs = ["src", "doesnt-exist"]
+                  }
+          )
 
-      runShowBuildInfo ["benchmark:complex-benchmarks"] >> withPlan (do
-        recordBuildInfo "Complex" (bench "complex-benchmarks")
-        assertComponent "Complex" (bench "complex-benchmarks") defCompAssertion
-          { modules = ["Paths_Complex"]
-          , sourceFiles = ["Main.hs"]
-          , sourceDirs = ["benchmark"]
-          })
+      runShowBuildInfo ["benchmark:complex-benchmarks"]
+        >> withPlan
+          ( do
+              recordBuildInfo "Complex" (bench "complex-benchmarks")
+              assertComponent
+                "Complex"
+                (bench "complex-benchmarks")
+                defCompAssertion
+                  { modules = ["Paths_Complex"]
+                  , sourceFiles = ["Main.hs"]
+                  , sourceDirs = ["benchmark"]
+                  }
+          )
 
-      runShowBuildInfo ["test:func-test"] >> withPlan (do
-        recordBuildInfo "Complex" (test "func-test")
-        assertComponent "Complex" (test "func-test") defCompAssertion
-          { sourceFiles = ["FuncMain.hs"]
-          , sourceDirs = ["test"]
-          })
+      runShowBuildInfo ["test:func-test"]
+        >> withPlan
+          ( do
+              recordBuildInfo "Complex" (test "func-test")
+              assertComponent
+                "Complex"
+                (test "func-test")
+                defCompAssertion
+                  { sourceFiles = ["FuncMain.hs"]
+                  , sourceDirs = ["test"]
+                  }
+          )
 
-      runShowBuildInfo ["test:unit-test"] >> withPlan (do
-        recordBuildInfo "Complex" (test "unit-test")
-        assertComponent "Complex" (test "unit-test") defCompAssertion
-          { sourceFiles = ["UnitMain.hs"]
-          , sourceDirs = ["test"]
-          })
+      runShowBuildInfo ["test:unit-test"]
+        >> withPlan
+          ( do
+              recordBuildInfo "Complex" (test "unit-test")
+              assertComponent
+                "Complex"
+                (test "unit-test")
+                defCompAssertion
+                  { sourceFiles = ["UnitMain.hs"]
+                  , sourceDirs = ["test"]
+                  }
+          )
