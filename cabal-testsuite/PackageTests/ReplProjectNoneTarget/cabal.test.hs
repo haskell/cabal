0a6569827628e4e7208fa4925877299a80cc8197
Phil de Joux <philderbeast@gmail.com>
2025-08-07 15:37:23 -0400

    Repl commands mentioning the fake-package

--- cabal-testsuite/PackageTests/ReplProjectNoneTarget/cabal.test.hs
+++ cabal-testsuite/PackageTests/ReplProjectNoneTarget/cabal.test.hs
@@ -3,13 +3,17 @@ import Test.Cabal.Prelude
 main = cabalTest . recordMode RecordMarked $ do
   let log = recordHeader . pure
 
-  -- Triggers "Assertion failed"
-  -- log "checking repl command with no project and --ignore-project"
-  -- _ <- fails $ cabal' "repl" ["--ignore-project"]
+  -- The following output is not what we want but is the current behaviour that
+  -- refers to the fake package.
+  log "checking repl command with no project and --ignore-project"
+  ignored <- cabal' "repl" ["--ignore-project"]
+  assertOutputContains "fake-package-0 (lib) (first run)" ignored
 
-  -- Triggers "Assertion failed"
-  -- log "checking repl command with no project and no project options"
-  -- _ <- fails $ cabal' "repl" []
+  -- The following output is not what we want but is the current behaviour that
+  -- refers to the fake package.
+  log "checking repl command with no project and no project options"
+  noOptions <- cabal' "repl" []
+  assertOutputContains "fake-package-0 (lib) (configuration changed)" noOptions
 
   log "checking repl command with a missing project"
   missing <- fails $ cabal' "repl" [ "--project-file=missing.project" ]
