25e12fb656a50441e47d0a54da48094fc9a1850f
Andrea Bedini <andrea@andreabedini.com>
2025-08-07 17:03:53 +0800

    fix(Cabal): fix abi tag in case ghc's unit-id is the same as the compiler id

--- Cabal/src/Distribution/Simple/GHC.hs
+++ Cabal/src/Distribution/Simple/GHC.hs
@@ -238,15 +238,13 @@ configureCompiler verbosity hcPath conf0 = do
       -- In this example, @AbiTag@ is "inplace".
       compilerAbiTag :: AbiTag
       compilerAbiTag =
-        maybe NoAbiTag (AbiTag . dropWhile (== '-') . stripCommonPrefix (prettyShow compilerId)) projectUnitId
-
-      wiredInUnitIds = do
-        ghcInternalUnitId <- Map.lookup "ghc-internal Unit Id" ghcInfoMap
-        ghcUnitId <- projectUnitId
-        pure
-          [ (mkPackageName "ghc", mkUnitId ghcUnitId)
-          , (mkPackageName "ghc-internal", mkUnitId ghcInternalUnitId)
-          ]
+        case Map.lookup "Project Unit Id" ghcInfoMap of
+          Nothing -> NoAbiTag
+          Just "" -> NoAbiTag
+          Just projectUnitId ->
+            case dropWhile (== '-') $ stripCommonPrefix (prettyShow compilerId) projectUnitId of
+              "" -> NoAbiTag
+              tag -> AbiTag tag
 
   let comp =
         Compiler
