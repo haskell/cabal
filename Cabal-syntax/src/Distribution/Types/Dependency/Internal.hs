c73e7234d09cd907ae49e9d8b67f45b676fd825a
Léana 江 <leana.jiang+git@icloud.com>
2026-01-08 13:53:29 +0100

    break module cycle in Distribution.Types.Dependency.Internal

--- Cabal-syntax/src/Distribution/Types/Dependency/Internal.hs
+++ Cabal-syntax/src/Distribution/Types/Dependency/Internal.hs
@@ -20,7 +20,7 @@ import Distribution.Version (VersionRange, anyVersion, simplifyVersionRange)
 import Distribution.CabalSpecVersion
 import Distribution.Compat.CharParsing (char, spaces)
 import Distribution.Compat.Parsing (between, option)
-import Distribution.Pretty
+-- import Distribution.Pretty
 import Distribution.Types.LibraryName.Internal
 import Distribution.Types.PackageName.Internal
 import Distribution.Types.UnqualComponentName.Internal
@@ -71,26 +71,6 @@ instance Binary Dependency
 instance Structured Dependency
 instance NFData Dependency where rnf = genericRnf
 
--- |
---
--- >>> prettyShow $ Dependency (mkPackageName "pkg") anyVersion mainLibSet
--- "pkg"
---
--- >>> prettyShow $ Dependency (mkPackageName "pkg") anyVersion $ NES.insert (LSubLibName $ mkUnqualComponentName "sublib") mainLibSet
--- "pkg:{pkg,sublib}"
---
--- >>> prettyShow $ Dependency (mkPackageName "pkg") anyVersion $ NES.singleton (LSubLibName $ mkUnqualComponentName "sublib")
--- "pkg:sublib"
---
--- >>> prettyShow $ Dependency (mkPackageName "pkg") anyVersion $ NES.insert (LSubLibName $ mkUnqualComponentName "sublib-b") $ NES.singleton (LSubLibName $ mkUnqualComponentName "sublib-a")
--- "pkg:{sublib-a,sublib-b}"
-instance Pretty Dependency where
-  pretty (Dependency name ver sublibs) = prettyLibraryNames name (NES.toNonEmpty sublibs) <+> pver
-    where
-      -- TODO: change to isAnyVersion after #6736
-      pver
-        | isAnyVersionLight ver = PP.empty
-        | otherwise = pretty ver
 
 
 -- | Library set with main library.
---
+++ Cabal-syntax/src/Distribution/Types/Dependency/Pretty.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Distribution.Types.Dependency.Pretty
+  ( Dependency (..)
+  , mkDependency
+  , depPkgName
+  , depVerRange
+  , depLibraries
+  , simplifyDependency
+  , mainLibSet
+  ) where
+
+import Distribution.Compat.Prelude
+import Prelude ()
+
+import Distribution.Types.VersionRange (isAnyVersionLight)
+import Distribution.Version (VersionRange, anyVersion, simplifyVersionRange)
+
+import Distribution.CabalSpecVersion
+import Distribution.Compat.CharParsing (char, spaces)
+import Distribution.Compat.Parsing (between, option)
+import Distribution.Pretty
+import Distribution.Types.LibraryName.Internal
+import Distribution.Types.PackageName.Internal
+import Distribution.Types.UnqualComponentName.Internal
+
+import qualified Distribution.Compat.NonEmptySet as NES
+import qualified Text.PrettyPrint as PP
+
+-- |
+--
+-- >>> prettyShow $ Dependency (mkPackageName "pkg") anyVersion mainLibSet
+-- "pkg"
+--
+-- >>> prettyShow $ Dependency (mkPackageName "pkg") anyVersion $ NES.insert (LSubLibName $ mkUnqualComponentName "sublib") mainLibSet
+-- "pkg:{pkg,sublib}"
+--
+-- >>> prettyShow $ Dependency (mkPackageName "pkg") anyVersion $ NES.singleton (LSubLibName $ mkUnqualComponentName "sublib")
+-- "pkg:sublib"
+--
+-- >>> prettyShow $ Dependency (mkPackageName "pkg") anyVersion $ NES.insert (LSubLibName $ mkUnqualComponentName "sublib-b") $ NES.singleton (LSubLibName $ mkUnqualComponentName "sublib-a")
+-- "pkg:{sublib-a,sublib-b}"
+instance Pretty Dependency where
+  pretty (Dependency name ver sublibs) = prettyLibraryNames name (NES.toNonEmpty sublibs) <+> pver
+    where
+      -- TODO: change to isAnyVersion after #6736
+      pver
+        | isAnyVersionLight ver = PP.empty
+        | otherwise = pretty ver
