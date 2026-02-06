a6691b82f5b9b7bb11d931d86f926d99bec77b70
Phil de Joux <philderbeast@gmail.com>
2026-02-06 14:49:12 -0500

    Add tests using projects

---
+++ cabal-testsuite/PackageTests/RequireExplicit/MultiPkgInProject/cabal.test.hs
@@ -0,0 +1,22 @@
+import Test.Cabal.Prelude
+
+-- See #4332, dep solving output is not deterministic
+-- other-lib is a dependency of b, but it's not listed in cabal.project
+-- and some-exe is a build-tool dependency of b, again not listed
+
+assertErr x = assertOutputContains ("not a user-provided goal nor mentioned as a constraint, but reject-unconstrained-dependencies=" ++ x ++ " was set")
+constraint x = "--constraint=" ++ x
+opts = [ "all", "--dry-run" ]
+
+main = do
+  cabalTest' "multipkg-all" . withRepo "repo" $ do
+    let proj n = "--project-file=cabal.all" <.> n <.> ".project" : opts
+    let no n = assertErr "all" =<< fails (cabal' "build" $ proj n)
+    mapM_ (no . ("no" <.>)) ["exe-any", "lib-any", "lib-exe-any"]
+
+  cabalTest' "multipkg-eq" . withRepo "repo" $ do
+    let proj n = "--project-file=cabal.eq" <.> n <.> ".project" : opts
+    let no n = assertErr "eq" =<< fails (cabal' "build" $ proj n)
+    let go n = cabal' "build" $ proj n
+    mapM_ (go . ("go" <.>)) ["eq-and-range", "eq"]
+    mapM_ (no . ("no" <.>)) ["eq-and-range", "exe-eq", "lib-eq", "lib-exe-eq", "lib-range"]
