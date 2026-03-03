19c26ff4d16be5c0e981144a97142692041d00a5
parsonsmatt <parsonsmatt@gmail.com>
2026-03-02 17:28:04 -0700

    nah

--- Cabal/src/Distribution/PackageDescription/Check/Target.hs
+++ Cabal/src/Distribution/PackageDescription/Check/Target.hs
@@ -205,7 +205,6 @@ checkTestSuite
       TestSuiteUnsupported tt ->
         tellP (PackageBuildWarning $ TestsuiteNotSupported tt)
       _ -> return ()
-
     checkP
       mainIsWrongExt
       (PackageBuildImpossible NoHsLhsMain)
@@ -294,7 +293,6 @@ checkBenchmark
         case benchmarkInterface_ of
           BenchmarkExeV10 _ f -> takeExtension (getSymbolicPath f) `notElem` [".hs", ".lhs"]
           _ -> False
-
 
 -- ------------------------------------------------------------
 -- Build info
