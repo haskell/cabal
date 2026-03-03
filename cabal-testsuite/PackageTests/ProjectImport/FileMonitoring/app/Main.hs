4bfa98048dd5d6cd056f457124543e69a02158e9
Julian Ospald <hasufell@posteo.de>
2026-03-03 17:36:19 +0800

    Add test for #10255

---
+++ cabal-testsuite/PackageTests/ProjectImport/FileMonitoring/app/Main.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import System.FilePath
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
---
+++ cabal-testsuite/PackageTests/ProjectImport/FileMonitoring/test/Main.hs
@@ -0,0 +1,4 @@
+module Main (main) where
+
+main :: IO ()
+main = puStrLn "Test suite not yet implemented."
