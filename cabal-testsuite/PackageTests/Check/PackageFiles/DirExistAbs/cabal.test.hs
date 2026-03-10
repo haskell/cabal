adbefa5da8799e715c82a130eacfe592aee22b6f
Phil de Joux <philderbeast@gmail.com>
2026-03-10 10:05:53 -0400

    Fix typo existant
    - Fix typo nonexistant
    - Add changelog entry
    - Exclude typo intentionally included in changelog

--- cabal-testsuite/PackageTests/Check/PackageFiles/DirExistAbs/cabal.test.hs
+++ cabal-testsuite/PackageTests/Check/PackageFiles/DirExistAbs/cabal.test.hs
@@ -1,5 +1,5 @@
 import Test.Cabal.Prelude
 
--- Do not warn on non-existant directory if it is absolute.
+-- Do not warn on non-existent directory if it is absolute.
 main = cabalTest $
   cabal "check" []
