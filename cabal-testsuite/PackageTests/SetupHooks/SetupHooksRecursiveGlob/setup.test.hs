c80e90a4de1c0406d29f32127ac212be69a7f098
Tobias Dammers <tdammers@gmail.com>
2026-03-23 13:14:36 +0100

    Failing integration test

--- cabal-testsuite/PackageTests/SetupHooks/SetupHooksRecursiveGlob/setup.test.hs
+++ cabal-testsuite/PackageTests/SetupHooks/SetupHooksRecursiveGlob/setup.test.hs
@@ -2,3 +2,9 @@ import Test.Cabal.Prelude
 main = setupTest $ do
   setup "configure" []
   setup "build" []
+  -- Now modify one file and build again to see if that exact file gets rebuilt
+  env <- getTestEnv
+  liftIO $ do
+    let srcDir = testSourceDir env
+    writeFile (srcDir </> "src/Foo/B.hs") "module Foo.B where\nModified text"
+  setup "build" []
---
+++ cabal-testsuite/PackageTests/SetupHooks/SetupHooksRecursiveGlob/src/Foo/B.hs
@@ -0,0 +1,2 @@
+module Foo.B where
+Modified text
