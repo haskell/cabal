518e76b665cee0f5b3692a6f79d01b9a09e41950
Tobias Dammers <tdammers@gmail.com>
2026-03-26 15:08:47 +0100

    Put modified file back as it was after running test

--- cabal-testsuite/PackageTests/SetupHooks/SetupHooksRecursiveGlob/setup.test.hs
+++ cabal-testsuite/PackageTests/SetupHooks/SetupHooksRecursiveGlob/setup.test.hs
@@ -1,14 +1,21 @@
 import Test.Cabal.Prelude
 
-main = setupTest $ do
-  setup "configure" []
-  setup "build" []
-  -- Now modify one file and build again to see if that exact file gets rebuilt
-  env <- getTestEnv
-  liftIO $ do
-    let srcDir = testSourceDir env
-    writeFile (srcDir </> "src/Foo/B.ppExt") "module Foo.B where\nModified text"
-  setup "build" []
-  liftIO $ do
-    let srcDir = testSourceDir env
-    writeFile (srcDir </> "src/Foo/B.ppExt") "module Foo.B where\nOriginal text"
+import Control.Monad.Catch (finally)
+
+main =
+  setupTest $ do
+    runSetupTest `finally` cleanup
+  where
+    runSetupTest = do
+        setup "configure" []
+        setup "build" []
+        -- Now modify one file and build again to see if that exact file gets rebuilt
+        env <- getTestEnv
+        liftIO $ do
+          let srcDir = testSourceDir env
+          writeFile (srcDir </> "src/Foo/B.ppExt") "module Foo.B where\nModified text"
+        setup "build" []
+    cleanup = do
+      env <- getTestEnv
+      let srcDir = testSourceDir env
+      liftIO $ writeFile (srcDir </> "src/Foo/B.ppExt") "module Foo.B where\nOriginal text\n"
