db141072e3de88dbf00d6cb3ff4064b197074780
Ilia Baryshnikov <zlonast3@gmail.com>
2026-05-14 14:58:17 +0300

    add test for foreign source in TemplateHaskell
    Co-authored-by: Yuriy Syrovetskiy <git-orbi@cblp.su>

--- cabal-testsuite/PackageTests/FFI/ForeignOptsCapi/cabal.test.hs
+++ cabal-testsuite/PackageTests/FFI/ForeignOptsCapi/cabal.test.hs
@@ -1,4 +1,5 @@
 import Test.Cabal.Prelude
+
 main = cabalTest $ do
-    cabal "v2-build" ["foreign-opts-capi-exe"]
-    withPlan $ runPlanExe "foreign-opts-capi" "foreign-opts-capi-exe" []
+  cabal "v2-build" ["foreign-opts-capi-exe"]
+  withPlan $ runPlanExe "foreign-opts-capi" "foreign-opts-capi-exe" []
