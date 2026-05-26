27813037486e64362abad9892853287f57382067
Phil de Joux <philderbeast@gmail.com>
2026-05-26 09:49:44 -0400

    Promote comment to haddocks

--- cabal-install/src/Distribution/Client/ProjectPlanning.hs
+++ cabal-install/src/Distribution/Client/ProjectPlanning.hs
@@ -4684,6 +4684,8 @@ determineCoverageFor configuredPkg plan =
     isIndefiniteOrInstantiation :: ModuleShape -> Bool
     isIndefiniteOrInstantiation = not . Set.null . modShapeRequires
 
+-- | Look up and merge the options from the project config that apply to all
+-- packages, all project local packages, and to specific named packages.
 lookupPerPkgOption
   :: (Package pkg, Monoid m)
   => pkg
@@ -4694,9 +4696,6 @@ lookupPerPkgOption
   -> (pkg -> Bool)
   -> m
 lookupPerPkgOption pkg f allPackagesConfig localPackagesConfig perPackageConfig isLocalPkg =
-  -- This is where we merge the options from the project config that
-  -- apply to all packages, all project local packages, and to specific
-  -- named packages
   global `mappend` local `mappend` perpkg
   where
     global = f allPackagesConfig
