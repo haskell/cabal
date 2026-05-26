c1dc8e1ea1c2bfde06499cca2a986cd4d96aad4a
Phil de Joux <philderbeast@gmail.com>
2026-05-26 09:50:17 -0400

    Reformat that fourmolu accepts

--- cabal-install/src/Distribution/Client/ProjectPlanning.hs
+++ cabal-install/src/Distribution/Client/ProjectPlanning.hs
@@ -4706,8 +4706,6 @@ lookupPerPkgOption pkg f allPackagesConfig localPackagesConfig perPackageConfig 
   where
     global = f allPackagesConfig
     local
-      | isLocalPkg pkg =
-          f localPackagesConfig
-      | otherwise =
-          mempty
+      | isLocalPkg pkg = f localPackagesConfig
+      | otherwise = mempty
     perpkg = maybe mempty f (Map.lookup (packageName pkg) perPackageConfig)
