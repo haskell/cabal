8091e259ae00493c0256e83fb1a753b839b97231
Phil de Joux <philderbeast@gmail.com>
2026-06-23 21:33:27 -0400

    Follow hlint suggestion: redundant $

--- Cabal/src/Distribution/Simple/Utils.hs
+++ Cabal/src/Distribution/Simple/Utils.hs
@@ -947,7 +947,7 @@ rawSystemExitCode
   -> Maybe [(String, String)]
   -> IO ExitCode
 rawSystemExitCode verbosity mbWorkDir path args menv =
-  withFrozenCallStack $
+  withFrozenCallStack
     ( fst
         <$> rawSystemIOWithEnvAndAction
           verbosity
