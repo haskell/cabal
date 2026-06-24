38046da26ac103e3f3b00a144b4653073387d6ce
Phil de Joux <philderbeast@gmail.com>
2026-06-23 21:25:43 -0400

    Follow hlint suggestion: redundant $

--- cabal-install/src/Distribution/Client/TargetSelector.hs
+++ cabal-install/src/Distribution/Client/TargetSelector.hs
@@ -2058,8 +2058,7 @@ matchPackageDir ps = \str fstatus ->
   case fstatus of
     FileStatusExistsDir canondir ->
       orNoSuchThing "package directory" str (map (snd . fst) dirs) $
-        increaseConfidenceFor $
-          (snd <$> matchExactly (fst . fst) dirs canondir)
+        increaseConfidenceFor (snd <$> matchExactly (fst . fst) dirs canondir)
     _ -> mzero
   where
     dirs =
@@ -2072,8 +2071,7 @@ matchPackageFile ps = \str fstatus -> do
   case fstatus of
     FileStatusExistsFile canonfile ->
       orNoSuchThing "package .cabal file" str (map (snd . fst) files) $
-        increaseConfidenceFor $
-          (snd <$> matchExactly (fst . fst) files canonfile)
+        increaseConfidenceFor (snd <$> matchExactly (fst . fst) files canonfile)
     _ -> mzero
   where
     files =
