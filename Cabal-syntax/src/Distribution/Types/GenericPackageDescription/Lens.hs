87d2d4ee7d10f28134bc85189c9e9a8dd51419cc
Phil de Joux <philderbeast@gmail.com>
2026-06-23 21:14:11 -0400

    Follow hlint suggestion: redundant <$>

--- Cabal-syntax/src/Distribution/Types/GenericPackageDescription/Lens.hs
+++ Cabal-syntax/src/Distribution/Types/GenericPackageDescription/Lens.hs
@@ -79,7 +79,7 @@ allCondTrees
   -> GenericPackageDescription
   -> f GenericPackageDescription
 allCondTrees f (GenericPackageDescription p v a1 x1 x2 x3 x4 x5 x6) =
-  (GenericPackageDescription p <$> pure v)
+  pure (GenericPackageDescription p v)
     <*> pure a1
     <*> traverse f x1
     <*> (traverse . _2) f x2
