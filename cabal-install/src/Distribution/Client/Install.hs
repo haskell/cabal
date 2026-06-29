2bd1b3a178e315350c58ef548ceca21797796ab0
Andrea Bedini <andrea@bedini.au>
2026-06-29 13:57:59 +0800

    refactor: make Graph.broken return NonEmpty missing deps
    Distribution.Compat.Graph.broken now returns [(a, NonEmpty (Key a))] and only
    includes nodes that actually have missing dependencies. PackageMissingDeps in
    both Client.InstallPlan and Client.SolverInstallPlan carry NonEmpty accordingly.

--- cabal-install/src/Distribution/Client/Install.hs
+++ cabal-install/src/Distribution/Client/Install.hs
@@ -717,7 +717,7 @@ pruneInstallPlan pkgSpecifiers =
           ordNub
             [ depid
             | SolverInstallPlan.PackageMissingDeps _ depids <- problems
-            , depid <- depids
+            , depid <- toList depids
             , packageName depid `elem` targetnames
             ]
 
