de3a401f6ae377e5448eb3b45adc1bb856bce72f
Léana <leana.jiang+git@icloud.com>
2026-07-20 07:40:06 +0200

    Retain comments in field parser
    This lays the foundation for Cabal-exactprint to consume user comments.
    
    The parser of the envelope format ("field parser") now annotates each of
    the parsed fields with its preceding and succeeding comments, along with
    the existing source location annotation 'Position'.
    
    This change is orthogonal to the final chosen exactprint implementation,
    or the algebra we use to modify existing fields and/or generate new
    fields. Instead, it merely makes the user comments available to any
    consumer that reads cabal fields, the envelope format.
    
    readFields* functions now have their counterparts that parses with
    comments, named readFieldsWithComments*.

--- Cabal-syntax/src/Distribution/Parsec/Position.hs
+++ Cabal-syntax/src/Distribution/Parsec/Position.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Distribution.Parsec.Position
   ( Position (..)
   , incPos
@@ -16,10 +19,11 @@ data Position
   = Position
       {-# UNPACK #-} !Int -- row
       {-# UNPACK #-} !Int -- column
-  deriving (Eq, Ord, Show, Generic)
+  deriving (Eq, Ord, Show, Generic, Data)
 
 instance Binary Position
 instance NFData Position
+instance Structured Position
 
 -- | Shift position by n columns to the right.
 incPos :: Int -> Position -> Position
