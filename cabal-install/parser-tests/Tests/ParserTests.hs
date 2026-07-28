e61ffde6f47d5503b5eeffbd0920a35f3b9fc301
Phil de Joux <philderbeast@gmail.com>
2026-07-28 12:32:38 -0400

    Add a glob test for optional packages

--- cabal-install/parser-tests/Tests/ParserTests.hs
+++ cabal-install/parser-tests/Tests/ParserTests.hs
@@ -75,6 +75,7 @@ parserTests =
     [ testCase "read packages" testPackages
     , testCase "read packages glob" testPackagesGlob
     , testCase "read optional-packages" testOptionalPackages
+    , testCase "read optional-packages glob" testOptionalPackagesGlob
     , testCase "read extra-packages" testExtraPackages
     , testCase "read source-repository-package" testSourceRepoList
     , testCase "read project-config-build-only" testProjectConfigBuildOnly
@@ -113,6 +114,12 @@ testOptionalPackages :: Assertion
 testOptionalPackages = do
   let expected = [".", "packages/packages.cabal"]
   (config, legacy) <- readConfigDefault "optional-packages"
+  assertConfigEquals expected config legacy (projectPackagesOptional . snd . condTreeData)
+
+testOptionalPackagesGlob :: Assertion
+testOptionalPackagesGlob = do
+  let expected = ["*/*.cabal", "../{foo,bar}/"]
+  (config, legacy) <- readConfig "optional-packages" "cabal.glob.project"
   assertConfigEquals expected config legacy (projectPackagesOptional . snd . condTreeData)
 
 testSourceRepoList :: Assertion
