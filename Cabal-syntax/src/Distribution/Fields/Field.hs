309c59a0c971d8bb1e09a438f6e766fcf76d4d4f
Léana <leana@jappiesoftware.com>
2026-08-14 09:04:28 +0200

    clean up pragmas, reduce diff, reword wonky comments

--- Cabal-syntax/src/Distribution/Fields/Field.hs
+++ Cabal-syntax/src/Distribution/Fields/Field.hs
@@ -1,7 +1,4 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE StandaloneDeriving #-}
 
 -- | Cabal-like file AST types: 'Field', 'Section' etc
 --
