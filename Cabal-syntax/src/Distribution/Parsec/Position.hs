4ceab2e66e635d06379154adbddc63a9792befe3
Léana <leana@jappiesoftware.com>
2026-08-14 09:04:28 +0200

    clean up pragmas, reduce diff, reword wonky comments

--- Cabal-syntax/src/Distribution/Parsec/Position.hs
+++ Cabal-syntax/src/Distribution/Parsec/Position.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DeriveGeneric #-}
-
 module Distribution.Parsec.Position
   ( Position (..)
   , incPos
@@ -19,7 +16,7 @@ data Position
   = Position
       {-# UNPACK #-} !Int -- row
       {-# UNPACK #-} !Int -- column
-  deriving (Eq, Ord, Show, Generic, Data)
+  deriving (Eq, Ord, Show, Generic)
 
 instance Binary Position
 instance NFData Position
