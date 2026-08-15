44ba14917d16250528b39681e326088c9eef410d
Phil de Joux <philderbeast@gmail.com>
2026-08-15 15:27:51 -0400

    act-as-setup warning and error

--- cabal-install/src/Distribution/Client/Main.hs
+++ cabal-install/src/Distribution/Client/Main.hs
@@ -232,6 +232,7 @@ import Distribution.Simple.Utils
   , cabalVersion
   , createDirectoryIfMissingVerbose
   , die'
+  , dieNoWrap
   , dieNoVerbosity
   , dieWithException
   , findPackageDesc
@@ -1594,9 +1595,14 @@ actAsSetupAction actAsSetupFlags args _globalFlags =
             Simple.autoconfSetupHooks
             defaultVerbosityHandles
             args
-        Make -> error "actAsSetupAction Main"
-        Hooks -> error "actAsSetupAction Hooks"
-        Custom -> error "actAsSetupAction Custom"
+        Make -> unsupportedBuildType
+        Hooks -> unsupportedBuildType
+        Custom -> unsupportedBuildType
+  where
+    verbosity = mkVerbosity defaultVerbosityHandles normal
+    unsupportedBuildType = do
+      warn verbosity "act-as-setup accepts --build-type=Simple|Configure, case-sensitively."
+      dieNoWrap verbosity "act-as-setup doesn't accept --build-type=Make|Hooks|Custom."
 
 manpageAction :: [CommandSpec action] -> ManpageFlags -> [String] -> Action
 manpageAction commands flags extraArgs _ = do
