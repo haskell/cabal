3d73d4f6dc91986a6cc3fb148208cde05850f0fe
Ilya Baryshnikov <zlonast3@gmail.com>
2026-09-02 10:15:35 +0300

    -Werror start work on cabal-testsuite.cabal:test-runtime-deps

--- cabal-testsuite/static/Main.hs
+++ cabal-testsuite/static/Main.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE CPP #-}
 module Main where
+
+import Control.Monad.Catch ()
+import Control.Monad.Trans.Class ()
+import Data.ByteString ()
+import Data.Set ()
+import Data.Time ()
+import Distribution.Simple ()
+import Distribution.Simple.SetupHooks ()
+import Distribution.Types.Version ()
+import System.Directory ()
+import System.FilePath ()
+import System.Process ()
+import Test.Cabal.Run ()
+
+#ifdef mingw32_HOST_OS
+import System.Win32 ()
+#else
+import System.Posix ()
+#endif
 
 main :: IO ()
 main = return ()
