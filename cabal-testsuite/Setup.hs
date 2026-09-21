a1b386b7d81936cef143c3a97ad22497acde7691
Ilya Baryshnikov <zlonast3@gmail.com>
2026-09-21 13:25:28 +0300

    cabal-testsuite: build the custom Setup with Cabal 3.18.1.0
    Co-authored-by: fendor <fendor@posteo.de>

--- cabal-testsuite/Setup.hs
+++ cabal-testsuite/Setup.hs
@@ -11,9 +11,10 @@ import Distribution.Simple.Utils
 import Distribution.Types.LocalBuildInfo
 import Distribution.Types.ModuleRenaming
 import Distribution.Types.UnqualComponentName
-import Distribution.Utils.Path (getSymbolicPath)
-import Distribution.Verbosity
+import Distribution.Utils.Path (getSymbolicPath, makeSymbolicPath)
+import Distribution.Verbosity (Verbosity, defaultVerbosityHandles, mkVerbosity, normal)
 
+import Data.List (intercalate, isPrefixOf)
 import System.Directory
 import System.FilePath
 
@@ -21,7 +22,9 @@ main :: IO ()
 main = defaultMainWithHooks simpleUserHooks
     { confHook = \args flags -> do
         lbi <- confHook simpleUserHooks args flags
-        generateScriptEnvModule lbi (fromFlagOrDefault minBound (configVerbosity flags))
+        generateScriptEnvModule lbi
+            $ mkVerbosity defaultVerbosityHandles
+            $ fromFlagOrDefault normal (configVerbosity flags)
         pure lbi
     }
 
@@ -46,7 +49,7 @@ generateScriptEnvModule lbi verbosity = do
       , "import Distribution.Utils.Path"
       , ""
       , "lbiPackageDbStack :: PackageDBStackCWD"
-      , "lbiPackageDbStack = " ++ show lbiPackageDbStack
+      , "lbiPackageDbStack = " ++ renderPackageDBStack lbiPackageDbStack
       , ""
       , "lbiPlatform :: Platform"
       , "lbiPlatform = " ++ show (hostPlatform lbi)
@@ -56,7 +59,7 @@ generateScriptEnvModule lbi verbosity = do
       -- to make sure that it is always defined,
       -- even if the test suite is being built with an older Cabal
 #if MIN_VERSION_Cabal(3,15,0)
-      , "lbiCompiler = " ++ show (compiler lbi)
+      , "lbiCompiler = " ++ fixupCompilerShow (show (compiler lbi))
 #else
       , "lbiCompiler = " ++ init (show (compiler lbi)) ++ ", compilerWiredInUnitIds = Nothing}"
 #endif
@@ -76,14 +79,45 @@ generateScriptEnvModule lbi verbosity = do
   where
     moduledir = libAutogenDir </> "Test" </> "Cabal"
     -- fixme: use component-specific folder
-    libAutogenDir = autogenPackageModulesDir lbi
+    libAutogenDir = getSymbolicPath (autogenPackageModulesDir lbi)
 
 -- | Convert package database into absolute path, so that
 -- if we change working directories in a subprocess we get the correct database.
 canonicalizePackageDB :: PackageDB -> IO PackageDB
 canonicalizePackageDB (SpecificPackageDB path)
-    = SpecificPackageDB `fmap` canonicalizePath path
+    = SpecificPackageDB . makeSymbolicPath <$> canonicalizePath (getSymbolicPath path)
 canonicalizePackageDB x = return x
+
+-- | Render a package database stack as Haskell source code.
+--
+-- 'PackageDBStackCWD' holds plain 'FilePath's (unlike the 'PackageDB' stack in
+-- 'LocalBuildInfo', whose paths are 'SymbolicPath's with an abstract
+-- constructor, so we cannot use 'show' on it).
+renderPackageDBStack :: PackageDBStack -> String
+renderPackageDBStack pdbs = "[" ++ intercalate "," (map renderPackageDB pdbs) ++ "]"
+
+renderPackageDB :: PackageDB -> String
+renderPackageDB GlobalPackageDB = "GlobalPackageDB"
+renderPackageDB UserPackageDB = "UserPackageDB"
+renderPackageDB (SpecificPackageDB path) = "SpecificPackageDB " ++ show (getSymbolicPath path)
+
+-- | The 'Show' instance of 'Compiler' renders wired-in unit ids as
+-- @PackageName \"ghc@ and @UnitId \"ghc-9.14.1\"@, but neither constructor is
+-- exported (GHC >= 9.14 populates 'compilerWiredInUnitIds', older compilers
+-- yield 'Nothing'), so the raw output is not valid source code. Use the
+-- exported 'mkPackageName' / 'mkUnitId' instead.
+fixupCompilerShow :: String -> String
+fixupCompilerShow = go
+  where
+    fixes =
+      [ ("PackageName \"", "mkPackageName \"")
+      , ("UnitId \"", "mkUnitId \"")
+      ]
+    go s = case [ (repl, drop (length needle) s) | (needle, repl) <- fixes, needle `isPrefixOf` s ] of
+      ((repl, rest) : _) -> repl ++ go rest
+      [] -> case s of
+        (c : cs) -> c : go cs
+        [] -> []
 
 -- | Compute the set of @-package-id@ flags which would be passed when
 -- building the public library.  Assumes that the public library is
