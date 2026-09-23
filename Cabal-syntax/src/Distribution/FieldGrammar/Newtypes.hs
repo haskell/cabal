9c049659cd77655e2a5a3c04f3e86a7a6cc80a98
Phil de Joux <philderbeast@gmail.com>
2026-09-23 13:55:40 -0400

    Use alternative for removed unpack'

--- Cabal-syntax/src/Distribution/FieldGrammar/Newtypes.hs
+++ Cabal-syntax/src/Distribution/FieldGrammar/Newtypes.hs
@@ -139,10 +139,10 @@ newtype List sep b a = List {_getList :: [a]}
 -- >>> :t alaList' FSep Token
 -- alaList' FSep Token :: [String] -> List FSep Token String
 --
--- >>> unpack' (alaList' FSep Token) <$> eitherParsec "foo bar foo"
+-- >>> _getList <$> (eitherParsec "foo bar foo" :: Either String (List FSep Token String))
 -- Right ["foo","bar","foo"]
 --
--- >>> unpack' (alaList' FSep Token) <$> eitherParsec "xL{4,IE-,eK<}fE?e"
+-- >>> _getList <$> (eitherParsec "xL{4,IE-,eK<}fE?e" :: Either String (List FSep Token String))
 -- Right ["xL{4","IE-","eK<}fE?e"]
 alaList :: sep -> [a] -> List sep (Identity a) a
 alaList _ = List
